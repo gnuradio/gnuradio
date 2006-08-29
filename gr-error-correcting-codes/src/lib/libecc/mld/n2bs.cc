ea554bd9003efc6fe32619f49962fd18ec65b9f2
eb <eb@221aa14e-8319-0410-a670-987f0aec2ac5>
2006-08-29 03:24:31 +0000

    revised fix for ticket:54
    git-svn-id: http://gnuradio.org/svn/gnuradio/trunk@3449 221aa14e-8319-0410-a670-987f0aec2ac5

--- gr-error-correcting-codes/src/lib/libecc/mld/n2bs.cc
+++ gr-error-correcting-codes/src/lib/libecc/mld/n2bs.cc
@@ -23,78 +23,15 @@
 #include <n2bs.h>
 #include <iostream>
 
-static const int g_num_bits_per_byte = 8;
-
-std::string DoIt (long long number, char digits)
+std::string _n2bs_helper(unsigned long long  v, size_t nbinary_digits)
 {
-  std::string retVal (digits, '0');
-  if (number != 0)
-    for (char n = digits-1; n >= 0; n--) {
-      if (number & 1) {
+  std::string retVal (nbinary_digits, '0');
+  if (v != 0)
+    for (int n = int(nbinary_digits) - 1; n >= 0; n--) {
+      if (v & 1) {
 	retVal[n] = '1';
       }
-      number >>= 1;
+      v >>= 1;
     }
   return (retVal);
-}
-
-std::string n2bs (char number, unsigned char digits)
-{
-  if (digits > (unsigned char)(sizeof (char) * g_num_bits_per_byte))
-    digits = sizeof (char) * g_num_bits_per_byte;
-  return DoIt ((long long) number, (char) digits);
-}
-std::string n2bs (short number, unsigned char digits)
-{
-  if (digits > (unsigned char)(sizeof (short) * g_num_bits_per_byte))
-    digits = sizeof (short) * g_num_bits_per_byte;
-  return DoIt ((long long) number, (char) digits);
-}
-std::string n2bs (int number, unsigned char digits)
-{
-  if (digits > (unsigned char)(sizeof (int) * g_num_bits_per_byte))
-    digits = sizeof (int) * g_num_bits_per_byte;
-  return DoIt ((long long) number, (char) digits);
-}
-std::string n2bs (long number, unsigned char digits)
-{
-  if (digits > (unsigned char)(sizeof (long long) * g_num_bits_per_byte))
-    digits = sizeof (long long) * g_num_bits_per_byte;
-  return DoIt (number, (char) digits);
-}
-std::string n2bs (long long number, unsigned char digits)
-{
-  if (digits > (unsigned char)(sizeof (long long) * g_num_bits_per_byte))
-    digits = sizeof (long long) * g_num_bits_per_byte;
-  return DoIt (number, (char) digits);
-}
-std::string n2bs (unsigned char number, unsigned char digits)
-{
-  if (digits > (unsigned char)(sizeof (unsigned char) * g_num_bits_per_byte))
-    digits = sizeof (unsigned char) * g_num_bits_per_byte;
-  return DoIt ((long long) number, (char) digits);
-}
-std::string n2bs (unsigned short number, unsigned char digits)
-{
-  if (digits > (unsigned char)(sizeof (unsigned short) * g_num_bits_per_byte))
-    digits = sizeof (unsigned short) * g_num_bits_per_byte;
-  return DoIt ((long long) number, (char) digits);
-}
-std::string n2bs (unsigned int number, unsigned char digits)
-{
-  if (digits > (unsigned char)(sizeof (unsigned int) * g_num_bits_per_byte))
-    digits = sizeof (unsigned int) * g_num_bits_per_byte;
-  return DoIt ((long long) number, (char) digits);
-}
-std::string n2bs (unsigned long number, unsigned char digits)
-{
-  if (digits > (unsigned char)(sizeof (unsigned long long) * g_num_bits_per_byte))
-    digits = sizeof (unsigned long long) * g_num_bits_per_byte;
-  return DoIt ((long long) number, (char) digits);
-}
-std::string n2bs (unsigned long long number, unsigned char digits)
-{
-  if (digits > (unsigned char)(sizeof (unsigned long long) * g_num_bits_per_byte))
-    digits = sizeof (unsigned long long) * g_num_bits_per_byte;
-  return DoIt ((long long) number, (char) digits);
 }
