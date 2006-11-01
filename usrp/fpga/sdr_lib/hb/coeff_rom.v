82550ac99edb201ef395fd2d7e072d4622871b37
matt <matt@221aa14e-8319-0410-a670-987f0aec2ac5>
2006-11-01 23:36:51 +0000

    optimized halfband coefficients, courtesy of Larry Doolittle
    git-svn-id: http://gnuradio.org/svn/gnuradio/trunk@3917 221aa14e-8319-0410-a670-987f0aec2ac5

--- usrp/fpga/sdr_lib/hb/coeff_rom.v
+++ usrp/fpga/sdr_lib/hb/coeff_rom.v
@@ -4,14 +4,14 @@ module coeff_rom (input clock, input [2:0] addr, output reg [15:0] data);
 
    always @(posedge clock)
      case (addr)
-       3'd0 : data <= #1 -16'd16;
-       3'd1 : data <= #1 16'd74;
-       3'd2 : data <= #1 -16'd254;
-       3'd3 : data <= #1 16'd669;
-       3'd4 : data <= #1 -16'd1468;
-       3'd5 : data <= #1 16'd2950;
-       3'd6 : data <= #1 -16'd6158;
-       3'd7 : data <= #1 16'd20585;
+       3'd0 : data <= #1 -16'd49;
+       3'd1 : data <= #1 16'd165;
+       3'd2 : data <= #1 -16'd412;
+       3'd3 : data <= #1 16'd873;
+       3'd4 : data <= #1 -16'd1681;
+       3'd5 : data <= #1 16'd3135;
+       3'd6 : data <= #1 -16'd6282;
+       3'd7 : data <= #1 16'd20628;
      endcase // case(addr)
       
 endmodule // coeff_rom
