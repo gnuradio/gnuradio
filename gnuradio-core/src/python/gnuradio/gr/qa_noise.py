d23ea17ced9b398c97203e7ba1ad37d64f9812b1
eb <eb@221aa14e-8319-0410-a670-987f0aec2ac5>
2007-01-16 17:45:41 +0000

    added minimal qa code for gr_noise_source
    git-svn-id: http://gnuradio.org/svn/gnuradio/trunk@4279 221aa14e-8319-0410-a670-987f0aec2ac5

---
+++ gnuradio-core/src/python/gnuradio/gr/qa_noise.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Free Software Foundation, Inc.
+# 
+# This file is part of GNU Radio
+# 
+# GNU Radio is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+# 
+# GNU Radio is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with GNU Radio; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street,
+# Boston, MA 02110-1301, USA.
+# 
+
+from gnuradio import gr, gr_unittest
+
+class test_noise_source(gr_unittest.TestCase):
+
+    def setUp (self):
+        self.fg = gr.flow_graph ()
+
+    def tearDown (self):
+        self.fg = None
+
+    def test_001(self):
+        # Just confirm that we can instantiate a noise source
+        op = gr.noise_source_f(gr.GR_GAUSSIAN, 10, 10)
+
+if __name__ == '__main__':
+    gr_unittest.main ()
+        
