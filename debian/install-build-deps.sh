577d40edc52e303a2594b3b51bd22a4927878273
jcorgan <jcorgan@221aa14e-8319-0410-a670-987f0aec2ac5>
2007-10-10 09:55:40 +0000

    Merged r6502:6602 from bdale/deb into trunk.  Tweaks on Debian packaging.
    git-svn-id: http://gnuradio.org/svn/gnuradio/trunk@6609 221aa14e-8319-0410-a670-987f0aec2ac5

---
+++ debian/install-build-deps.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+ISSUE=`cut -f 1 -d ' ' /etc/issue`
+
+if test "$ISSUE" = "Ubuntu"; then
+    FFTW3DEV=fftw3-dev
+else
+    FFTW3DEV=libfftw3-dev
+fi
+
+sudo apt-get install \
+    autotools-dev doxygen $FFTW3DEV libasound2-dev \
+    libboost-dev libcppunit-dev libjack0.100.0-dev libsdl1.2-dev \
+    portaudio19-dev libtool libusb-dev pkg-config python python-all-dev \
+    python-central python-numpy python-wxgtk2.6 sdcc swig xmlto
+
