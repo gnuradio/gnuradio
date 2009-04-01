85121bf83232ff0026e945185efe19c765463fa5
matt <matt@221aa14e-8319-0410-a670-987f0aec2ac5>
2009-04-01 02:25:04 +0000

    now checks the crc as well for the received side
    git-svn-id: http://gnuradio.org/svn/gnuradio/trunk@10728 221aa14e-8319-0410-a670-987f0aec2ac5

--- usrp2/fpga/simple_gemac/crc.v
+++ usrp2/fpga/simple_gemac/crc.v
@@ -5,7 +5,8 @@ module crc
    input clear, 
    input [7:0] data,
    input calc,
-   output [31:0] crc_out);
+   output [31:0] crc_out,
+   output match);
    
    function[31:0]  NextCRC;
       input[7:0]      D;
@@ -59,5 +60,7 @@ module crc
 		      crc_reg[16],crc_reg[17],crc_reg[18],crc_reg[19],crc_reg[20],crc_reg[21],crc_reg[22],crc_reg[23],
 		      crc_reg[8],crc_reg[9],crc_reg[10],crc_reg[11],crc_reg[12],crc_reg[13],crc_reg[14],crc_reg[15],
 		      crc_reg[0],crc_reg[1],crc_reg[2],crc_reg[3],crc_reg[4],crc_reg[5],crc_reg[6],crc_reg[7] };
-   
+
+   assign match  = (crc_reg == 32'hc704_dd7b);
+		    
 endmodule // crc
