a5b340fa42de5c6cc087fd4f258f9f50254a7a8f
Matt Ettus <matt@ettus.com>
2009-09-02 17:23:12 -0700

    cascadefifo.v wasn't used, only the double cascade version.  fifo_2clock.v and fifo_2clock.v are empty

--- usrp2/fpga/control_lib/fifo_tb.v
+++ usrp2/fpga/control_lib/fifo_tb.v
@@ -2,11 +2,11 @@ module fifo_tb();
    
    reg clk, rst;
    wire short_full, short_empty, long_full, long_empty;
-   wire casc_full, casc_empty, casc2_full, casc2_empty;
+   wire casc2_full, casc2_empty;
    reg 	read, write;
    
    wire [7:0] short_do, long_do;
-   wire [7:0] casc_do, casc2_do;
+   wire [7:0] casc2_do;
    reg [7:0]  di;
 
    reg 	      clear = 0;
@@ -18,10 +18,6 @@ module fifo_tb();
    longfifo #(.WIDTH(8), .SIZE(4)) longfifo
      (.clk(clk),.rst(rst),.datain(di),.dataout(long_do),.clear(clear),
       .read(read),.write(write),.full(long_full),.empty(long_empty));
-   
-   cascadefifo #(.WIDTH(8), .SIZE(4)) cascadefifo
-     (.clk(clk),.rst(rst),.datain(di),.dataout(casc_do),.clear(clear),
-      .read(read),.write(write),.full(casc_full),.empty(casc_empty));
    
    cascadefifo2 #(.WIDTH(8), .SIZE(4)) cascadefifo2
      (.clk(clk),.rst(rst),.datain(di),.dataout(casc2_do),.clear(clear),
