2085083d68354bee8cfdc55eb4677f9b76541948
Matt Ettus <matt@ettus.com>
2010-01-15 12:05:43 -0800

    add debug pins to find the problem with lost eof in the udp core

--- usrp2/fpga/udp/udp_wrapper.v
+++ usrp2/fpga/udp/udp_wrapper.v
@@ -77,7 +77,7 @@ module udp_wrapper
 
    assign debug = { { 1'b0, rx_f19_data[18:16], rx_f19_src_rdy_i, rx_f19_dst_rdy_o, rx_f36_src_rdy_o, rx_f36_dst_rdy_i },
 		    { 2'b0, rx_int1_src_rdy, rx_int1_dst_rdy, rx_int2_src_rdy, rx_int2_dst_rdy, rx_int3_src_rdy, rx_int3_dst_rdy},
-		    { 4'b0, rx_f36_data[35:32] },
-		    { 8'b0 } };
+		    { rx_int3_data[35:32],, rx_f36_data[35:32] },
+		    { 2'b0, rx_int1_data[18:16], rx_int2_data[18:16] } };
    
 endmodule // udp_wrapper
