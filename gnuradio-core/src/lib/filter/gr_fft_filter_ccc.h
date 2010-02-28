72c47024f4b4677f116222a0adfadeedf180122c
Tom Rondeau <trondeau@vt.edu>
2010-02-28 17:37:48 -0500

    New fft filter base class to handle complex data types. Passes make check.

--- gnuradio-core/src/lib/filter/gr_fft_filter_ccc.h
+++ gnuradio-core/src/lib/filter/gr_fft_filter_ccc.h
@@ -28,8 +28,7 @@ class gr_fft_filter_ccc;
 typedef boost::shared_ptr<gr_fft_filter_ccc> gr_fft_filter_ccc_sptr;
 gr_fft_filter_ccc_sptr gr_make_fft_filter_ccc (int decimation, const std::vector<gr_complex> &taps);
 
-class gr_fir_ccc;
-class gri_fft_complex;
+class gri_fft_filter_ccc;
 
 /*!
  * \brief Fast FFT filter with gr_complex input, gr_complex output and gr_complex taps
@@ -40,15 +39,10 @@ class gr_fft_filter_ccc : public gr_sync_decimator
  private:
   friend gr_fft_filter_ccc_sptr gr_make_fft_filter_ccc (int decimation, const std::vector<gr_complex> &taps);
 
-  int			   d_ntaps;
   int			   d_nsamples;
-  int			   d_fftsize;		// fftsize = ntaps + nsamples - 1
-  gri_fft_complex	  *d_fwdfft;		// forward "plan"
-  gri_fft_complex	  *d_invfft;		// inverse "plan"
-  std::vector<gr_complex>  d_tail;		// state carried between blocks for overlap-add
-  std::vector<gr_complex>  d_xformed_taps;	// Fourier xformed taps
-  std::vector<gr_complex>  d_new_taps;
   bool			   d_updated;
+  gri_fft_filter_ccc	  *d_filter;  
+  std::vector<gr_complex>  d_new_taps;
 
   /*!
    * Construct a FFT filter with the given taps
@@ -57,10 +51,6 @@ class gr_fft_filter_ccc : public gr_sync_decimator
    * \param taps        complex filter taps
    */
   gr_fft_filter_ccc (int decimation, const std::vector<gr_complex> &taps);
-
-  void compute_sizes(int ntaps);
-  int tailsize() const { return d_ntaps - 1; }
-  void actual_set_taps (const std::vector<gr_complex> &taps);
 
  public:
   ~gr_fft_filter_ccc ();
---
+++ gnuradio-core/src/lib/filter/gri_fft_filter_ccc.h
@@ -0,0 +1,57 @@
+/* -*- c++ -*- */
+/*
+ * Copyright 2010 Free Software Foundation, Inc.
+ * 
+ * This file is part of GNU Radio
+ * 
+ * GNU Radio is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ * 
+ * GNU Radio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Radio; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef INCLUDED_GRI_FFT_FILTER_CCC_H
+#define INCLUDED_GRI_FFT_FILTER_CCC_H
+
+#include <gr_complex.h>
+#include <vector>
+
+class gri_fft_complex;
+
+class gri_fft_filter_ccc
+{
+ private:
+  int			   d_ntaps;
+  int			   d_nsamples;
+  int			   d_fftsize;		// fftsize = ntaps + nsamples - 1
+  int                      d_decimation;
+  gri_fft_complex	  *d_fwdfft;		// forward "plan"
+  gri_fft_complex	  *d_invfft;		// inverse "plan"
+  std::vector<gr_complex>  d_tail;		// state carried between blocks for overlap-add
+  std::vector<gr_complex>  d_xformed_taps;	// Fourier xformed taps
+  std::vector<gr_complex>  d_new_taps;
+
+  void compute_sizes(int ntaps);
+  int tailsize() const { return d_ntaps - 1; }
+  
+ public:
+  gri_fft_filter_ccc (int decimation, const std::vector<gr_complex> &taps);
+  ~gri_fft_filter_ccc ();
+
+  int set_taps (const std::vector<gr_complex> &taps);
+  
+  int filter (int nitems, const gr_complex *input, gr_complex *output);
+
+};
+
+#endif /* INCLUDED_GRI_FFT_FILTER_CCC_H */
