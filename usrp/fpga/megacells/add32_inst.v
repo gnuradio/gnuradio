a2c00f5cff7407ff10fc6c812d06fefe52c0b6a3
Johnathan Corgan <jcorgan@corganenterprises.com>
2010-02-28 12:47:43 -0800

    Remove usrp1 and usrp2 FPGA files.  These are now hosted at:
    git://ettus.sourcerepo.com/ettus/fpga.git
    
    ...under the 'usrp1' and 'usrp2' top-level directories.

--- usrp/fpga/megacells/add32_inst.v
+++
@@ -1,5 +0,0 @@
-add32	add32_inst (
-	.dataa ( dataa_sig ),
-	.datab ( datab_sig ),
-	.result ( result_sig )
-	);
--- usrp/fpga/megacells/sub32_inst.v
+++
@@ -1,8 +0,0 @@
-sub32	sub32_inst (
-	.dataa ( dataa_sig ),
-	.datab ( datab_sig ),
-	.clock ( clock_sig ),
-	.aclr ( aclr_sig ),
-	.clken ( clken_sig ),
-	.result ( result_sig )
-	);
