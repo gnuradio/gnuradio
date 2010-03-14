6de98961cf9b93c486c8c6825789de94dc79afae
Tom Rondeau <trondeau@vt.edu>
2010-03-14 18:37:23 -0400

    Fixing copyright of a couple of stragglers.

--- gnuradio-examples/python/digital/benchmark_qt_rx2.py
+++ gnuradio-examples/python/digital/benchmark_qt_rx2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright 2005,2006,2007,2009,2010 Free Software Foundation, Inc.
+# Copyright 2010 Free Software Foundation, Inc.
 # 
 # This file is part of GNU Radio
 # 
