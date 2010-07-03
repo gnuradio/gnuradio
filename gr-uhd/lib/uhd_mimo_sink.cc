4e636b2b4bb29d5224aeff4ae79af121020338f1
Josh Blum <josh@joshknows.com>
2010-07-02 18:30:31 -0700

    uhd mimo: created mimo source and sink block for gr-uhd

--- gr-uhd/lib/uhd_mimo_sink.cc
+++ gr-uhd/lib/uhd_mimo_sink.cc
@@ -20,7 +20,7 @@
  * Boston, MA 02110-1301, USA.
  */
 
-#include <uhd_simple_sink.h>
+#include <uhd_mimo_sink.h>
 #include <gr_io_signature.h>
 #include <stdexcept>
 #include "utils.h"
@@ -28,82 +28,75 @@
 /***********************************************************************
  * UHD Sink
  **********************************************************************/
-uhd_simple_sink::uhd_simple_sink(gr_io_signature_sptr sig)
-:gr_sync_block("uhd simple sink", sig, gr_make_io_signature(0, 0, 0)){
+uhd_mimo_sink::uhd_mimo_sink(gr_io_signature_sptr sig)
+:gr_sync_block("uhd mimo sink", sig, gr_make_io_signature(0, 0, 0)){
     /* NOP */
 }
 
 /***********************************************************************
  * UHD Sink Impl
  **********************************************************************/
-class uhd_simple_sink_impl : public uhd_simple_sink{
+class uhd_mimo_sink_impl : public uhd_mimo_sink{
 public:
-    uhd_simple_sink_impl(
+    uhd_mimo_sink_impl(
+        size_t num_channels,
         const std::string &args,
         const uhd::io_type_t &type
-    ) : uhd_simple_sink(gr_make_io_signature(1, 1, type.size)), _type(type)
+    ) : uhd_mimo_sink(gr_make_io_signature(num_channels, num_channels, type.size)), _type(type)
     {
-        _dev = uhd::usrp::simple_usrp::make(args);
+        _dev = uhd::usrp::mimo_usrp::make(args);
     }
 
-    ~uhd_simple_sink_impl(void){
+    ~uhd_mimo_sink_impl(void){
         //NOP
     }
 
-    void set_samp_rate(double rate){
-        _dev->set_tx_rate(rate);
-        do_samp_rate_error_message(rate, get_samp_rate());
+    void set_samp_rate_all(double rate){
+        _dev->set_tx_rate_all(rate);
+        do_samp_rate_error_message(rate, get_samp_rate_all());
     }
 
-    double get_samp_rate(void){
-        return _dev->get_tx_rate();
+    double get_samp_rate_all(void){
+        return _dev->get_tx_rate_all();
     }
 
-    uhd::tune_result_t set_center_freq(double freq){
-        return _dev->set_tx_freq(freq);
+    uhd::tune_result_t set_center_freq(size_t chan, double freq){
+        return _dev->set_tx_freq(chan, freq);
     }
 
-    uhd::freq_range_t get_freq_range(void){
-        return _dev->get_tx_freq_range();
+    uhd::freq_range_t get_freq_range(size_t chan){
+        return _dev->get_tx_freq_range(chan);
     }
 
-    void set_gain(float gain){
-        return _dev->set_tx_gain(gain);
+    void set_gain(size_t chan, float gain){
+        return _dev->set_tx_gain(chan, gain);
     }
 
-    float get_gain(void){
-        return _dev->get_tx_gain();
+    float get_gain(size_t chan){
+        return _dev->get_tx_gain(chan);
     }
 
-    uhd::gain_range_t get_gain_range(void){
-        return _dev->get_tx_gain_range();
+    uhd::gain_range_t get_gain_range(size_t chan){
+        return _dev->get_tx_gain_range(chan);
     }
 
-    void set_antenna(const std::string &ant){
-        return _dev->set_tx_antenna(ant);
+    void set_antenna(size_t chan, const std::string &ant){
+        return _dev->set_tx_antenna(chan, ant);
     }
 
-    std::string get_antenna(void){
-        return _dev->get_tx_antenna();
+    std::string get_antenna(size_t chan){
+        return _dev->get_tx_antenna(chan);
     }
 
-    std::vector<std::string> get_antennas(void){
-        return _dev->get_tx_antennas();
-    }
-
-    void set_clock_config(const uhd::clock_config_t &clock_config){
-        return _dev->set_clock_config(clock_config);
-    }
-
-    void set_time_now(const uhd::time_spec_t &time_spec){
-        return _dev->set_time_now(time_spec);
+    std::vector<std::string> get_antennas(size_t chan){
+        return _dev->get_tx_antennas(chan);
     }
 
     void set_time_next_pps(const uhd::time_spec_t &time_spec){
         return _dev->set_time_next_pps(time_spec);
     }
 
-    uhd::usrp::simple_usrp::sptr get_device(void){
+    uhd::usrp::mimo_usrp::sptr get_device(void){
         return _dev;
     }
 
@@ -125,18 +118,19 @@ public:
     }
 
 protected:
-    uhd::usrp::simple_usrp::sptr _dev;
+    uhd::usrp::mimo_usrp::sptr _dev;
     const uhd::io_type_t _type;
 };
 
 /***********************************************************************
  * Make UHD Sink
  **********************************************************************/
-boost::shared_ptr<uhd_simple_sink> uhd_make_simple_sink(
+boost::shared_ptr<uhd_mimo_sink> uhd_make_mimo_sink(
+    size_t num_channels,
     const std::string &args,
     const uhd::io_type_t::tid_t &type
 ){
-    return boost::shared_ptr<uhd_simple_sink>(
-        new uhd_simple_sink_impl(args, type)
+    return boost::shared_ptr<uhd_mimo_sink>(
+        new uhd_mimo_sink_impl(num_channels, args, type)
     );
 }
---
+++ gr-uhd/lib/uhd_mimo_source.h
@@ -0,0 +1,129 @@
+/* -*- c++ -*- */
+/*
+ * Copyright 2010 Free Software Foundation, Inc.
+ * 
+ * This file is part of GNU Radio
+ * 
+ * GNU Radio is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ * 
+ * GNU Radio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Radio; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef INCLUDED_UHD_MIMO_SOURCE_H
+#define INCLUDED_UHD_MIMO_SOURCE_H
+
+#include <gr_sync_block.h>
+#include <uhd/usrp/mimo_usrp.hpp>
+
+class uhd_mimo_source;
+
+boost::shared_ptr<uhd_mimo_source> uhd_make_mimo_source(
+    size_t num_channels,
+    const std::string &args,
+    const uhd::io_type_t::tid_t &type
+);
+
+class uhd_mimo_source : public gr_sync_block{
+public:
+
+    /*!
+     * Set the IO signature for this block.
+     * \param sig the output signature
+     */
+    uhd_mimo_source(gr_io_signature_sptr sig);
+
+    /*!
+     * Set the sample rate for the usrp device (across all mboards).
+     * \param rate a new rate in Sps
+     */
+    virtual void set_samp_rate_all(double rate) = 0;
+
+    /*!
+     * Get the sample rate for the usrp device (across all mboards).
+     * This is the actual sample rate and may differ from the rate set.
+     * \return the actual rate in Sps
+     */
+    virtual double get_samp_rate_all(void) = 0;
+
+    /*!
+     * Tune the usrp device to the desired center frequency.
+     * \param chan the channel number from 0 to N-1
+     * \param freq the desired frequency in Hz
+     * \return a tune result with the actual frequencies
+     */
+    virtual uhd::tune_result_t set_center_freq(size_t chan, double freq) = 0;
+
+    /*!
+     * Get the tunable frequency range.
+     * \param chan the channel number from 0 to N-1
+     * \return the frequency range in Hz
+     */
+    virtual uhd::freq_range_t get_freq_range(size_t chan) = 0;
+
+    /*!
+     * Set the gain for the dboard.
+     * \param chan the channel number from 0 to N-1
+     * \param gain the gain in dB
+     */
+    virtual void set_gain(size_t chan, float gain) = 0;
+
+    /*!
+     * Get the actual dboard gain setting.
+     * \param chan the channel number from 0 to N-1
+     * \return the actual gain in dB
+     */
+    virtual float get_gain(size_t chan) = 0;
+
+    /*!
+     * Get the settable gain range.
+     * \param chan the channel number from 0 to N-1
+     * \return the gain range in dB
+     */
+    virtual uhd::gain_range_t get_gain_range(size_t chan) = 0;
+
+    /*!
+     * Set the antenna to use.
+     * \param chan the channel number from 0 to N-1
+     * \param ant the antenna string
+     */
+    virtual void set_antenna(size_t chan, const std::string &ant) = 0;
+
+    /*!
+     * Get the antenna in use.
+     * \param chan the channel number from 0 to N-1
+     * \return the antenna string
+     */
+    virtual std::string get_antenna(size_t chan) = 0;
+
+    /*!
+     * Get a list of possible antennas.
+     * \param chan the channel number from 0 to N-1
+     * \return a vector of antenna strings
+     */
+    virtual std::vector<std::string> get_antennas(size_t chan) = 0;
+
+    /*!
+     * Set the time registers at the next pps (across all mboards).
+     * \param time_spec the new time
+     */
+    virtual void set_time_next_pps(const uhd::time_spec_t &time_spec) = 0;
+
+    /*!
+     * Get access to the underlying uhd device object.
+     * \return the mimo usrp device object
+     */
+    virtual uhd::usrp::mimo_usrp::sptr get_device(void) = 0;
+};
+
+#endif /* INCLUDED_UHD_MIMO_SOURCE_H */
