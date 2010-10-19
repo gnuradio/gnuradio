d3d187f1beb6525977079a6f9c822f971cced8da
Rob Savoye <rob@welcomehome.org>
2010-10-18 18:21:09 -0600

    improve comment

--- gnuradio-core/src/lib/swig/gnuradio_swig_guile.scm
+++ gnuradio-core/src/lib/swig/gnuradio_swig_guile.scm
@@ -18,8 +18,8 @@
 ;;; 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 ;;;
 
-;; Only use these if load-* fails t find a .scm or .so. If you can't
-;; find a .so. try running ldconfig. guile seems to only load modules 
+;; Only use these if load-* fails to find a .scm or .so. If you can't
+;; find a .so, try running ldconfig. guile seems to only load modules 
 ;; that are in the ldconfig paths.
 ;; (set! %load-path (append %load-path '("/usr/lib/guile/gnuradio")))
 ;; (set! %load-path (append %load-path '("/usr/share/gnuradio")))
@@ -28,7 +28,7 @@
 (load "gnuradio/gnuradio_swig_py_io.scm")
 (load "gnuradio/gnuradio_swig_py_runtime.scm")
 
-;; FIXME: these don't load for sme reason
+;; FIXME: these don't load for sme reason cause of issues with the .so files
 ;; (load "gnuradio/gnuradio_swig_py_general.scm")
 ;; (load "gnuradio/gnuradio_swig_py_gengen.scm")
 ;; (load "gnuradio/gnuradio_swig_py_heir.scm")
