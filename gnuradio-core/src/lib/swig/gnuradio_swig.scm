827f95ce49fadfaeec1ee09185d1502074eb0bbb
Eric Blossom <eb@comsec.com>
2010-10-22 13:56:05 -0700

    Add stub for gnuradio_swig.scm to get make to work

---
+++ gnuradio-core/src/lib/swig/gnuradio_swig.scm
@@ -0,0 +1,13 @@
+;;;
+;;; Not sure that we need this for guile or not.
+;;; We'll need to assemble the (gnuradio gr) module somewhere...
+;;;
+
+;; # This file implements the old gnuradio_swig_python namespace
+;;
+;; from gnuradio_swig_py_runtime import *
+;; from gnuradio_swig_py_general import *
+;; from gnuradio_swig_py_gengen import *
+;; from gnuradio_swig_py_filter import *
+;; from gnuradio_swig_py_io import *
+;; from gnuradio_swig_py_hier import *
