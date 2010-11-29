71c2792b3cc59e82dd8e0ac8e7359ac474ae7d69
Rob Savoye <rob@welcomehome.org>
2010-11-29 11:39:33 -0700

    test for the proper results in the scheme code too

--- gr-run-waveform/guile/simple.scm
+++ gr-run-waveform/guile/simple.scm
@@ -11,12 +11,26 @@ path-with-xyzzy
 (define extensions '(".scm" ""))
 
 ;; Both of these return "/usr/share/guile/1.8/ice-9/boot-9.scm"
+(define expected "/usr/share/guile/1.8/ice-9/boot-9.scm")
 (define result1 (search-path path filename extensions))
+(if (string=? result1 expected)
+    (display "PASSED: xyzzy-search-path from guile\n")
+    (display "FAILED: xyzzy-search-path from guile\n"))
 
 (define result2 (search-path path-with-xyzzy filename extensions))
+(if (string=? result2 expected)
+    (display "PASSED: xyzzy-search-path from guile\n")
+    (display "FAILED: xyzzy-search-path from guile\n"))
 
 ;; Should return "/usr/share/guile/1.8/ice-9/boot-9.scm"
 (define result3 (xyzzy-search-path path filename extensions))
+(if (string=? result3 expected)
+    (display "PASSED: xyzzy-search-path from guile\n")
+    (display "FAILED: xyzzy-search-path from guile\n"))
 
 ;; Should return "/-xyzzy-/ice-9/boot-9.scm"
+(define expected "/-xyzzy-/ice-9/boot-9.scm")
 (define result4 (xyzzy-search-path path-with-xyzzy filename extensions))
+(if (string=? result4 expected)
+    (display "PASSED: xyzzy-search-path from guile\n")
+    (display "FAILED: xyzzy-search-path from guile\n"))
