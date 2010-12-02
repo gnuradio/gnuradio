09dc1cd6928d1c32cf06509a8b81202e11bcf3a4
Eric Blossom <eb@comsec.com>
2010-12-02 14:18:41 -0800

    Update copyright dates

--- gr-run-waveform/xyzzy-load.c
+++ gr-run-waveform/xyzzy-load.c
@@ -1,9 +1,9 @@
-/* Copyright (C) 1995,1996,1998,1999,2000,2001, 2004, 2006 Free Software Foundation, Inc.
+/* Copyright (C) 1995,1996,1998,1999,2000,2001,2004,2006,2010 Free Software Foundation, Inc.
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * version 3 of the License, or (at your option) any later version.
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
