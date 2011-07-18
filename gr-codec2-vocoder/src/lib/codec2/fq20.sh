2efdfbb5cc8b9c76dec861fea8396a8f011df1c2
Johnathan Corgan <jcorgan@corganenterprises.com>
2011-07-18 16:39:20 -0700

    gr-vocoder: removed gr-codec2-vocoder

--- gr-codec2-vocoder/src/lib/codec2/fq20.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# fq20.shsh
-# David Rowe 27 July 2010
-# 
-# Decode a file with fully quantised codec at 20ms frame rate
-
-../src/sinedec ../raw/$1.raw $1.mdl -o $1_phase0_lsp_20_EWo2.raw --phase 0 --lpc 10 --lsp --postfilter --dec
-
--- gr-codec2-vocoder/src/lib/codec2/listensim.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# listensim.sh
-# David Rowe 10 Sep 2009
-#
-# Listen to files processed with sim.sh
-
-../script/menu.sh ../raw/$1.raw $1_uq.raw $1_phase0.raw $1_lpc10.raw $1_phase0_lpc10.raw $1_phase0_lpc10_dec.raw $1_phase0_lsp_dec.raw $2 $3
-
-
