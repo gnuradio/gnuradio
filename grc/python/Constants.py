413964379c19fce5c2c0ad25d1de968a68744f4d
Josh Blum <josh@joshknows.com>
2011-10-08 10:23:00 -0700

    grc: added new IO types
    Added all complex/real float/integer types.
    Used volk naming convention: fc32, etc...
    
    The port type checking now relies on IO size,
    therefore a short vector of length 2 can connect to a complex short,
    a float can connect to an int.
    Basically, the same size checking done in gnuradio runtime.

--- grc/python/Constants.py
+++ grc/python/Constants.py
@@ -45,6 +45,38 @@ FLOW_GRAPH_TEMPLATE = os.path.join(DATA_DIR, 'flow_graph.tmpl')
 BLOCK_DTD = os.path.join(DATA_DIR, 'block.dtd')
 DEFAULT_FLOW_GRAPH = os.path.join(DATA_DIR, 'default_flow_graph.grc')
 
+CORE_TYPES = ( #name, key, sizeof, color
+	('Complex Float 64', 'fc64', 16, '#72f313'),
+	('Complex Float 32', 'fc32', 8, '#3399FF'),
+	('Complex Integer 32', 'sc32', 8, '#00b789'),
+	('Complex Integer 16', 'sc16', 4, '#f37913'),
+	('Complex Integer 8', 'sc8', 2, '#ff0e7f'),
+	('Float 64', 'f64', 8, '#86a8fa'),
+	('Float 32', 'f32', 4, '#FF8C69'),
+	('Integer 32', 's32', 4, '#00FF99'),
+	('Integer 16', 's16', 2, '#FFFF66'),
+	('Integer 8', 's8', 1, '#FF66FF'),
+	('Message Queue', 'msg', 0, '#777777'),
+	('Wildcard', '', 0, '#FFFFFF'),
+)
+
+ALIAS_TYPES = {
+	'complex' : (8, '#3399FF'),
+	'float'   : (4, '#FF8C69'),
+	'int'     : (4, '#00FF99'),
+	'short'   : (2, '#FFFF66'),
+	'byte'    : (1, '#FF66FF'),
+}
+
+TYPE_TO_COLOR = dict()
+TYPE_TO_SIZEOF = dict()
+for name, key, sizeof, color in CORE_TYPES:
+	TYPE_TO_COLOR[key] = color
+	TYPE_TO_SIZEOF[key] = sizeof
+for key, (sizeof, color) in ALIAS_TYPES.iteritems():
+	TYPE_TO_COLOR[key] = color
+	TYPE_TO_SIZEOF[key] = sizeof
+
 #coloring
 COMPLEX_COLOR_SPEC = '#3399FF'
 FLOAT_COLOR_SPEC = '#FF8C69'
