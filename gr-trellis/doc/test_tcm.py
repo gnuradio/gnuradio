ad47d75bc010608d94abce0ba809e012a0f4b7a4
Tom Rondeau <trondeau@vt.edu>
2011-10-25 18:31:04 -0400

    trellis: fixed doc examples to use gr-digital (test_viterbi_equalization1.py bombs out on something else, now).

--- gr-trellis/doc/test_tcm.py
+++ gr-trellis/doc/test_tcm.py
@@ -2,7 +2,7 @@
 
 from gnuradio import gr
 from gnuradio import audio
-from gnuradio import trellis
+from gnuradio import trellis, digital
 from gnuradio import eng_notation
 import math
 import sys
@@ -24,7 +24,7 @@ def run_test (f,Kb,bitspersymbol,K,dimensionality,constellation,N0,seed):
     noise = gr.noise_source_f(gr.GR_GAUSSIAN,math.sqrt(N0/2),seed)
 
     # RX
-    metrics = trellis.metrics_f(f.O(),dimensionality,constellation,trellis.TRELLIS_EUCLIDEAN) # data preprocessing to generate metrics for Viterbi
+    metrics = trellis.metrics_f(f.O(),dimensionality,constellation,digital.TRELLIS_EUCLIDEAN) # data preprocessing to generate metrics for Viterbi
     va = trellis.viterbi_s(f,K,0,-1) # Put -1 if the Initial/Final states are not set.
     fsmi2s = gr.unpacked_to_packed_ss(bitspersymbol,gr.GR_MSB_FIRST) # pack FSM input symbols to shorts
     dst = gr.check_lfsr_32k_s(); 
