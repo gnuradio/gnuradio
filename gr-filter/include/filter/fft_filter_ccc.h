2c80be2c6e3aadcc96046c2e99891c54f61b027c
Tom Rondeau <trondeau@vt.edu>
2012-05-03 13:33:54 -0400

    filter: improved documentation for fft_filters.

--- gr-filter/include/filter/fft_filter_ccc.h
+++ gr-filter/include/filter/fft_filter_ccc.h
@@ -39,6 +39,25 @@ namespace gr {
        * \brief Fast FFT filter with gr_complex input, gr_complex output and gr_complex taps
        * \ingroup filter_blk
        *
+       * This block implements a complex decimating filter using the
+       * fast convolution method via an FFT. The decimation factor is
+       * an interger that is greater than or equal to 1.
+       *
+       * The filter takes a set of complex (or real) taps to use in
+       * the filtering operation. These taps can be defined as
+       * anything that satisfies the user's filtering needs. For
+       * standard filters such as lowpass, highpass, bandpass, etc.,
+       * the filter.firdes and filter.optfir classes provide
+       * convenient generating methods.
+       *
+       * This filter is implemented by using the FFTW package to
+       * perform the required FFTs. An optional argument, nthreads,
+       * may be passed to the constructor (or set using the
+       * set_nthreads member function) to split the FFT among N number
+       * of threads. This can improve performance on very large FFTs
+       * (that is, if the number of taps used is very large) if you
+       * have enough threads/cores to support it.
+       * 
        * \param decimation  >= 1
        * \param taps        complex filter taps
        * \param nthreads    number of threads for the FFT to use
