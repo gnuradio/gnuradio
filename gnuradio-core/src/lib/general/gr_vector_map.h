d4fe4377e72165d88fdf00f6c8d5a5b986b25955
Tom Rondeau <trondeau@vt.edu>
2012-05-21 12:48:33 -0400

    core: minor formatting changes to vector_map for style consistency.

--- gnuradio-core/src/lib/general/gr_vector_map.h
+++ gnuradio-core/src/lib/general/gr_vector_map.h
@@ -33,43 +33,49 @@ typedef boost::shared_ptr<gr_vector_map> gr_vector_map_sptr;
 
 GR_CORE_API gr_vector_map_sptr
 gr_make_vector_map (size_t item_size, std::vector<size_t> in_vlens,
-					std::vector< std::vector< std::vector<size_t> > > mapping);
+		    std::vector< std::vector< std::vector<size_t> > > mapping);
 
 /*!
  * \brief Maps elements from a set of input vectors to a set of output vectors.
  *
- * If in[i] is the input vector in the i'th stream then the output vector in the j'th stream is:
+ * If in[i] is the input vector in the i'th stream then the output
+ * vector in the j'th stream is:
+ *
  * out[j][k] = in[mapping[j][k][0]][mapping[j][k][1]]
- * That is mapping is of the form (out_stream1_mapping, out_stream2_mapping, ...)
- * and out_stream1_mapping is of the form (element1_mapping, element2_mapping, ...)
- * and element1_mapping is of the form (in_stream, in_element).
+ *
+ * That is mapping is of the form (out_stream1_mapping,
+ * out_stream2_mapping, ...)  and out_stream1_mapping is of the form
+ * (element1_mapping, element2_mapping, ...)  and element1_mapping is
+ * of the form (in_stream, in_element).
  *
  * \param item_size (integer) size of vector elements
  *
- * \param in_vlens (vector of integers) number of elements in each input vector
+ * \param in_vlens (vector of integers) number of elements in each
+ *                 input vector
  *
- * \param mapping (vector of vectors of vectors of integers) how to map elements from input to output vectors
+ * \param mapping (vector of vectors of vectors of integers) how to
+ *                map elements from input to output vectors
  *
  * \ingroup slicedice_blk
  */
 class GR_CORE_API gr_vector_map : public gr_sync_block
 {
   friend GR_CORE_API gr_vector_map_sptr
-    gr_make_vector_map (size_t item_size, std::vector<size_t> in_vlens,
-						std::vector< std::vector< std::vector<size_t> > > mapping);
+    gr_make_vector_map(size_t item_size, std::vector<size_t> in_vlens,
+		       std::vector< std::vector< std::vector<size_t> > > mapping);
   size_t d_item_size;
   std::vector<size_t> d_in_vlens;
   std::vector< std::vector< std::vector<size_t> > > d_mapping;
   gruel::mutex d_mutex; // mutex to protect set/work access
 
  protected:
-  gr_vector_map (size_t item_size, std::vector<size_t> in_vlens,
-				 std::vector< std::vector< std::vector<size_t> > > mapping);
+  gr_vector_map(size_t item_size, std::vector<size_t> in_vlens,
+		std::vector< std::vector< std::vector<size_t> > > mapping);
 
  public:
-  int work (int noutput_items,
-			gr_vector_const_void_star &input_items,
-			gr_vector_void_star &output_items);
+  int work(int noutput_items,
+	   gr_vector_const_void_star &input_items,
+	   gr_vector_void_star &output_items);
 
   void set_mapping(std::vector< std::vector< std::vector<size_t> > > mapping);
 };
