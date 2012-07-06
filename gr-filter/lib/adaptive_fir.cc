5f103b0a99d5f31e0dc55693242b887b8186eac3
Tom Rondeau <trondeau@vt.edu>
2012-07-06 18:08:19 -0400

    filter: added kernel for adaptive fir filter.
    Note: still need to update the blocks to use this new kernel.

---
+++ gr-filter/lib/adaptive_fir.cc
@@ -0,0 +1,86 @@
+/* -*- c++ -*- */
+/*
+ * Copyright 2011,2012 Free Software Foundation, Inc.
+ *
+ * This file is part of GNU Radio
+ *
+ * GNU Radio is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * GNU Radio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Radio; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <filter/adaptive_fir.h>
+#include <gr_io_signature.h>
+
+namespace gr {
+  namespace filter {
+    namespace kernel {
+
+      adaptive_fir_ccc::adaptive_fir_ccc(int decimation,
+					 const std::vector<gr_complex> &taps)
+	: kernel::fir_filter_ccc(decimation, taps)
+      {
+      }
+
+      adaptive_fir_ccc::~adaptive_fir_ccc()
+      {
+      }
+
+      void
+      adaptive_fir_ccc::set_taps(const std::vector<gr_complex> &taps)
+      {
+	d_new_taps = taps;
+	d_updated = true;
+      }
+      
+      std::vector<gr_complex>
+      adaptive_fir_ccc::taps() const
+      {
+	return kernel::fir_filter_ccc::taps();
+      }   
+
+
+      /**************************************************************/
+
+
+      adaptive_fir_ccf::adaptive_fir_ccf(int decimation,
+					 const std::vector<float> &taps)
+	: kernel::fir_filter_ccf(decimation, taps)
+      {
+      }
+
+      adaptive_fir_ccf::~adaptive_fir_ccf()
+      {
+      }
+
+      void
+      adaptive_fir_ccf::set_taps(const std::vector<float> &taps)
+      {
+	d_new_taps = taps;
+	d_updated = true;
+      }
+      
+      std::vector<float>
+      adaptive_fir_ccf::taps() const
+      {
+	return kernel::fir_filter_ccf::taps();
+      }   
+
+    } /* namespace kernel */
+  } /* namespace filter */
+} /* namespace gr */
