2faee9d759397d11ea4ecc0b7dbe7246c26e059e
Tom Rondeau <trondeau@vt.edu>
2012-07-16 21:27:44 -0400

    wxgui: update wxgui components to use gr-filter component.

--- gr-wxgui/src/python/fftsink_nongl.py
+++ gr-wxgui/src/python/fftsink_nongl.py
@@ -20,7 +20,7 @@
 # Boston, MA 02110-1301, USA.
 #
 
-from gnuradio import gr, gru, window
+from gnuradio import gr, gru, window, filter
 from gnuradio.wxgui import stdgui2
 import wx
 import plot
@@ -128,7 +128,7 @@ class fft_sink_f(gr.hier_block2, fft_sink_base):
             power += tap*tap
 
         self.c2mag = gr.complex_to_mag(self.fft_size)
-        self.avg = gr.single_pole_iir_filter_ff(1.0, self.fft_size)
+        self.avg = filter.single_pole_iir_filter_ff(1.0, self.fft_size)
 
         # FIXME  We need to add 3dB to all bins but the DC bin
         self.log = gr.nlog10_ff(20, self.fft_size,
@@ -173,7 +173,7 @@ class fft_sink_c(gr.hier_block2, fft_sink_base):
             power += tap*tap
 
         self.c2mag = gr.complex_to_mag(self.fft_size)
-        self.avg = gr.single_pole_iir_filter_ff(1.0, self.fft_size)
+        self.avg = filter.single_pole_iir_filter_ff(1.0, self.fft_size)
 
         # FIXME  We need to add 3dB to all bins but the DC bin
         self.log = gr.nlog10_ff(20, self.fft_size,
--- gr-wxgui/src/python/waterfallsink_nongl.py
+++ gr-wxgui/src/python/waterfallsink_nongl.py
@@ -20,7 +20,7 @@
 # Boston, MA 02110-1301, USA.
 #
 
-from gnuradio import gr, gru, window
+from gnuradio import gr, gru, window, filter
 from gnuradio.wxgui import stdgui2
 import wx
 import gnuradio.wxgui.plot as plot
@@ -93,7 +93,7 @@ class waterfall_sink_f(gr.hier_block2, waterfall_sink_base):
         mywindow = window.blackmanharris(self.fft_size)
         self.fft = gr.fft_vfc(self.fft_size, True, mywindow)
         self.c2mag = gr.complex_to_mag(self.fft_size)
-        self.avg = gr.single_pole_iir_filter_ff(1.0, self.fft_size)
+        self.avg = filter.single_pole_iir_filter_ff(1.0, self.fft_size)
         self.log = gr.nlog10_ff(20, self.fft_size, -20*math.log10(self.fft_size))
         self.sink = gr.message_sink(gr.sizeof_float * self.fft_size, self.msgq, True)
 	self.connect(self, self.s2p, self.one_in_n, self.fft, self.c2mag, self.avg, self.log, self.sink)
@@ -124,7 +124,7 @@ class waterfall_sink_c(gr.hier_block2, waterfall_sink_base):
         mywindow = window.blackmanharris(self.fft_size)
         self.fft = gr.fft_vcc(self.fft_size, True, mywindow)
         self.c2mag = gr.complex_to_mag(self.fft_size)
-        self.avg = gr.single_pole_iir_filter_ff(1.0, self.fft_size)
+        self.avg = filter.single_pole_iir_filter_ff(1.0, self.fft_size)
         self.log = gr.nlog10_ff(20, self.fft_size, -20*math.log10(self.fft_size))
         self.sink = gr.message_sink(gr.sizeof_float * self.fft_size, self.msgq, True)
 	self.connect(self, self.s2p, self.one_in_n, self.fft, self.c2mag, self.avg, self.log, self.sink)
