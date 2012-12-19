256b438259469ff9ab58572c73f6444c79c24f1c
Tom Rondeau <trondeau@vt.edu>
2012-12-19 14:38:22 -0500

    QA: adding a test to make sure the set_processor_affinity calls work and top_block starts correctly.

--- gnuradio-core/src/lib/runtime/qa_gr_top_block.h
+++ gnuradio-core/src/lib/runtime/qa_gr_top_block.h
@@ -38,6 +38,11 @@ class qa_gr_top_block : public CppUnit::TestCase
   CPPUNIT_TEST(t4_reconfigure);  // triggers 'join never returns' bug
   CPPUNIT_TEST(t5_max_noutputs);
   CPPUNIT_TEST(t6_reconfig_max_noutputs);
+  CPPUNIT_TEST(t7_max_noutputs_per_block);
+  CPPUNIT_TEST(t8_reconfig_max_noutputs_per_block);
+  CPPUNIT_TEST(t9_max_output_buffer);
+  CPPUNIT_TEST(t10_reconfig_max_output_buffer);
+  CPPUNIT_TEST(t11_set_block_affinity);
 
   CPPUNIT_TEST_SUITE_END();
 
@@ -54,6 +59,8 @@ private:
   void t8_reconfig_max_noutputs_per_block();
   void t9_max_output_buffer();
   void t10_reconfig_max_output_buffer();
+  void t11_set_block_affinity();
+
 };
 
 #endif /* INCLUDED_QA_GR_TOP_BLOCK_H */
