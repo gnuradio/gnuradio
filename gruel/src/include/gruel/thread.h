7bc415bd2875132ce321c5913950d23e7c9ad8b1
Tom Rondeau <trondeau@vt.edu>
2012-12-19 13:37:39 -0500

    gruel: adding wrapper layer for gruel threads to enable setting thread affinity.
    Working in Linux; programmed for Windows (but untested); nops for OSX.

--- gruel/src/include/gruel/thread.h
+++ gruel/src/include/gruel/thread.h
@@ -1,6 +1,6 @@
 /* -*- c++ -*- */
 /*
- * Copyright 2009,2010,2011 Free Software Foundation, Inc.
+ * Copyright 2009-2012 Free Software Foundation, Inc.
  *
  * This file is part of GNU Radio
  *
@@ -26,6 +26,7 @@
 #include <boost/thread/mutex.hpp>
 #include <boost/thread/locks.hpp>
 #include <boost/thread/condition_variable.hpp>
+#include <vector>
 
 namespace gruel {
 
@@ -33,6 +34,22 @@ namespace gruel {
   typedef boost::mutex                     mutex;
   typedef boost::mutex::scoped_lock        scoped_lock;
   typedef boost::condition_variable        condition_variable;
+
+#if defined(_WIN32) || defined(__WIN32__) || defined(WIN32)
+  typedef HANDLE gr_thread_t;
+#elif defined(macintosh) || defined(__APPLE__) || defined(__APPLE_CC__)
+  typedef pthread_t gr_thread_t;
+#else
+  typedef pthread_t gr_thread_t;
+#endif
+
+  GRUEL_API gr_thread_t get_current_thread_id();
+  GRUEL_API void thread_bind_to_processor(const std::vector<unsigned int> &mask);
+  GRUEL_API void thread_bind_to_processor(unsigned int n);
+  GRUEL_API void thread_bind_to_processor(gr_thread_t thread, const std::vector<unsigned int> &mask);
+  GRUEL_API void thread_bind_to_processor(gr_thread_t thread, unsigned int n);
+  GRUEL_API void thread_unbind();
+  GRUEL_API void thread_unbind(gr_thread_t thread);
 
 } /* namespace gruel */
 
