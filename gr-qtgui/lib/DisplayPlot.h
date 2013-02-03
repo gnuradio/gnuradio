6062b4e0a4cdd13d971b5f3e89e005763ced0cdf
Tom Rondeau <trondeau@vt.edu>
2013-02-03 13:48:07 -0500

    qtgui: making sure all qtgui files have copyright notice.

--- gr-qtgui/lib/DisplayPlot.h
+++ gr-qtgui/lib/DisplayPlot.h
@@ -1,6 +1,6 @@
 /* -*- c++ -*- */
 /*
- * Copyright 2008,2009,2010,2011,2012 Free Software Foundation, Inc.
+ * Copyright 2008-2012 Free Software Foundation, Inc.
  *
  * This file is part of GNU Radio
  *
--- gr-qtgui/lib/time_raster_sink_c_impl.cc
+++ gr-qtgui/lib/time_raster_sink_c_impl.cc
@@ -1,6 +1,6 @@
 /* -*- c++ -*- */
 /*
- * Copyright 2012 Free Software Foundation, Inc.
+ * Copyright 2012,2013 Free Software Foundation, Inc.
  *
  * This file is part of GNU Radio
  *
