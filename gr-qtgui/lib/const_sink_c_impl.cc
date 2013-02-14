3a9a4b44f95d848e28bef679454f8d9e4bdc219b
Tom Rondeau <trondeau@vt.edu>
2013-02-14 16:29:22 -0500

    qtgui: allows setting of update rate in GRC for all QTGUI plotters.

--- gr-qtgui/lib/const_sink_c_impl.cc
+++ gr-qtgui/lib/const_sink_c_impl.cc
@@ -104,7 +104,6 @@ namespace gr {
       d_main_gui->setNPoints(d_size);
       // initialize update time to 10 times a second
       set_update_time(0.1);
-      d_last_time = 0;
     }
 
     void
@@ -146,6 +145,7 @@ namespace gr {
       gruel::high_res_timer_type tps = gruel::high_res_timer_tps();
       d_update_time = t * tps;
       d_main_gui->setUpdateTime(t);
+      d_last_time = 0;
     }
 
     void
--- gr-qtgui/lib/waterfall_sink_c_impl.cc
+++ gr-qtgui/lib/waterfall_sink_c_impl.cc
@@ -127,7 +127,6 @@ namespace gr {
 
       // initialize update time to 10 times a second
       set_update_time(0.1);
-      d_last_time = 0;
     }
 
     void
@@ -215,6 +214,7 @@ namespace gr {
       gruel::high_res_timer_type tps = gruel::high_res_timer_tps();
       d_update_time = t * tps;
       d_main_gui->setUpdateTime(t);
+      d_last_time = 0;
     }
 
     void
