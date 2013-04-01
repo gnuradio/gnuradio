f87c90fe3e2470007121b27018e08c5fe6bcd589
Tom Rondeau <trondeau@vt.edu>
2013-04-01 17:58:07 -0400

    runtime: fixing missing includes from previous merge conflict.

--- gnuradio-runtime/include/gr_logger.h
+++ gnuradio-runtime/include/gr_logger.h
@@ -39,6 +39,8 @@
 #include <gr_runtime_api.h>
 #include <assert.h>
 #include <iostream>
+#include <boost/filesystem.hpp>
+#include <boost/thread.hpp>
 #include <boost/format.hpp>
 
 #ifdef ENABLE_GR_LOG
