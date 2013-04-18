ba9066ef2213b61cf39f7aadb732451f14de5855
Tim O'Shea <tim.oshea753@gmail.com>
2013-04-18 18:22:54 -0400

    blocks: interleaved_short_to_complex switch to volk, allow vector short input option

--- gr-blocks/lib/interleaved_short_array_to_complex.cc
+++ gr-blocks/lib/interleaved_short_array_to_complex.cc
@@ -26,14 +26,12 @@
 
 #include "interleaved_short_array_to_complex.h"
 #include <assert.h>
+#include <volk/volk.h>
 
 void
 interleaved_short_array_to_complex (const short *in,
 				    gr_complex *out, int nsamples)
 {
   assert (nsamples % 2 == 0);
-
-  for (int i = 0; i < nsamples/2; i++){
-    out[i] = gr_complex (in[i*2 + 0], in[i*2 + 1]);
-  }
+  volk_16i_s32f_convert_32f_u( (float*)out, (const int16_t*) in, 1, nsamples );
 }
