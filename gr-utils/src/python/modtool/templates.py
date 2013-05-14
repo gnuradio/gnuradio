4850598303359963de1b84791f0d66b8f9c9ad9c
Tom Rondeau <trondeau@vt.edu>
2013-05-14 18:36:25 +0100

    modtool: fixed some setup for noblock gen.

--- gr-utils/src/python/modtool/templates.py
+++ gr-utils/src/python/modtool/templates.py
@@ -230,9 +230,10 @@ namespace gr {
      */
     class ${modname.upper()}_API $blockname
     {
+    public:
         ${blockname}(${arglist});
         ~${blockname}();
-        private:
+    private:
     };
 #else
     /*!
@@ -496,11 +497,15 @@ gr_modtool help <command> -- Shows the help for a given command. '''
 
 # SWIG string
 Templates['swig_block_magic'] = """#if $version == '36'
+#if $blocktype != 'noblock'
 GR_SWIG_BLOCK_MAGIC($modname, $blockname);
+#end if
 %include "${modname}_${blockname}.h"
 #else
 %include "${modname}/${blockname}.h"
+#if $blocktype != 'noblock'
 GR_SWIG_BLOCK_MAGIC2($modname, $blockname);
+#end if
 #end if
 """
 
