ad855d310f76b5954420e8bfa6c83b773220d60c
Jiri Pinkava <j-pi@seznam.cz>
2013-07-19 20:03:58 +0200

    build: require Python v2 for build configuration

--- cmake/Modules/GrSwig.cmake
+++ cmake/Modules/GrSwig.cmake
@@ -127,7 +127,7 @@ macro(GR_SWIG_MAKE name)
     endif()
 
     #append additional include directories
-    find_package(PythonLibs)
+    find_package(PythonLibs 2)
     list(APPEND GR_SWIG_INCLUDE_DIRS ${PYTHON_INCLUDE_PATH}) #deprecated name (now dirs)
     list(APPEND GR_SWIG_INCLUDE_DIRS ${PYTHON_INCLUDE_DIRS})
 
