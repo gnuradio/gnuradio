061bb4e56df7fcb093bb1fb0a48e3396f5b0d19e
Tom Rondeau <tom@trondeau.com>
2013-11-17 17:44:57 -0500

    filter: fixed missing copyright and import sys in examples

--- gr-filter/examples/fft_filter_ccc.py
+++ gr-filter/examples/fft_filter_ccc.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python
+#
+# Copyright 2013 Free Software Foundation, Inc.
+#
+# This file is part of GNU Radio
+#
+# GNU Radio is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+#
+# GNU Radio is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GNU Radio; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street,
+# Boston, MA 02110-1301, USA.
+#
 
 from gnuradio import gr, filter
 from gnuradio import analog
@@ -6,6 +26,7 @@ from gnuradio import blocks
 from gnuradio import eng_notation
 from gnuradio.eng_option import eng_option
 from optparse import OptionParser
+import sys
 
 try:
     import scipy
