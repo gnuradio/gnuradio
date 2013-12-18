b3b8a1f4965f8283f2c3d22ae45b569b2fe6d713
Tom Rondeau <tom@trondeau.com>
2013-12-18 11:24:07 -0500

    filter: fixed issue with resampler using buffer size information (always a mistake).
    The intent was to ask the scheduler for the maximum amount of data. Now, it plays it safe by asking for just enough the next time. Performance tests show it works well.

--- gr-filter/python/filter/qa_pfb_arb_resampler.py
+++ gr-filter/python/filter/qa_pfb_arb_resampler.py
@@ -95,7 +95,7 @@ class test_pfb_arb_resampler(gr_unittest.TestCase):
         snk = blocks.vector_sink_c()
         
         self.tb.connect(signal, pfb, snk)
-        self.tb.run() 
+        self.tb.run()
 
         Ntest = 50
         L = len(snk.data())
@@ -103,7 +103,44 @@ class test_pfb_arb_resampler(gr_unittest.TestCase):
         # Get group delay and estimate of phase offset from the filter itself.
         delay = pfb.group_delay()
         phase = pfb.phase_offset(freq, fs)
-        
+
+        # Create a timeline offset by the filter's group delay
+        t = map(lambda x: float(x)/(fs*rrate), xrange(delay, L+delay))
+
+        # Data of the sinusoid at frequency freq with the delay and phase offset.
+        expected_data = map(lambda x: math.cos(2.*math.pi*freq*x+phase) + \
+                                1j*math.sin(2.*math.pi*freq*x+phase), t)
+
+        dst_data = snk.data()
+
+        self.assertComplexTuplesAlmostEqual(expected_data[-Ntest:], dst_data[-Ntest:], 2)
+
+    def test_ccf_001(self):
+        N = 50000        # number of samples to use
+        fs = 5000.0      # baseband sampling rate
+        rrate = 0.75     # resampling rate
+
+        nfilts = 32
+        taps = filter.firdes.low_pass_2(nfilts, nfilts*fs, fs/4, fs/10,
+                                        attenuation_dB=80,
+                                        window=filter.firdes.WIN_BLACKMAN_hARRIS)
+
+        freq = 211.123
+        data = sig_source_c(fs, freq, 1, N)
+        signal = blocks.vector_source_c(data)
+        pfb = filter.pfb_arb_resampler_ccf(rrate, taps, nfilts)
+        snk = blocks.vector_sink_c()
+
+        self.tb.connect(signal, pfb, snk)
+        self.tb.run()
+
+        Ntest = 50
+        L = len(snk.data())
+
+        # Get group delay and estimate of phase offset from the filter itself.
+        delay = pfb.group_delay()
+        phase = pfb.phase_offset(freq, fs)
+
         # Create a timeline offset by the filter's group delay
         t = map(lambda x: float(x)/(fs*rrate), xrange(delay, L+delay))
 
