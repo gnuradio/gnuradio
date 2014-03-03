61b3bc11df93b59f7aaab3813d5c607f0e652790
Tom Rondeau <tom@trondeau.com>
2014-03-03 16:36:07 -0500

    filter: completing previous fix for CID 1189412 for new ccf filter on master.

--- gr-filter/lib/fft_filter.cc
+++ gr-filter/lib/fft_filter.cc
@@ -352,8 +352,8 @@ namespace gr {
       fft_filter_ccf::fft_filter_ccf(int decimation,
 				     const std::vector<float> &taps,
 				     int nthreads)
-	: d_fftsize(-1), d_decimation(decimation), d_fwdfft(0),
-	  d_invfft(0), d_nthreads(nthreads), d_xformed_taps(NULL)
+	: d_fftsize(-1), d_decimation(decimation), d_fwdfft(NULL),
+	  d_invfft(NULL), d_nthreads(nthreads), d_xformed_taps(NULL)
       {
 	set_taps(taps);
       }
