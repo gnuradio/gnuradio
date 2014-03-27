ba2951861b99f59df79022f98f51a12273fe2aa8
Tom Rondeau <tom@trondeau.com>
2014-03-27 14:29:03 -0700

    digital: correlate_access_code_tag d_mask was set improperly when access code len = 64.

--- gr-digital/lib/correlate_access_code_bb_impl.cc
+++ gr-digital/lib/correlate_access_code_bb_impl.cc
@@ -1,19 +1,19 @@
 /* -*- c++ -*- */
 /*
  * Copyright 2004,2006,2010-2012 Free Software Foundation, Inc.
- * 
+ *
  * This file is part of GNU Radio
- * 
+ *
  * GNU Radio is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3, or (at your option)
  * any later version.
- * 
+ *
  * GNU Radio is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with GNU Radio; see the file COPYING.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street,
@@ -97,7 +97,7 @@ namespace gr {
 	t |= ((d_data_reg >> 63) & 0x1) << 0;
 	t |= ((d_flag_reg >> 63) & 0x1) << 1;	// flag bit
 	out[i] = t;
-    
+
 	// compute hamming distance between desired access code and current data
 	unsigned long long wrong_bits = 0;
 	unsigned int nwrong = d_threshold+1;
@@ -128,6 +128,6 @@ namespace gr {
 
       return noutput_items;
     }
-  
+
   } /* namespace digital */
 } /* namespace gr */
