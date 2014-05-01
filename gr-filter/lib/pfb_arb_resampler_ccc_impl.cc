b20df754b45db9b330c3876814b2570167aba441
Sean Nowlan <sean.nowlan@gtri.gatech.edu>
2014-05-01 18:34:17 -0400

    filter: fix pfb_arb_resampler tag propagation by setting enable_update_rate

--- gr-filter/lib/pfb_arb_resampler_ccc_impl.cc
+++ gr-filter/lib/pfb_arb_resampler_ccc_impl.cc
@@ -53,6 +53,7 @@ namespace gr {
       d_resamp = new kernel::pfb_arb_resampler_ccc(rate, taps, filter_size);
       set_history(d_resamp->taps_per_filter());
       set_relative_rate(rate);
+      enable_update_rate(true);
     }
 
     pfb_arb_resampler_ccc_impl::~pfb_arb_resampler_ccc_impl()
