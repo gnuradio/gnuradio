00b8dbe951768ce874cc5d74ffb9f0fa57d580d2
Martin Braun <martin.braun@ettus.com>
2014-05-08 13:34:04 +0200

    blocks: updated some QAs to use proper tsb functions

--- gr-blocks/python/blocks/qa_tsb_vector_sink_X.py
+++ gr-blocks/python/blocks/qa_tsb_vector_sink_X.py
@@ -29,7 +29,7 @@ class qa_tsb_vector_sink (gr_unittest.TestCase):
 
     def setUp (self):
         self.tb = gr.top_block ()
-        self.tsb_key = "ts_last"
+        self.tsb_key = "tsb"
 
     def tearDown (self):
         self.tb = None
@@ -42,7 +42,7 @@ class qa_tsb_vector_sink (gr_unittest.TestCase):
         tag.offset = 5
         tag.value = pmt.intern("bar")
         src = blocks.vector_source_f(data, tags=(tag,))
-        sink = blocks.tsb_vector_sink_f()
+        sink = blocks.tsb_vector_sink_f(tsb_key=self.tsb_key)
         self.tb.connect(
                 src,
                 blocks.stream_to_tagged_stream(gr.sizeof_float, 1, packet_len, self.tsb_key),
