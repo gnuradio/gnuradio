535534ca2b0d6f4293fba2343446cb257ec8bc13
tracierenea <tracie.perez@mavs.uta.edu>
2014-06-11 13:01:07 -0500

    fec: LDPC: Adding framework for bit flip decoder.
    Adding framework for the LDPC bit flip decoder class. Used the
    repetition_decoder class as an example for the organization.
    
    Still trying to get the basics of the framework for the classes in
    place so that they work with the fec api.

---
+++ gr-fec/include/gnuradio/fec/ldpc_par_chk_mtrx.h
@@ -0,0 +1,46 @@
+/* -*- c++ -*- */
+/* 
+ * Copyright 2013-2014 Free Software Foundation, Inc.
+ * 
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published 
+ * by the Free Software Foundation; either version 3, or (at your 
+ * option) any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef INCLUDED_LDPC_PARITY_CHECK_MATRIX
+#define INCLUDED_LDPC_PARITY_CHECK_MATRIX
+
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+namespace gr {
+    namespace fec {
+        namespace code {
+            class LDPC_par_chk_mtrx
+            {
+                // not sure what to put here....
+
+            };
+
+        }  /* namespace code */
+    }  /* namespace fec */
+}  /* namespace gr */
+
+
+
+
+
+#endif /* INCLUDED_LDPC_HELPER_CLASSES_H */
