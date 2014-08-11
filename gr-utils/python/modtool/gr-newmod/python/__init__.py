39f14138ac9158eb0610e89a2ae1f8290f184c44
Tom Rondeau <tom@trondeau.com>
2014-08-11 09:21:37 -0400

    modtool: removing RTLD_GLOBAL hack for swig in gr-modtool.
    This was removed from GNU Radio a while ago due to newer issues. Looks like it's affecting OOT modules as well.

--- gr-utils/python/modtool/gr-newmod/python/__init__.py
+++ gr-utils/python/modtool/gr-newmod/python/__init__.py
@@ -23,24 +23,6 @@ This is the GNU Radio HOWTO module. Place your Python package
 description here (python/__init__.py).
 '''
 
-# ----------------------------------------------------------------
-# Temporary workaround for ticket:181 (swig+python problem)
-import sys
-_RTLD_GLOBAL = 0
-try:
-    from dl import RTLD_GLOBAL as _RTLD_GLOBAL
-except ImportError:
-    try:
-	from DLFCN import RTLD_GLOBAL as _RTLD_GLOBAL
-    except ImportError:
-	pass
-
-if _RTLD_GLOBAL != 0:
-    _dlopenflags = sys.getdlopenflags()
-    sys.setdlopenflags(_dlopenflags|_RTLD_GLOBAL)
-# ----------------------------------------------------------------
-
-
 # import swig generated symbols into the howto namespace
 try:
 	# this might fail if the module is python-only
@@ -50,9 +32,3 @@ except ImportError:
 
 # import any pure python here
 #
-
-# ----------------------------------------------------------------
-# Tail of workaround
-if _RTLD_GLOBAL != 0:
-    sys.setdlopenflags(_dlopenflags)      # Restore original flags
-# ----------------------------------------------------------------
