aff13e7fa6085f7eaff54b9f805fd4606a254d6b
Tomek CEDRO <cederom@tlen.pl>
2014-08-12 22:17:43 +0200

    FreeBSD: FCD/HID LibUSB related fixes.

--- gr-fcd/lib/hid/hid-libusb.c
+++ gr-fcd/lib/hid/hid-libusb.c
@@ -250,7 +250,7 @@ static int get_usage(uint8_t *report_descriptor, size_t size,
 }
 #endif /* INVASIVE_GET_USAGE */
 
-#ifdef __FreeBSD__
+#ifdef FREEBSD_HAVE_NO_LIBUSB_GET_STRING_DESCRIPTOR
 /* The FreeBSD version of libusb doesn't have this funciton. In mainline
    libusb, it's inlined in libusb.h. This function will bear a striking
    resemblence to that one, because there's about one way to code it.
@@ -756,8 +756,13 @@ static void *read_thread(void *param)
 	   if no transfers are pending, but that's OK. */
 	libusb_cancel_transfer(dev->transfer);
 
+#ifdef FREEBSD_HAVE_NO_LIBUSB_HANDLE_EVENTS_COMPLETED
+	while (!dev->cancelled)
+		libusb_handle_events(NULL);
+#else
 	while (!dev->cancelled)
 		libusb_handle_events_completed(usb_context, &dev->cancelled);
+#endif
 
 	/* Now that the read thread is stopping, Wake any threads which are
 	   waiting on data (in hid_read_timeout()). Do this under a mutex to
