796cc152530bce73bc07a8628e7e6502794bf900
Martin Braun <martin.braun@ettus.com>
2014-09-19 16:35:43 -0400

    digital: Removed all references to remove_item_tag()

--- gr-digital/include/gnuradio/digital/ofdm_frame_equalizer_vcvc.h
+++ gr-digital/include/gnuradio/digital/ofdm_frame_equalizer_vcvc.h
@@ -58,18 +58,19 @@ namespace gr {
       /*!
        * \param equalizer The equalizer object that will do the actual work
        * \param cp_len Length of the cyclic prefix in samples (required to correct the frequency offset)
-       * \param len_tag_key Length tag key
+       * \param tsb_key TSB key
        * \param propagate_channel_state If true, the channel state after the last symbol
        *                                will be added to the first symbol as a tag
-       * \param fixed_frame_len Set if the frame length is fixed throughout, 
-       *                        helps with book keeping.
+       * \param fixed_frame_len Set if the frame length is fixed. When this value is given,
+       *                        the TSB tag key can be left empty, but it is useful even
+       *                        when using tagged streams at the input.
        */
       static sptr make(
-	   ofdm_equalizer_base::sptr equalizer,
-	   int cp_len,
-	   const std::string &len_tag_key = "frame_len",
-	   bool propagate_channel_state=false,
-	   int fixed_frame_len=0
+          ofdm_equalizer_base::sptr equalizer,
+          int cp_len,
+          const std::string &tsb_key="frame_len",
+          bool propagate_channel_state=false,
+          int fixed_frame_len=0
       );
     };
 
