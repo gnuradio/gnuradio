d6a09ead61d7a7bc27f575273ad13704fd5a89fb
Johnathan Corgan <johnathan@corganlabs.com>
2014-09-23 13:55:00 -0700

    runtime: add stream flags to io_signature
    This adds a new parameter to the io_signature class to allow specifying
    flags for each input or output port.  The initial use case for this
    capability is to allow a block to indicate that it needs to allocate its
    own buffer memory for its outputs, or to indicate to the runtime that it
    needs to let this block allocate upstream connected blocks' output buffers.
    
    The flags are a 32-bit field with the following bits defined, which may be
    or'd as needed:
    
    MEM_BLOCK_ALLOC - Indicate to the runtime that this block needs to allocate
    the memory for a given output buffer or the output buffer of an upstream
    connected block. Absent other flags, the block's custom allocator must
    provide a contiguous block of doubly mapped virtual memory that aligns on
    a machine page boundary.
    
    MEM_BLOCK_SINGLE - Indicate to the runtime that the custom allocator can
    only define a singly mapped buffer.  This will cause the runtime to
    restrict calls to work() to areas that do not wrap.
    
    MEM_BLOCK_ODD - Indicate to the runtime that the custom allocator can only
    allocate non-page aligned memory.

--- gnuradio-runtime/lib/io_signature.cc
+++ gnuradio-runtime/lib/io_signature.cc
@@ -1,6 +1,6 @@
 /* -*- c++ -*- */
 /*
- * Copyright 2004,2007,2013 Free Software Foundation, Inc.
+ * Copyright 2004,2007,2014 Free Software Foundation, Inc.
  *
  * This file is part of GNU Radio
  *
@@ -32,54 +32,71 @@ namespace gr {
 
   gr::io_signature::sptr
   io_signature::makev(int min_streams, int max_streams,
-                      const std::vector<int> &sizeof_stream_items)
+                      const std::vector<int> &sizeof_stream_items,
+		      const std::vector<uint32_t> &stream_flags)
   {
     return gr::io_signature::sptr
       (new io_signature(min_streams, max_streams,
-                        sizeof_stream_items));
+                        sizeof_stream_items, stream_flags));
   }
 
   gr::io_signature::sptr
   io_signature::make(int min_streams, int max_streams,
-                     int sizeof_stream_item)
+                     int sizeof_stream_item,
+		     uint32_t flags)
   {
     std::vector<int> sizeof_items(1);
+    std::vector<uint32_t> stream_flags(1);
     sizeof_items[0] = sizeof_stream_item;
-    return io_signature::makev(min_streams, max_streams, sizeof_items);
+    stream_flags[0] = flags;
+    return io_signature::makev(min_streams, max_streams, sizeof_items, stream_flags);
   }
 
   gr::io_signature::sptr
   io_signature::make2(int min_streams, int max_streams,
                       int sizeof_stream_item1,
-                      int sizeof_stream_item2)
+                      int sizeof_stream_item2,
+		      uint32_t flags1,
+		      uint32_t flags2)
   {
     std::vector<int> sizeof_items(2);
+    std::vector<uint32_t> stream_flags(2);
     sizeof_items[0] = sizeof_stream_item1;
     sizeof_items[1] = sizeof_stream_item2;
-    return io_signature::makev(min_streams, max_streams, sizeof_items);
+    stream_flags[0] = flags1;
+    stream_flags[1] = flags2;
+    return io_signature::makev(min_streams, max_streams, sizeof_items, stream_flags);
   }
 
   gr::io_signature::sptr
   io_signature::make3(int min_streams, int max_streams,
                       int sizeof_stream_item1,
                       int sizeof_stream_item2,
-                      int sizeof_stream_item3)
+                      int sizeof_stream_item3,
+		      uint32_t flags1,
+		      uint32_t flags2,
+		      uint32_t flags3)
   {
     std::vector<int> sizeof_items(3);
+    std::vector<uint32_t> stream_flags(3);
     sizeof_items[0] = sizeof_stream_item1;
     sizeof_items[1] = sizeof_stream_item2;
     sizeof_items[2] = sizeof_stream_item3;
-    return io_signature::makev(min_streams, max_streams, sizeof_items);
+    stream_flags[0] = flags1;
+    stream_flags[1] = flags2;
+    stream_flags[2] = flags3;
+    return io_signature::makev(min_streams, max_streams, sizeof_items, stream_flags);
   }
 
   // ------------------------------------------------------------------------
 
   io_signature::io_signature(int min_streams, int max_streams,
-                             const std::vector<int> &sizeof_stream_items)
+                             const std::vector<int> &sizeof_stream_items,
+			     const std::vector<uint32_t> &stream_flags)
   {
     if(min_streams < 0
        || (max_streams != IO_INFINITE && max_streams < min_streams))
-      throw std::invalid_argument ("gr::io_signature(1)");
+      throw std::invalid_argument("gr::io_signature(1)");
 
     if(sizeof_stream_items.size() < 1)
       throw std::invalid_argument("gr::io_signature(2)");
@@ -92,6 +109,20 @@ namespace gr {
     d_min_streams = min_streams;
     d_max_streams = max_streams;
     d_sizeof_stream_item = sizeof_stream_items;
+
+    // If user doesn't supply stream flags, make vector of defaults
+    // of the same size as sizeof_stream_items
+    if(stream_flags.size() == 0) {
+      for (size_t i = 0; i < sizeof_stream_items.size(); i++)
+	d_stream_flags.push_back(io_signature::DEFAULT_FLAGS);
+    }
+    else {
+      // Make sure user supplied vector matches
+      if(sizeof_stream_items.size() != stream_flags.size())
+	throw std::invalid_argument("gr::io_signature(4)");
+
+      d_stream_flags = stream_flags;
+    }
   }
 
   io_signature::~io_signature()
@@ -108,10 +139,26 @@ namespace gr {
     return d_sizeof_stream_item[std::min(index, d_sizeof_stream_item.size() - 1)];
   }
 
+  uint32_t
+  io_signature::stream_flags(int _index) const
+  {
+    if(_index < 0)
+      throw std::invalid_argument("gr::io_signature::sizeof_stream_item");
+
+    size_t index = _index;
+    return d_stream_flags[std::min(index, d_stream_flags.size() - 1)];
+  }
+
   std::vector<int>
   io_signature::sizeof_stream_items() const
   {
     return d_sizeof_stream_item;
+  }
+
+  std::vector<uint32_t>
+  io_signature::stream_flags() const
+  {
+    return d_stream_flags;
   }
 
 } /* namespace gr */
