cb32b70b79f430456208a2cd521d028e0ece5d5b
Nathan West <nathan.west@okstate.edu>
2014-10-22 14:39:40 -0500

    volk: add a neon table version of 16u_byteswap

--- volk/kernels/volk/volk_16u_byteswappuppet_16u.h
+++ volk/kernels/volk/volk_16u_byteswappuppet_16u.h
@@ -24,6 +24,15 @@ static inline void volk_16u_byteswappuppet_16u_neon(uint16_t*output, uint16_t* i
 }
 #endif
 
+#ifdef LV_HAVE_NEON
+static inline void volk_16u_byteswappuppet_16u_neon_table(uint16_t*output, uint16_t* intsToSwap, unsigned int num_points){
+
+    volk_16u_byteswap_neon_table((uint16_t*)intsToSwap, num_points);
+    memcpy((void*)output, (void*)intsToSwap, num_points * sizeof(uint16_t));
+
+}
+#endif
+
 #ifdef LV_HAVE_SSE2
 static inline void volk_16u_byteswappuppet_16u_u_sse2(uint16_t* intsToSwap, unsigned int num_points){
 
