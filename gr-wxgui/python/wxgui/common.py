b09c7b3dbac0e0e681e5095aef4ea26f5777c2d6
Johnathan Corgan <johnathan@corganlabs.com>
2014-12-30 07:46:48 -0800

    wxgui: Fix wxgui breakage due to renamed variable

--- gr-wxgui/python/wxgui/common.py
+++ gr-wxgui/python/wxgui/common.py
@@ -48,7 +48,7 @@ class wxgui_hb(object):
 		"""
 		try:
 			assert points[0] == self or points[0][0] == self
-			copy = blocks.copy(self._hb.input_signature().sizeof_stream_item(0))
+			copy = blocks.copy(self._impl.input_signature().sizeof_stream_item(0))
 			handler = self._handler_factory(copy.set_enabled)
 			if RUN_ALWAYS == False:
 				handler(False) #initially disable the copy block
