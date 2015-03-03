3da7369d88f67258d1ed69069521a2620657b875
Tom Rondeau <tom@trondeau.com>
2015-03-03 15:45:36 -0500

    controlport: cleaning up; trying to handle shutdown better.
    Added QA code in for testing probes and other basic connection features. Requires nthreads >= 10.

--- gnuradio-runtime/lib/controlport/thrift/rpcserver_booter_thrift.cc
+++ gnuradio-runtime/lib/controlport/thrift/rpcserver_booter_thrift.cc
@@ -64,6 +64,16 @@ template<typename TserverBase, typename TserverClass>
 const unsigned int thrift_application_base<TserverBase, TserverClass>::d_default_num_thrift_threads(10U);
 
 template<typename TserverBase, typename TserverClass>
+thrift_application_base<TserverBase, TserverClass>::~thrift_application_base()
+{
+  GR_LOG_DEBUG(d_debug_logger, "thrift_application_base: shutdown");
+  if(d_is_running) {
+    d_thriftserver->stop();
+    d_is_running = false;
+  }
+}
+
+template<typename TserverBase, typename TserverClass>
 void thrift_application_base<TserverBase, TserverClass>::start_thrift()
 {
   //char* argv[2];
@@ -85,6 +95,8 @@ void thrift_application_base<TserverBase, TserverClass>::start_thrift()
   //}
 
   //std::cerr << "thrift_application_base: start_thrift" << std::endl;
-  d_thriftserver->serve();
+  GR_LOG_DEBUG(d_debug_logger, "thrift_application_base: start server");
   d_is_running = true;
+  d_thriftserver->serve();
+  GR_LOG_DEBUG(d_debug_logger, "thrift_application_base: server started");
 }
