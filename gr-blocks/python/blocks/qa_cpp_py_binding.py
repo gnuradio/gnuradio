31d605f9319707a9c7e91bf2e6d004603a395c7c
Tom Rondeau <tom@trondeau.com>
2015-03-10 15:06:20 -0400

    controlport: QA needs to get host and port out of the endpoint.
    Probably need a better parsing strategy.

--- gr-blocks/python/blocks/qa_cpp_py_binding.py
+++ gr-blocks/python/blocks/qa_cpp_py_binding.py
@@ -24,7 +24,7 @@
 # This program tests mixed python and c++ ctrlport exports in a single app
 #
 
-import sys, time, random, numpy
+import sys, time, random, numpy, re
 from gnuradio import gr, gr_unittest, blocks
 
 from gnuradio.ctrlport import GNURadio
@@ -152,10 +152,13 @@ class test_cpp_py_binding(gr_unittest.TestCase):
 
         # Get available endpoint
         ep = gr.rpcmanager_get().endpoints()[0]
+        hostname = re.search("-h (\S+|\d+\.\d+\.\d+\.\d+)", ep).group(1)
+        portnum = re.search("-p (\d+)", ep).group(1)
+        argv = [None, hostname, portnum]
 
         # Initialize a simple ControlPort client from endpoint
         from gnuradio.ctrlport.GNURadioControlPortClient import GNURadioControlPortClient
-        radiosys = GNURadioControlPortClient(rpcmethod='thrift')
+        radiosys = GNURadioControlPortClient(argv=argv, rpcmethod='thrift')
         radio = radiosys.client
 
         # Get all exported knobs
