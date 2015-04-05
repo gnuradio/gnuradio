defccdf24ccb79df21cb3b4b559f9212169afacf
Tim O'Shea <tim.oshea753@gmail.com>
2015-04-05 13:23:58 -0700

    fec: TPC adding standard headers

--- gr-fec/lib/tpc_decoder.cc
+++ gr-fec/lib/tpc_decoder.cc
@@ -1,3 +1,25 @@
+/* -*- c++ -*- */
+/*
+ * Copyright 2015 Free Software Foundation, Inc.
+ *
+ * This file is part of GNU Radio
+ *
+ * GNU Radio is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * GNU Radio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Radio; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
 #include <gnuradio/fec/tpc_decoder.h>
 #include <gnuradio/fec/tpc_common.h>
 #include <volk/volk.h>
@@ -12,13 +34,6 @@
 #include <string.h>             // for memcpy
 
 #include <gnuradio/fec/maxstar.h>
-
-/**
- * This code borrows from the CML library for the SISO Decode functionality, 
- * and for the RSC_Encode functionality.  Please have a look @:
- * https://code.google.com/p/iscml
- * 
- */
 
 namespace gr { 
  namespace fec { 
--- gr-fec/lib/tpc_encoder.cc
+++ gr-fec/lib/tpc_encoder.cc
@@ -1,3 +1,25 @@
+/* -*- c++ -*- */
+/*
+ * Copyright 2015 Free Software Foundation, Inc.
+ *
+ * This file is part of GNU Radio
+ *
+ * GNU Radio is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * GNU Radio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Radio; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
 #include <gnuradio/fec/tpc_encoder.h>
 #include <gnuradio/fec/tpc_common.h>
 #include <gnuradio/fec/generic_encoder.h>
@@ -12,13 +34,6 @@
 #include <algorithm>            // for std::reverse
 #include <string.h>             // for memcpy
 
-
-/**
- * This code borrows from the CML library for the CC Encode functionality, 
- * and for the RSC_Encode functionality.  Please have a look @:
- * https://code.google.com/p/iscml
- * 
- */
 namespace gr {
 namespace fec { 
 generic_encoder::sptr
