f13b98946835d592212b7bda0c3f02fa8773a6c8
Marcus Müller <marcus.mueller@ettus.com>
2016-01-18 20:07:02 +0100

    added a --clock-source argument, defaulting to "internal"

--- gr-uhd/apps/uhd_app.py
+++ gr-uhd/apps/uhd_app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright 2015 Free Software Foundation, Inc.
+# Copyright 2015-2016 Free Software Foundation, Inc.
 #
 # This file is part of GNU Radio
 #
@@ -141,6 +141,8 @@ class UHDApp(object):
         if args.spec:
             for mb_idx in xrange(self.usrp.get_num_mboards()):
                 self.usrp.set_subdev_spec(args.spec, mb_idx)
+        # Set the clock source:
+        self.usrp.set_clock_source(args.clock_source)
         # Sampling rate:
         self.usrp.set_samp_rate(args.samp_rate)
         self.samp_rate = self.usrp.get_samp_rate()
@@ -295,5 +297,7 @@ class UHDApp(object):
                             help="Show asynchronous message notifications from UHD")
         group.add_argument("--sync", choices=('default', 'pps', 'auto'),
                           default='auto', help="Set to 'pps' to sync devices to PPS")
+        group.add_argument("--clock-source",
+                          default='internal', help="Set the clock source")
         return parser
 
