7b0164dde6ca6ca049ef122f0bcc57a74c875064
Andrew Jeffery <andrew@aj.id.au>
2016-08-31 23:14:24 +0930

    gr-pager: flex_parse_impl: Break down functions into small classes
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>

--- gr-pager/lib/flex_parse_impl.cc
+++ gr-pager/lib/flex_parse_impl.cc
@@ -1,6 +1,6 @@
 /* -*- c++ -*- */
 /*
- * Copyright 2004,2006,2007,2010,2012 Free Software Foundation, Inc.
+ * Copyright 2004,2006,2007,2010,2012,2016 Free Software Foundation, Inc.
  *
  * This file is part of GNU Radio
  *
@@ -25,11 +25,12 @@
 #endif
 
 #include "flex_parse_impl.h"
-#include "bch3221.h"
 #include <gnuradio/io_signature.h>
 #include <ctype.h>
 #include <iostream>
 #include <iomanip>
+
+#define FLEX_IDLE_CODEWORD 0x001FFFFF
 
 namespace gr {
   namespace pager {
@@ -43,15 +44,10 @@ namespace gr {
 
     flex_parse_impl::flex_parse_impl(msg_queue::sptr queue, float freq) :
       sync_block("flex_parse",
-		    io_signature::make(1, 1, sizeof(int32_t)),
+		    io_signature::make(1, 1, sizeof(uint32_t)),
 		    io_signature::make(0, 0, 0)),
       d_queue(queue),
       d_freq(freq)
-    {
-      d_count = 0;
-    }
-
-    flex_parse_impl::~flex_parse_impl()
     {
     }
 
@@ -59,174 +55,417 @@ namespace gr {
 			      gr_vector_const_void_star &input_items,
 			      gr_vector_void_star &output_items)
     {
-      const int32_t *in = (const int32_t *)input_items[0];
+      const uint32_t *in = (const uint32_t *)input_items[0];
 
       int i = 0;
       while(i < noutput_items) {
 	// Accumulate one whole frame's worth of data words (88 of them)
-	d_datawords[d_count] = *in++; i++;
-	if(++d_count == 88) {
-	  parse_data();
-	  d_count = 0;
+	d_datawords.push_back(*in++);
+	i++;
+	if(d_datawords.size() == 88) {
+	  parse_frame(d_datawords);
+	  d_datawords.clear();
 	}
       }
 
       return i;
     }
 
-    /* FLEX data frames (that is, 88 data words per phase recovered
-       after sync, symbol decoding, dephasing, deinterleaving, error
-       correction, and conversion from codewords to data words) start
-       with a block information word containing indices of the page
-       address field and page vector fields.  */
     void
-    flex_parse_impl::parse_capcode(int aw1, int aw2)
+    flex_parse_impl::parse_frame(std::vector<uint32_t> &data)
     {
-      d_laddr = (aw1 < 0x008001L) ||
-	(aw1 > 0x1E0000L) ||
-	(aw1 > 0x1E7FFEL);
+      flex_frame frame(data);
 
-      if(d_laddr)
-        d_capcode = aw1+((aw2^0x001FFFFF)<<15)+0x1F9000;  // Don't ask
-      else
-        d_capcode = aw1-0x8000;
-    }
-
-    void
-    flex_parse_impl::parse_data()
-    {
-      // Block information word is the first data word in frame
-      int biw = d_datawords[0];
-
-      // Nothing to see here, please move along
-      if(biw == 0 || biw == 0x001FFFFF)
+      if (!frame.is_active())
 	return;
 
-      // Vector start index is bits 15-10
-      // Address start address is bits 9-8, plus one for offset
-      int voffset = (biw >> 10) & 0x3f;
-      int aoffset = ((biw >> 8) & 0x03) + 1;
+      if (!frame.validate())
+	return;
 
-      //printf("BIW:%08X AW:%02i-%02i\n", biw, aoffset, voffset);
-
-      // Iterate through pages and dispatch to appropriate handler
-      for(int i = aoffset; i < voffset; i++) {
-	int j = voffset+i-aoffset;		// Start of vector field for address @ i
-
-	if(d_datawords[i] == 0x00000000 ||
-	   d_datawords[i] == 0x001FFFFF)
-	  continue;				// Idle codewords, invalid address
-
-	parse_capcode(d_datawords[i], d_datawords[i+1]);
-        if(d_laddr)
-	  i++;
-
-        if(d_capcode < 0)			// Invalid address, skip
-          continue;
-
-        // Parse vector information word for address @ offset 'i'
-	int viw = d_datawords[j];
-	d_type = (page_type_t)((viw >> 4) & 0x00000007);
-	int mw1 = (viw >> 7) & 0x00000007F;
-	int len = (viw >> 14) & 0x0000007F;
-
-	if(is_numeric_page(d_type))
-	  len &= 0x07;
-        int mw2 = mw1+len;
-
-	if(mw1 == 0 && mw2 == 0)
-	  continue;				// Invalid VIW
-
-	if(is_tone_page(d_type))
-	  mw1 = mw2 = 0;
-
-	if(mw1 > 87 || mw2 > 87)
-	  continue;				// Invalid offsets
+      for (flex_page_iter i = frame.begin(); i != frame.end(); i++) {
 
 	d_payload.str("");
 	d_payload.setf(std::ios::showpoint);
 	d_payload << std::setprecision(6) << std::setw(7)
-		  << d_freq/1e6 << FIELD_DELIM
-		  << std::setw(10) << d_capcode << FIELD_DELIM
-		  << flex_page_desc[d_type] << FIELD_DELIM;
+	  << d_freq/1e6 << FIELD_DELIM;
 
-	if(is_alphanumeric_page(d_type))
-	  parse_alphanumeric(mw1, mw2-1, j);
-	else if(is_numeric_page(d_type))
-	  parse_numeric(mw1, mw2, j);
-	else if(is_tone_page(d_type))
-	  parse_tone_only();
-	else
-	  parse_unknown(mw1, mw2);
-
-	message::sptr msg = message::make_from_string(std::string(d_payload.str()));
-	d_queue->handle(msg);
+	if (i->parse(d_payload))
+	  d_queue->handle(message::make_from_string(d_payload.str()));
       }
     }
 
-    void
-    flex_parse_impl::parse_alphanumeric(int mw1, int mw2, int j)
+    bool
+    is_active_codeword(uint32_t word)
     {
-      int frag;
-      //bool cont;
+      return !(word == 0 || word == FLEX_IDLE_CODEWORD);
+    }
 
-      if(!d_laddr) {
-	frag = (d_datawords[mw1] >> 11) & 0x03;
-	//cont = (d_datawords[mw1] >> 10) & 0x01;
-	mw1++;
+    bool
+    flex_page_iter::is_active()
+    {
+      return gr::pager::is_active_codeword(d_frame.get_word(d_aoffset + d_i));
+    }
+
+    bool
+    flex_page_iter::has_laddr()
+    {
+      int aw1 = d_frame.get_word(d_aoffset + d_i);
+
+      return (aw1 < 0x008001L) || (aw1 > 0x1E0000L) || (aw1 > 0x1E7FFEL);
+    }
+
+    flex_header *
+    flex_page_iter::generate_header()
+    {
+      if (!is_active())
+	return new flex_header_inactive();
+
+      if (has_laddr())
+	return new flex_header_laddr(&d_frame, d_i, d_aoffset, d_voffset);
+
+      return new flex_header_saddr(&d_frame, d_i, d_aoffset, d_voffset);
+    }
+
+    void
+    flex_page_iter::next()
+    {
+      delete d_header;
+      d_header = NULL;
+      delete d_page;
+      d_page = NULL;
+
+      if (is_active())
+	if (has_laddr())
+	  d_i += 2;
+	else
+	  d_i += 1;
+      else
+	d_i = d_max;
+
+      if (d_i > d_max)
+	d_i = d_max;
+    }
+
+    flex_page *
+    flex_page_iter::get_page()
+    {
+      d_header = generate_header();
+      d_page = d_header->generate_page();
+
+      return d_page;
+    }
+
+    flex_page_iter
+    flex_page_iter::operator++(int junk)
+    {
+      next();
+      return *this;
+    }
+
+    flex_page*
+    flex_page_iter::operator->()
+    {
+      return get_page();
+    }
+
+    bool
+    flex_page_iter::operator==(const flex_page_iter& rhs)
+    {
+      return &d_frame == &(rhs.d_frame) &&
+	(d_aoffset + d_i) == (rhs.d_aoffset + rhs.d_i);
+    }
+
+    bool
+    flex_page_iter::operator!=(const flex_page_iter &rhs)
+    {
+      return !(*this == rhs);
+    }
+
+    uint32_t
+    flex_frame::get_block_info()
+    {
+      // Block information word is the first data word in frame
+      return d_datawords[0];
+    }
+
+    size_t
+    flex_frame::get_vector_offset()
+    {
+      // Vector start index is bits 15-10
+      return (get_block_info() >> 10) & 0x3f;
+    }
+
+    size_t
+    flex_frame::get_address_offset()
+    {
+      // Address start address is bits 9-8, plus one for offset
+      return ((get_block_info() >> 8) & 0x03) + 1;
+    }
+
+    uint32_t
+    flex_frame::get_word(size_t i)
+    {
+      return d_datawords.at(i);
+    }
+
+    bool
+    flex_frame::is_active()
+    {
+      return gr::pager::is_active_codeword(get_block_info());
+    }
+
+    bool
+    flex_frame::parse(std::ostringstream &stream)
+    {
+      return true;
+    }
+
+    bool
+    flex_frame::validate()
+    {
+      size_t voffset = get_vector_offset();
+      size_t aoffset = get_address_offset();
+
+      return (2 * voffset - aoffset) < d_datawords.size();
+    }
+
+    bool
+    flex_frame::validate(size_t start, size_t len)
+    {
+      return start > get_vector_offset() &&
+	(start + len) < d_datawords.size();
+    }
+
+    flex_page_iter
+    flex_frame::begin()
+    {
+      int aoffset = get_address_offset();
+      int voffset = get_vector_offset();
+
+      if (aoffset > voffset)
+	aoffset = voffset;
+
+      return flex_page_iter(*this, aoffset, voffset);
+    }
+
+    flex_page_iter
+    flex_frame::end()
+    {
+      return flex_page_iter(*this, get_vector_offset(), get_vector_offset());
+    }
+
+    uint32_t
+    flex_header::get_vector_field()
+    {
+      return d_frame->get_word(d_voffset + d_page_id);
+    }
+
+    page_type_t
+    flex_header::get_type()
+    {
+      return (page_type_t)((get_vector_field() >> 4) & 0x7);
+    }
+
+    size_t
+    flex_header::get_message_start()
+    {
+      return (get_vector_field() >> 7) & 0x00000007F;
+    }
+
+    size_t
+    flex_header::get_message_length()
+    {
+      return (get_vector_field() >> 14) & 0x0000007F;
+    }
+
+    bool
+    flex_header::validate()
+    {
+      size_t start = get_message_start();
+
+      if (!d_frame->validate(start, get_message_length()))
+	return false;
+
+      return get_capcode() >= 0 &&
+       start > (d_voffset + d_page_id);
+    }
+
+    bool
+    flex_header::parse(std::ostringstream &stream)
+    {
+      if (d_frame->parse(stream)) {
+	stream << std::setw(10) << get_capcode() << FIELD_DELIM
+	  << flex_page_desc[get_type()] << FIELD_DELIM;
+	return true;
       }
-      else {
-	frag = (d_datawords[j+1] >> 11) & 0x03;
-	//cont = (d_datawords[j+1] >> 10) & 0x01;
-	mw2--;
+
+      return false;
+    }
+
+    uint32_t
+    flex_header_inactive::get_meta()
+    {
+      return 0;
+    }
+
+    int
+    flex_header_inactive::get_capcode()
+    {
+      return -1;
+    }
+
+    flex_page *
+    flex_header_inactive::generate_page()
+    {
+      return new flex_page_inactive();
+    }
+
+    uint32_t
+    flex_header_laddr::get_meta()
+    {
+      return d_frame->get_word(d_voffset + d_page_id + 1);
+    }
+
+    int
+    flex_header_laddr::get_capcode()
+    {
+      int aw1 = d_frame->get_word(d_aoffset + d_page_id);
+      int aw2 = d_frame->get_word(d_aoffset + d_page_id + 1);
+
+      return aw1 + ((aw2 ^ FLEX_IDLE_CODEWORD) << 15) + 0x1F9000;
+    }
+
+    flex_page *
+    flex_header_laddr::generate_page()
+    {
+      page_type_t type;
+      size_t mw1;
+      size_t len;
+
+      if (!validate())
+	return new flex_page_inactive();
+
+      type = get_type();
+      mw1 = get_message_start();
+      len = get_message_length();
+
+      if (is_alphanumeric_page(type)) {
+	return new flex_page_aln(d_frame, this, get_meta(), mw1, (len - 1),
+	    get_capcode());
+      } else if (is_numeric_page(type)) {
+	return new flex_page_num(d_frame, this, get_meta(), (mw1 + 1), len, type);
       }
 
-      //d_payload << frag << FIELD_DELIM;
-      //d_payload << cont << FIELD_DELIM;
+      return new flex_page_inactive();
+    }
 
-      for(int i = mw1; i <= mw2; i++) {
-	int dw = d_datawords[i];
+    int
+    flex_header_saddr::get_capcode()
+    {
+      return d_frame->get_word(d_aoffset + d_page_id) - 0x8000;
+    }
+
+    uint32_t
+    flex_header_saddr::get_meta()
+    {
+      return d_frame->get_word(get_message_start());
+    }
+
+    flex_page *
+    flex_header_saddr::generate_page()
+    {
+      page_type_t type;
+      size_t mw1;
+      size_t len;
+
+      if (!validate())
+	return new flex_page_inactive();
+
+      type = get_type();
+      mw1 = get_message_start() + 1;
+      len = get_message_length();
+
+      if (is_alphanumeric_page(type)) {
+	return new flex_page_aln(d_frame, this, get_meta(), mw1, len - 1,
+	    get_capcode());
+      } else if (is_numeric_page(type)) {
+	return new flex_page_num(d_frame, this, get_meta(), mw1, len, type);
+      }
+
+      return new flex_page_inactive();
+    }
+
+    bool
+    flex_page::is_valid_alphanumeric(unsigned char c)
+    {
+      return c != 0x03;
+    }
+
+    bool
+    flex_page::parse(std::ostringstream &stream)
+    {
+      return d_header->parse(stream);
+    }
+
+    bool
+    flex_page_inactive::parse(std::ostringstream &stream)
+    {
+      return false;
+    }
+
+    bool
+    flex_page_aln::parse_page(std::ostringstream &stream)
+    {
+      for(size_t i = d_mw1; i <= (d_mw1 + d_len); i++) {
+	int dw = d_frame->get_word(i);
 	unsigned char ch;
 
-	if(i > mw1 || frag != 0x03) {
+	if(i > d_mw1 || d_meta != 0x03) {
 	  ch = dw & 0x7F;
-	  if(ch != 0x03)
-	    d_payload << ch;
+	  if(is_valid_alphanumeric(ch))
+	    stream << ch;
 	}
 
 	ch = (dw >> 7) & 0x7F;
-	if(ch != 0x03)	// Fill
-	  d_payload << ch;
+	if(is_valid_alphanumeric(ch))
+	  stream << ch;
 
 	ch = (dw >> 14) & 0x7F;
-	if(ch != 0x03)	// Fill
-	  d_payload << ch;
+	if(is_valid_alphanumeric(ch))
+	  stream << ch;
       }
+
+      return true;
     }
 
-    void
-    flex_parse_impl::parse_numeric(int mw1, int mw2, int j)
+    bool
+    flex_page_aln::parse(std::ostringstream &stream)
     {
-      // Get first dataword from message field or from second
-      // vector word if long address
-      int dw;
-      if(!d_laddr) {
-	dw = d_datawords[mw1];
-	mw1++;
-	mw2++;
-      }
-      else {
-	dw = d_datawords[j+1];
-      }
+      int mw2 = d_mw1 + d_len;
 
+      if (d_mw1 > 87 || mw2 > 87)
+	return false;
+
+      if (!flex_page::parse(stream))
+	return false;
+
+      return parse_page(stream);
+    }
+
+    bool
+    flex_page_num::parse(std::ostringstream &stream)
+    {
+      int dw = d_meta;
       unsigned char digit = 0;
       int count = 4;
-      if(d_type == FLEX_NUMBERED_NUMERIC)
-	count += 10;	// Skip 10 header bits for numbered numeric pages
-      else
-	count += 2;	// Otherwise skip 2
 
-      for(int i = mw1; i <= mw2; i++) {
+      if (!flex_page::parse(stream))
+	return false;
+
+      // Skip 10 header bits for numbered numeric pages, otherwise 2
+      count += (d_type == FLEX_NUMBERED_NUMERIC) ? 10 : 2;
+
+      for(size_t i = d_mw1; i <= (d_mw1 + d_len); i++) {
 	for(int k = 0; k < 21; k++) {
 	  // Shift LSB from data word into digit
 	  digit = (digit >> 1) & 0x0F;
@@ -234,25 +473,16 @@ namespace gr {
 	    digit ^= 0x08;
 	  dw >>= 1;
 	  if(--count == 0) {
-	    if(digit != 0x0C) // Fill
-	      d_payload << flex_bcd[digit];
+	    if(digit != 0x0C)
+	      stream << flex_bcd[digit];
 	    count = 4;
 	  }
 	}
 
-	dw = d_datawords[i];
+	dw = d_frame->get_word(i);
       }
-    }
 
-    void
-    flex_parse_impl::parse_tone_only()
-    {
+      return true;
     }
-
-    void
-    flex_parse_impl::parse_unknown(int mw1, int mw2)
-    {
-    }
-
   } /* namespace pager */
 } /* namespace gr */
--- gr-pager/lib/flex_parse_impl.h
+++ gr-pager/lib/flex_parse_impl.h
@@ -28,6 +28,8 @@
 #include <gnuradio/msg_queue.h>
 #include "flex_modes.h"
 #include <sstream>
+#include <map>
+#include <utility>
 
 namespace gr {
   namespace pager {
@@ -37,31 +39,227 @@ namespace gr {
     class flex_parse_impl : public flex_parse
     {
     private:
+      msg_queue::sptr d_queue;
+      float d_freq;
+      std::vector<uint32_t> d_datawords;
       std::ostringstream d_payload;
-      msg_queue::sptr d_queue;		  // Destination for decoded pages
 
-      int d_count;	                  // Count of received codewords
-      int d_datawords[88];                // 11 blocks of 8 32-bit words
-
-      page_type_t d_type;	  	  // Current page type
-      int d_capcode;	                  // Current page destination address
-      bool d_laddr;	                  // Current page has long address
-      float d_freq;			  // Channel frequency
-
-      void parse_data();	    	  // Handle a frame's worth of data
-      void parse_capcode(int32_t aw1, int32_t aw2);
-      void parse_alphanumeric(int mw1, int mw2, int j);
-      void parse_numeric(int mw1, int mw2, int j);
-      void parse_tone_only();
-      void parse_unknown(int mw1, int mw2);
+      void parse_frame(std::vector<uint32_t> &data);
 
     public:
       flex_parse_impl(msg_queue::sptr queue, float freq);
-      ~flex_parse_impl();
+      ~flex_parse_impl() { }
 
       int work(int noutput_items,
 	       gr_vector_const_void_star &input_items,
 	       gr_vector_void_star &output_items);
+    };
+
+    bool is_active_codeword(uint32_t word);
+
+    class flex_frame;
+    class flex_header;
+    class flex_page;
+    class flex_page_iter;
+
+    class flex_frame
+    {
+      private:
+	std::vector<uint32_t> &d_datawords;
+
+	static bool is_active_codeword(uint32_t word);
+
+	/* FLEX data frames (that is, 88 data words per phase recovered
+	   after sync, symbol decoding, dephasing, deinterleaving, error
+	   correction, and conversion from codewords to data words) start
+	   with a block information word containing indices of the page
+	   address field and page vector fields.  */
+
+	uint32_t get_block_info();
+	size_t get_vector_offset();
+	size_t get_address_offset();
+
+      public:
+	flex_frame(std::vector<uint32_t> &datawords): d_datawords(datawords) { }
+	virtual ~flex_frame() { }
+
+	uint32_t get_word(size_t i);
+	bool is_active();
+	bool validate();
+	bool validate(size_t start, size_t len);
+	bool parse(std::ostringstream &stream);
+	flex_page_iter begin();
+	flex_page_iter end();
+    };
+
+    class flex_header
+    {
+      private:
+	uint32_t get_vector_field();
+	virtual uint32_t get_meta() = 0;
+	virtual int get_capcode() = 0;
+
+      protected:
+	flex_frame *d_frame;
+	unsigned int d_page_id;
+	size_t d_aoffset;
+	size_t d_voffset;
+
+	page_type_t get_type();
+	size_t get_message_start();
+	size_t get_message_length();
+	bool validate();
+
+      public:
+	flex_header(flex_frame *frame, unsigned int page_id, size_t aoffset,
+	    size_t voffset) :
+	  d_frame(frame), d_page_id(page_id), d_aoffset(aoffset),
+	  d_voffset(voffset) { }
+	virtual ~flex_header() { }
+
+	bool parse(std::ostringstream &stream);
+	virtual flex_page *generate_page() = 0;
+    };
+
+    class flex_header_inactive : public flex_header
+    {
+      private:
+	uint32_t get_meta();
+	int get_capcode();
+
+      public:
+	flex_header_inactive() : flex_header(NULL, 0, 0, 0) { }
+	virtual ~flex_header_inactive() { }
+	flex_page *generate_page();
+    };
+
+    class flex_header_laddr : public flex_header
+    {
+      private:
+	uint32_t get_meta();
+	int get_capcode();
+
+      public:
+	flex_header_laddr(flex_frame *frame, unsigned int page_id, size_t aoffset,
+	    size_t voffset) : flex_header(frame, page_id, aoffset, voffset) { }
+	virtual ~flex_header_laddr() { }
+
+	flex_page *generate_page();
+    };
+
+    class flex_header_saddr : public flex_header
+    {
+      private:
+	uint32_t get_meta();
+	int get_capcode();
+
+      public:
+	flex_header_saddr(flex_frame *frame, unsigned int page_id, size_t aoffset,
+	    size_t voffset) : flex_header(frame, page_id, aoffset, voffset) { }
+	virtual ~flex_header_saddr() {}
+
+	flex_page *generate_page();
+    };
+
+    class flex_page
+    {
+      protected:
+	flex_frame *d_frame;
+	flex_header *d_header;
+	uint32_t d_meta;
+	size_t d_mw1;
+	size_t d_len;
+
+	bool is_valid_alphanumeric(unsigned char c);
+
+      public:
+	flex_page(flex_frame *frame, flex_header *header, uint32_t meta,
+	    size_t mw1, size_t len)
+	  : d_frame(frame), d_header(header), d_meta(meta), d_mw1(mw1),
+	    d_len(len) { };
+	virtual ~flex_page() { };
+
+        virtual bool parse(std::ostringstream &stream) = 0;
+    };
+
+    class flex_page_inactive : public flex_page
+    {
+      public:
+	flex_page_inactive() : flex_page(NULL, NULL, 0, 0, 0) { }
+	virtual ~flex_page_inactive() { }
+
+	bool parse(std::ostringstream &stream);
+    };
+
+    class flex_page_aln : public flex_page
+    {
+      private:
+	int d_capcode;
+        std::map<int, std::ostringstream*> d_split_queue;
+
+	bool parse_page(std::ostringstream &stream);
+
+      public:
+	flex_page_aln(flex_frame *frame, flex_header *header, uint32_t meta,
+	    size_t mw1, size_t len, int capcode) :
+	  flex_page(frame, header, ((meta >> 11) & 0x03), mw1, (len - 1)),
+	  d_capcode(capcode) { }
+	virtual ~flex_page_aln() { }
+
+	bool parse(std::ostringstream &stream);
+    };
+
+    class flex_page_num : public flex_page
+    {
+      private:
+	int d_type;
+
+      public:
+	flex_page_num(flex_frame *frame, flex_header *header, uint32_t meta,
+	    size_t mw1, size_t len, size_t type) :
+	  flex_page(frame, header, meta, mw1, len), d_type(type & 0x7) { }
+	virtual ~flex_page_num() { }
+
+	bool parse(std::ostringstream &stream);
+    };
+
+    class flex_page_iter
+    {
+      private:
+	unsigned int d_i;
+	flex_frame &d_frame;
+	size_t d_aoffset;
+	size_t d_voffset;
+	unsigned int d_max;
+	flex_header *d_header;
+	flex_page *d_page;
+
+	bool is_active_codeword(uint32_t word);
+	bool is_active();
+	bool has_laddr();
+	flex_header *generate_header();
+	void next();
+	flex_page *get_page();
+
+      public:
+	flex_page_iter(flex_frame &frame, size_t aoffset, size_t voffset)
+	  : d_frame(frame), d_aoffset(aoffset), d_voffset(voffset),
+	    d_max(voffset - aoffset)
+	{
+	  d_i = 0;
+	  d_header = NULL;
+	  d_page = NULL;
+	};
+
+	virtual ~flex_page_iter() { 
+	  delete d_header;
+	  delete d_page;
+	};
+
+	flex_page_iter operator++(int junk);
+	flex_page* operator->();
+	bool operator==(const flex_page_iter& rhs);
+	bool operator!=(const flex_page_iter& rhs);
     };
 
   } /* namespace pager */
