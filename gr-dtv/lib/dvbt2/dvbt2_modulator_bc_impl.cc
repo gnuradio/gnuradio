3d1df35c34aa8da04979cdf58ba64a5056665423
Ron Economos <w6rz@comcast.net>
2017-02-09 08:21:00 -0800

    Update DVB-T2 modulator copyright date.

--- gr-dtv/lib/dvbt2/dvbt2_modulator_bc_impl.cc
+++ gr-dtv/lib/dvbt2/dvbt2_modulator_bc_impl.cc
@@ -1,6 +1,6 @@
 /* -*- c++ -*- */
 /* 
- * Copyright 2015 Free Software Foundation, Inc.
+ * Copyright 2015,2017 Free Software Foundation, Inc.
  * 
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
