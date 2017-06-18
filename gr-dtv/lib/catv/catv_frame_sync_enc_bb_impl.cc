15713fdf6b783e73fa0ac54bfa5c55c897f2af28
Ron Economos <w6rz@comcast.net>
2017-06-18 14:44:22 -0700

    dtv: Add 256QAM mode to ITU-T J.83B transmitter.

--- gr-dtv/lib/catv/catv_frame_sync_enc_bb_impl.cc
+++ gr-dtv/lib/catv/catv_frame_sync_enc_bb_impl.cc
@@ -1,6 +1,6 @@
 /* -*- c++ -*- */
 /* 
- * Copyright 2016 Free Software Foundation, Inc.
+ * Copyright 2016,2017 Free Software Foundation, Inc.
  * 
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -29,22 +29,28 @@ namespace gr {
   namespace dtv {
 
     catv_frame_sync_enc_bb::sptr
-    catv_frame_sync_enc_bb::make(int ctrlword)
+    catv_frame_sync_enc_bb::make(catv_constellation_t constellation, int ctrlword)
     {
       return gnuradio::get_initial_sptr
-        (new catv_frame_sync_enc_bb_impl(ctrlword));
+        (new catv_frame_sync_enc_bb_impl(constellation, ctrlword));
     }
 
     /*
      * The private constructor
      */
-    catv_frame_sync_enc_bb_impl::catv_frame_sync_enc_bb_impl(int ctrlword)
+    catv_frame_sync_enc_bb_impl::catv_frame_sync_enc_bb_impl(catv_constellation_t constellation, int ctrlword)
       : gr::block("catv_frame_sync_enc_bb",
               gr::io_signature::make(1, 1, sizeof(unsigned char)),
               gr::io_signature::make(1, 1, sizeof(unsigned char)))
     {
-      set_output_multiple(60 * 128 + 6);
+      if (constellation == CATV_MOD_64QAM) {
+        set_output_multiple((60 * 128 * 7) + 42);
+      }
+      else {
+        set_output_multiple((88 * 128 * 7) + 40);
+      }
       control_word = ctrlword;
+      signal_constellation = constellation;
     }
 
     /*
@@ -57,7 +63,12 @@ namespace gr {
     void
     catv_frame_sync_enc_bb_impl::forecast (int noutput_items, gr_vector_int &ninput_items_required)
     {
-      ninput_items_required[0] = noutput_items / (60 * 128 + 6) * (60 * 128);
+      if (signal_constellation == CATV_MOD_64QAM) {
+        ninput_items_required[0] = noutput_items / ((60 * 128 * 7) + 42) * (60 * 128);
+      }
+      else {
+        ninput_items_required[0] = noutput_items / ((88 * 128 * 7) + 40) * (88 * 128);
+      }
     }
 
     int
@@ -68,24 +79,78 @@ namespace gr {
     {
       const unsigned char *in = (const unsigned char *) input_items[0];
       unsigned char *out = (unsigned char *) output_items[0];
+      int consumed = 0;
+      int i = 0;
+      unsigned char b;
 
-      int i = 0, j = 0;
       while (i < noutput_items) {
-        memcpy(out + i, in + j, 60 * 128);
-        i += 60 * 128;
-        j += 60 * 128;
-
-        out[i++] = 0x75;
-        out[i++] = 0x2C;
-        out[i++] = 0x0D;
-        out[i++] = 0x6C;
-        out[i++] = control_word << 3;
-        out[i++] = 0x00;
+        if (signal_constellation == CATV_MOD_64QAM) {
+          for (int j = 0; j < 60 * 128; j++) {
+            b = *in++;
+            consumed++;
+            for (int n = 6; n >= 0; n--) {
+              out[i++] = b & (1 << n) ? 1 : 0;
+            }
+          }
+          b = 0x75;
+          for (int n = 6; n >= 0; n--) {
+            out[i++] = b & (1 << n) ? 1 : 0;
+          }
+          b = 0x2c;
+          for (int n = 6; n >= 0; n--) {
+            out[i++] = b & (1 << n) ? 1 : 0;
+          }
+          b = 0x0d;
+          for (int n = 6; n >= 0; n--) {
+            out[i++] = b & (1 << n) ? 1 : 0;
+          }
+          b = 0x6c;
+          for (int n = 6; n >= 0; n--) {
+            out[i++] = b & (1 << n) ? 1 : 0;
+          }
+          b = control_word << 3;
+          for (int n = 6; n >= 0; n--) {
+            out[i++] = b & (1 << n) ? 1 : 0;
+          }
+          b = 0x00;
+          for (int n = 6; n >= 0; n--) {
+            out[i++] = b & (1 << n) ? 1 : 0;
+          }
+        }
+        else {
+          for (int j = 0; j < 88 * 128; j++) {
+            b = *in++;
+            consumed++;
+            for (int n = 6; n >= 0; n--) {
+              out[i++] = b & (1 << n) ? 1 : 0;
+            }
+          }
+          b = 0x71;
+          for (int n = 7; n >= 0; n--) {
+            out[i++] = b & (1 << n) ? 1 : 0;
+          }
+          b = 0xe8;
+          for (int n = 7; n >= 0; n--) {
+            out[i++] = b & (1 << n) ? 1 : 0;
+          }
+          b = 0x4d;
+          for (int n = 7; n >= 0; n--) {
+            out[i++] = b & (1 << n) ? 1 : 0;
+          }
+          b = 0xd4;
+          for (int n = 7; n >= 0; n--) {
+            out[i++] = b & (1 << n) ? 1 : 0;
+          }
+          b = control_word << 4;
+          for (int n = 7; n >= 0; n--) {
+            out[i++] = b & (1 << n) ? 1 : 0;
+          }
+        }
       }
 
       // Tell runtime system how many input items we consumed on
       // each input stream.
-      consume_each (j);
+      consume_each (consumed);
 
       // Tell runtime system how many output items we produced.
       return noutput_items;
