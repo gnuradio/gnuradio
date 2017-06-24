742f37c2fbbd4a0abcfe2d17f3d448a05922e346
Ron Economos <w6rz@comcast.net>
2017-06-24 15:16:57 -0700

    dtv: Update ITU-T J.83B transmitter block documentation.

--- gr-dtv/include/gnuradio/dtv/catv_frame_sync_enc_bb.h
+++ gr-dtv/include/gnuradio/dtv/catv_frame_sync_enc_bb.h
@@ -29,11 +29,11 @@ namespace gr {
   namespace dtv {
 
     /*!
-     * \brief Frame Sync Encoder. Adds a 42-bit frame sync pattern with control word.
+     * \brief Frame Sync Encoder. Adds a 42-bit (64QAM) or 40-bit (256QAM) frame sync pattern with control word.
      * \ingroup dtv
      *
-     * Input: Scrambled FEC Frame packets of 60 * 128 7-bit symbols.\n
-     * Output: Scrambled FEC Frame packets of 60 * 128 7-bit symbols with 42-bit FSYNC word.
+     * Input: Scrambled FEC Frame packets of 60 * 128 (64QAM) or 88 * 128 (256QAM) 7-bit symbols.\n
+     * Output: Scrambled FEC Frame packets of 60 * 128 (64QAM) or 88 * 128 (256QAM) 7-bit symbols with 42-bit (64QAM) or 40-bit (256QAM) FSYNC word.
      */
     class DTV_API catv_frame_sync_enc_bb : virtual public gr::block
     {
@@ -43,6 +43,7 @@ namespace gr {
       /*!
        * \brief Create an ITU-T J.83B Frame Sync Encoder.
        *
+       * \param constellation 64QAM or 256QAM constellation. \n
        * \param ctrlword convolutional interleaver control word.
        */
       static sptr make(catv_constellation_t constellation, int ctrlword);
