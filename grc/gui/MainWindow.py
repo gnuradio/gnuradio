0a1d807511cd6707456a54bdde90fc915c67db7f
Sebastian Koslowski <sebastian.koslowski@gmail.com>
2017-09-30 19:22:13 +0200

    [grc] additional fixes from merging bokeh-gui (#1434)

--- grc/gui/MainWindow.py
+++ grc/gui/MainWindow.py
@@ -71,11 +71,8 @@ class MainWindow(Gtk.ApplicationWindow):
         vbox = Gtk.VBox()
         self.add(vbox)
 
-        icon_theme = gtk.icon_theme_get_default()
-        icon = icon_theme.lookup_icon("gnuradio-grc", 48, 0)
-        if not icon:
-            # Set window icon
-            self.set_icon_from_file(os.path.dirname(os.path.abspath(__file__)) + "/icon.png")
+        # Set window icon
+        self.set_icon_from_file(os.path.dirname(os.path.abspath(__file__)) + "/icon.png")
 
         # Create the menu bar and toolbar
         generate_modes = platform.get_generate_options()
