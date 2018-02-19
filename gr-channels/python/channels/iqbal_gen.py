92390470fc58673149371926cfa8e62e85898cf6
Bill Clark <saikou@vt.edu>
2018-02-19 17:08:24 -0500

    gr-channels: Updating the iq_imbal block to be able to represent transmitter or receiver IQ imbalance based on signal component offsets.

--- gr-channels/python/channels/iqbal_gen.py
+++ gr-channels/python/channels/iqbal_gen.py
@@ -13,7 +13,7 @@ import math
 
 class iqbal_gen(gr.hier_block2):
 
-    def __init__(self, magnitude=0, phase=0):
+    def __init__(self, magnitude=0, phase=0, mode=0):
         gr.hier_block2.__init__(
             self, "IQ Imbalance Generator",
             gr.io_signature(1, 1, gr.sizeof_gr_complex*1),
@@ -25,29 +25,53 @@ class iqbal_gen(gr.hier_block2):
         ##################################################
         self.magnitude = magnitude
         self.phase = phase
+        self.mode = mode
 
         ##################################################
         # Blocks
         ##################################################
-        self.mag = blocks.multiply_const_vff((math.pow(10,magnitude/20.0), ))
-        self.sin_phase = blocks.multiply_const_vff((math.sin(phase*math.pi/180.0), ))
-        self.cos_phase = blocks.multiply_const_vff((math.cos(phase*math.pi/180.0), ))
-        self.f2c = blocks.float_to_complex(1)
-        self.c2f = blocks.complex_to_float(1)
-        self.adder = blocks.add_vff(1)
+        if(self.mode):
+            # Receiver Mode
+            self.mag = blocks.multiply_const_vff((math.pow(10,magnitude/20.0), ))
+            self.sin_phase = blocks.multiply_const_vff((math.sin(phase*math.pi/180.0), ))
+            self.cos_phase = blocks.multiply_const_vff((math.cos(phase*math.pi/180.0), ))
+            self.f2c = blocks.float_to_complex(1)
+            self.c2f = blocks.complex_to_float(1)
+            self.adder = blocks.add_vff(1)
+        else:
+            # Transmitter Mode
+            self.mag = blocks.multiply_const_vff((math.pow(10,magnitude/20.0), ))
+            self.sin_phase = blocks.multiply_const_vff((math.sin(phase*math.pi/180.0), ))
+            self.cos_phase = blocks.multiply_const_vff((math.cos(phase*math.pi/180.0), ))
+            self.f2c = blocks.float_to_complex(1)
+            self.c2f = blocks.complex_to_float(1)
+            self.adder = blocks.add_vff(1)
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self, 0), (self.c2f, 0))
-        self.connect((self.c2f, 0), (self.mag, 0))
-        self.connect((self.mag, 0), (self.cos_phase, 0))
-        self.connect((self.cos_phase, 0), (self.f2c, 0))
-        self.connect((self.mag, 0), (self.sin_phase, 0))
-        self.connect((self.sin_phase, 0), (self.adder, 0))
-        self.connect((self.c2f, 1), (self.adder, 1))
-        self.connect((self.adder, 0), (self.f2c, 1))
-        self.connect((self.f2c, 0), (self, 0))
+        if(self.mode):
+            # Receiver Mode
+            self.connect((self, 0), (self.c2f, 0))
+            self.connect((self.c2f, 0), (self.cos_phase, 0))
+            self.connect((self.cos_phase, 0), (self.adder, 0))
+            self.connect((self.c2f, 1), (self.sin_phase, 0))
+            self.connect((self.sin_phase, 0), (self.adder, 1))
+            self.connect((self.adder, 0), (self.mag, 0))
+            self.connect((self.mag, 0), (self.f2c, 0))
+            self.connect((self.c2f, 1), (self.f2c, 1))
+            self.connect((self.f2c, 0), (self, 0))
+        else:
+            # Transmitter Mode
+            self.connect((self, 0), (self.c2f, 0))
+            self.connect((self.c2f, 0), (self.mag, 0))
+            self.connect((self.mag, 0), (self.cos_phase, 0))
+            self.connect((self.cos_phase, 0), (self.f2c, 0))
+            self.connect((self.mag, 0), (self.sin_phase, 0))
+            self.connect((self.sin_phase, 0), (self.adder, 0))
+            self.connect((self.c2f, 1), (self.adder, 1))
+            self.connect((self.adder, 0), (self.f2c, 1))
+            self.connect((self.f2c, 0), (self, 0))
 
 
     # QT sink close method reimplementation
