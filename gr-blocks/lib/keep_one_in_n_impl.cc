c0217d8a92aa68e4d17abe8c1ba7ed1b648db639
Felix Wunsch <felix.wunsch@kit.edu>
2018-02-28 19:55:33 +0100

    blocks: Keep 1 in N: raise on n<1, fix docs, tabs to spaces

--- gr-blocks/lib/keep_one_in_n_impl.cc
+++ gr-blocks/lib/keep_one_in_n_impl.cc
@@ -37,9 +37,9 @@ namespace gr {
 
     keep_one_in_n_impl::keep_one_in_n_impl(size_t itemsize, int n)
       : block("keep_one_in_n",
-		 io_signature::make (1, 1, itemsize),
-		 io_signature::make (1, 1, itemsize)),
-	d_count(n)
+        io_signature::make (1, 1, itemsize),
+        io_signature::make (1, 1, itemsize)),
+            d_count(n)
     {
       // To avoid bad behavior with using set_relative_rate in this block with
       // VERY large values of n, we will keep track of things ourselves. Using
@@ -53,8 +53,9 @@ namespace gr {
     void
     keep_one_in_n_impl::set_n(int n)
     {
-      if (n < 1)
-	n = 1;
+      if (n < 1) {
+        throw std::invalid_argument("N must be at least 1.");
+      }
 
       d_n = n;
       d_count = n;
@@ -67,27 +68,27 @@ namespace gr {
 
     int
     keep_one_in_n_impl::general_work(int noutput_items,
-				     gr_vector_int &ninput_items,
-				     gr_vector_const_void_star &input_items,
-				     gr_vector_void_star &output_items)
+        gr_vector_int &ninput_items,
+        gr_vector_const_void_star &input_items,
+        gr_vector_void_star &output_items)
     {
       const char *in = (const char *) input_items[0];
       char *out = (char *) output_items[0];
 
       size_t item_size = input_signature ()->sizeof_stream_item (0);
-      int	    ni = 0;
-      int	    no = 0;
+      int ni = 0;
+      int no = 0;
 
       while (ni < ninput_items[0] && no < noutput_items){
-	d_count--;
-	if (d_count <= 0){
-	  memcpy (out, in, item_size);		// copy 1 item
-	  out += item_size;
-	  no++;
-	  d_count = d_n;
-	}
-	in += item_size;
-	ni++;
+        d_count--;
+        if (d_count <= 0){
+          memcpy (out, in, item_size); // copy 1 item
+          out += item_size;
+          no++;
+          d_count = d_n;
+        }
+        in += item_size;
+        ni++;
       }
 
       // Because we have set TPP_DONT, we have to propagate the tags here manually.
@@ -96,9 +97,9 @@ namespace gr {
       std::vector<tag_t>::iterator t;
       get_tags_in_range(tags, 0, nitems_read(0), nitems_read(0)+ni);
       for(t = tags.begin(); t != tags.end(); t++) {
-	tag_t new_tag = *t;
-	new_tag.offset *= d_decim_rate;
-	add_item_tag(0, new_tag);
+        tag_t new_tag = *t;
+        new_tag.offset *= d_decim_rate;
+        add_item_tag(0, new_tag);
       }
 
       consume_each (ni);
