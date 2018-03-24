46748b4e14ac813978aa58e0697da4709ca7715b
gnieboer <gnieboer@corpcomm.net>
2018-03-24 16:59:21 -0400

    reduced startup sleep to 50ms

--- gr-zeromq/python/zeromq/qa_zeromq_sub.py
+++ gr-zeromq/python/zeromq/qa_zeromq_sub.py
@@ -50,7 +50,7 @@ class qa_zeromq_sub (gr_unittest.TestCase):
         self.tb.connect(zeromq_sub_source, sink)
 
         self.tb.start()
-        time.sleep(0.25)
+        time.sleep(0.05)
         self.pub_socket.send(src_data.tostring())
         time.sleep(0.25)
         self.tb.stop()
@@ -68,7 +68,7 @@ class qa_zeromq_sub (gr_unittest.TestCase):
         self.tb.connect(zeromq_sub_source, sink)
 
         self.tb.start()
-        time.sleep(0.25)
+        time.sleep(0.05)
         self.pub_socket.send_multipart(src_data)
         time.sleep(0.25)
         self.tb.stop()
