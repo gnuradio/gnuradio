53d831638f54592e04ffa82c272e3c9f19487fc0
A. Maitland Bottoms <bottoms@debian.org>
2018-03-27 12:51:17 +0200

    runtime: BSD shared memory and threading specifics
    From the debian patch set

--- gnuradio-runtime/lib/thread/thread.cc
+++ gnuradio-runtime/lib/thread/thread.cc
@@ -166,7 +166,7 @@ namespace gr {
 
 
 #elif defined(macintosh) || defined(__APPLE__) || defined(__APPLE_CC__) || \
-    defined(__FreeBSD__)
+  defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__GNU__)
 
 namespace gr {
   namespace thread {
