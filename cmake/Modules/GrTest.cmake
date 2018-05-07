7f0ddaf05014f06a77ce17e41568b458043bc7bb
Martin Braun <martin.braun@ettus.com>
2018-05-06 17:09:33 -0700

    cmake: Add option for Boost UTF-based C++ unit tests

--- cmake/Modules/GrTest.cmake
+++ cmake/Modules/GrTest.cmake
@@ -141,3 +141,26 @@ function(GR_ADD_TEST test_name)
     endif(WIN32)
 
 endfunction(GR_ADD_TEST)
+
+########################################################################
+# Add a C++ unit test and setup the environment for a unit test.
+# Takes the same arguments as the ADD_TEST function.
+#
+# test_name -- An identifier for your test, for usage with ctest -R
+# test_source -- Path to the .cc file
+#
+# Before calling set the following variables:
+# GR_TEST_TARGET_DEPS  - built targets for the library path
+########################################################################
+function(GR_ADD_CPP_TEST test_name test_source)
+    add_executable(${test_name} ${test_source})
+    target_link_libraries(
+        ${test_name}
+        ${GR_TEST_TARGET_DEPS}
+    )
+    set_target_properties(${test_name}
+        PROPERTIES COMPILE_DEFINITIONS "BOOST_TEST_DYN_LINK;BOOST_TEST_MAIN"
+    )
+    GR_ADD_TEST(${test_name} ${test_name})
+endfunction(GR_ADD_CPP_TEST)
+
