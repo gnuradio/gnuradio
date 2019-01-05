8ecdef41e47641f820fff73e0ad11c0890544b71
MBoerschig <code+github at boerschig dot net>
2019-01-06 00:14:06 +0100

    vocoder: Fix build on NetBSD.
    There was an #ifdef missing in lib/thread/thread.cc and in gr-vocoder
    the include directive for gsm.h contains a gsm/ prefix.
    The cmake FindGSM.cmake looks for **/include/ and **/include/gsm
    anyways.

--- gnuradio-runtime/lib/thread/thread.cc
+++ gnuradio-runtime/lib/thread/thread.cc
@@ -166,7 +166,8 @@ namespace gr {
 
 
 #elif defined(macintosh) || defined(__APPLE__) || defined(__APPLE_CC__) || \
-  defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__GNU__)
+  defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__GNU__) || \
+  defined(__NetBSD__)
 
 namespace gr {
   namespace thread {
