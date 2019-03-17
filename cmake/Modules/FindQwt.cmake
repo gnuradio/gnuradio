2b91dc27204ff0a36f75a42f517875bb93765886
Diane Bruce <db@db.net>
2019-03-17 11:06:46 -0400

    Add FreeBSD paths for qwt6

--- cmake/Modules/FindQwt.cmake
+++ cmake/Modules/FindQwt.cmake
@@ -15,10 +15,10 @@ find_path(QWT_INCLUDE_DIRS
   ${CMAKE_INSTALL_PREFIX}/include/qwt
   ${CMAKE_PREFIX_PATH}/include/qwt
   PATHS
+  /usr/local/include/qt5/qwt6
   /usr/local/include/qwt-${QWT_QT_VERSION}
   /usr/local/include/qwt
   /usr/include/qwt6
-  /usr/include/qt5/qwt
   /usr/include/qwt-${QWT_QT_VERSION}
   /usr/include/qwt
   /usr/include/${QWT_QT_VERSION}/qwt
@@ -29,8 +29,9 @@ find_path(QWT_INCLUDE_DIRS
 )
 
 find_library (QWT_LIBRARIES
-  NAMES ${PC_QWT_LIBRARIES} qwt6-${QWT_QT_VERSION} qwt-${QWT_QT_VERSION}
+  NAMES ${PC_QWT_LIBRARIES} qwt6 qwt6-${QWT_QT_VERSION} qwt-${QWT_QT_VERSION}
   HINTS
+  /usr/local/lib/qt5
   ${PC_QWT_LIBDIR}
   ${CMAKE_INSTALL_PREFIX}/lib
   ${CMAKE_INSTALL_PREFIX}/lib64
