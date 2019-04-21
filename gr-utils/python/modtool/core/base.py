e9038d6863c2eb2d55e87e4cba7283e71b2b72b2
Marcus Müller <mmueller@gnuradio.org>
2019-04-21 16:37:45 +0200

    modtool: Supply SimpleNamespace for legacy Python

--- gr-utils/python/modtool/core/base.py
+++ gr-utils/python/modtool/core/base.py
@@ -29,7 +29,22 @@ import re
 import glob
 import logging
 import itertools
-from types import SimpleNamespace
+
+try:
+    from types import SimpleNamespace
+except:
+    # Py2.7 doesn't have SimpleNamespace, soooo use what Py3 docs say is roughly equivalent
+    class SimpleNamespace:
+        def __init__(self, **kwargs):
+            self.__dict__.update(kwargs)
+
+        def __repr__(self):
+            keys = sorted(self.__dict__)
+            items = ("{}={!r}".format(k, self.__dict__[k]) for k in keys)
+            return "{}({})".format(type(self).__name__, ", ".join(items))
+
+        def __eq__(self, other):
+            return self.__dict__ == other.__dict__
 
 from gnuradio import gr
 from ..tools import get_modname, SCMRepoFactory
