c445ef15d4eb3e354927daa3c9cc2125de81dca7
Michael Dickens <michael.dickens@ettus.com>
2019-08-14 09:41:34 -0400

    blocks: simple fix for Boost 1.70.0 in socket_pdu
    NOTE: There have been multiple fixes proposed, such as https://github.com/gnuradio/gnuradio/pull/2451 .
    This one is the simplest and most compatible.
    
    This is a backport of the same fix for GR38: https://github.com/gnuradio/gnuradio/commit/c01473bf00b73ba1dd72813fbc4c4c5d0f66d339

--- gr-blocks/lib/socket_pdu_impl.cc
+++ gr-blocks/lib/socket_pdu_impl.cc
@@ -165,7 +165,11 @@ namespace gr {
     void
     socket_pdu_impl::start_tcp_accept()
     {
+#if (BOOST_VERSION >= 107000)
+      tcp_connection::sptr new_connection = tcp_connection::make(d_io_service, d_rxbuf.size(), d_tcp_no_delay);
+#else
       tcp_connection::sptr new_connection = tcp_connection::make(d_acceptor_tcp->get_io_service(), d_rxbuf.size(), d_tcp_no_delay);
+#endif
 
       d_acceptor_tcp->async_accept(new_connection->socket(),
         boost::bind(&socket_pdu_impl::handle_tcp_accept, this,
