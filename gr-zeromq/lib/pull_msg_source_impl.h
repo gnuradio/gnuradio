79b650984310430efe36b7045e759dc631073ba1
Bastian Bloessl <mail@bastibl.net>
2019-08-27 09:55:25 +0200

    zeromq: fix inconsistent use of override

--- gr-zeromq/lib/pull_msg_source_impl.h
+++ gr-zeromq/lib/pull_msg_source_impl.h
@@ -46,8 +46,8 @@ public:
     pull_msg_source_impl(char* address, int timeout);
     ~pull_msg_source_impl();
 
-    bool start();
-    bool stop();
+    bool start() override;
+    bool stop() override;
 
     std::string last_endpoint() override
     {
