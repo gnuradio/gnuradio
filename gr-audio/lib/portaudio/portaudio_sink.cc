43f7dbfe50b65d577bc066f2645ed70c0c0b758e
Marcus Müller <mmueller@gnuradio.org>
2020-01-06 00:03:03 +0100

    portaudio sink: can't use d_logger_something in non-class callback

--- gr-audio/lib/portaudio/portaudio_sink.cc
+++ gr-audio/lib/portaudio/portaudio_sink.cc
@@ -120,8 +120,8 @@ int portaudio_sink_callback(const void* inputBuffer,
         self->d_nunderuns++;
         ssize_t r = ::write(2, "aU", 2); // FIXME change to non-blocking call
         if (r == -1) {
-            GR_LOG_ERROR(d_debug_logger,
-                         boost::format("write error to stderr: %s") % strerror(errno));
+            perror("audio_portaudio_source::portaudio_source_callback write error to "
+                   "stderr.");
         }
 
         // FIXME we should transfer what we've got and pad the rest
