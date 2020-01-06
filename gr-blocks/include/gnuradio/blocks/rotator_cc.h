21e7b29a436cbac7f942d5e417a4a266617a602e
Igor Freire <igor@blockstream.com>
2020-01-06 16:00:48 -0300

    blocks: Add msg port to set rotator's phase inc
    Receivers commonly estimate the frequency offset on a block that is
    downstream relative to the frequency correction NCO. In such
    architectures, typically the frequency offset estimator feeds the
    estimation back to the NCO. Until now, this was not possible if using
    the rotator block as the NCO.
    
    This patch adds a message port to the rotator block such that the
    referred feedback architecture becomes feasible. A downstream block can
    estimate the frequency offset and send a message to the rotator block to
    update its rotating frequency (i.e., its phase increment).
    
    The requesting block can schedule the phase increment update to a
    specific absolute sample index. This feature is useful for receivers
    that rely on a frame structure and pilot-aided frequency offset
    estimation. If the true frequency offset is allowed to change at any
    random instant throughout the frame, the frame-averaged frequency offset
    estimation can become less reliable. Instead, it is often better to
    update the frequency correction right at the start of a new frame. With
    that, the true frequency offset is preserved in the course of a frame
    and only changes around the boundary between consecutive frames.
    
    For flexibility when using this feature, it is the responsibility of the
    downstream block to schedule the phase increment update properly. For
    example, if there is a decimator in between the rotator block and the
    frequency offset estimator, the latter will need to take the sample rate
    conversion into account when defining the absolute sample offset when
    the update should be applied.
    
    Besides, the rotator block can now place a tag on the sample where it
    updates the phase increment. This allows a downstream block to better
    calibrate and validate the scheduling of phase increment updates.
    
    Signed-off-by: Igor Freire <igor@blockstream.com>

--- gr-blocks/include/gnuradio/blocks/rotator_cc.h
+++ gr-blocks/include/gnuradio/blocks/rotator_cc.h
@@ -20,6 +20,23 @@ namespace blocks {
 /*!
  * \brief Complex rotator
  * \ingroup math_operators_blk
+ *
+ * \details
+ *
+ * Rotates an input complex sequence using a complex exponential in the form of
+ * exp(1j * phase_inc * n), where "phase_inc" is a chosen phase increment in
+ * radians and "n" is the sample index.
+ *
+ * Message Ports:
+ *
+ * - cmd (input):
+ *    Receives a PMT dictionary with a command message to set a new phase
+ *    increment on the rotator at a specified sample offset. The new increment
+ *    must be provided as a PMT double on a key named "inc". The target sample
+ *    offset on which to update the phase increment must be given as a PMT
+ *    uint64 (with the absolute output item number) on a key named
+ *    "offset". Unlike the "inc" key, the "offset" key is optional. When not
+ *    provided, the rotator updates its phase increment immediately.
  */
 class BLOCKS_API rotator_cc : virtual public sync_block
 {
@@ -28,10 +45,13 @@ public:
     typedef std::shared_ptr<rotator_cc> sptr;
 
     /*!
-     * \brief Make an complex rotator block
+     * \brief Make a complex rotator block
      * \param phase_inc rotational velocity
+     * \param tag_inc_updates Tag the sample where a phase increment update is
+     *                        applied following the reception of a control
+     *                        message received via the input message port.
      */
-    static sptr make(double phase_inc = 0.0);
+    static sptr make(double phase_inc = 0.0, bool tag_inc_updates = false);
 
     virtual void set_phase_inc(double phase_inc) = 0;
 };
