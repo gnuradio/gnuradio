76fc8415ba7e185c74d0a0ddbf10290acd097725
Marc Lichtman <mlichtman@perspectalabs.com>
2020-01-08 02:10:41 -0500

    Marc added in everything he had working so far on his own version

--- grc/gui_qt/views/block.py
+++ grc/gui_qt/views/block.py
@@ -5,7 +5,7 @@ from PyQt5.QtCore import Qt
 ARC        = 10  # arc radius for block corners
 LONG_VALUE = 20  # maximum length of a param string.
                  # if exceeded, '...' will be displayed
-
+'''
 class BlockTitle(QtWidgets.QLabel):
     def __init__(self, block_key):
         super(BlockTitle, self).__init__()
@@ -99,14 +99,44 @@ class BlockParams(QtWidgets.QWidget):
 
         # use the default handler for everything else
         super(BlockParams, self).paintEvent(event)
+'''
 
-
-class Block(QtWidgets.QWidget):
-    def __init__(self, block_key, attrib, params):
+class Block(QtWidgets.QGraphicsItem):
+    def __init__(self, block_key, block_label, attrib, params):
         super(Block, self).__init__()
-
         self.__dict__.update(attrib)
-
+        self.params = params
+        self.x = attrib['_coordinate'][0]
+        self.y = attrib['_coordinate'][1]
+        self.width = 300 # default shouldnt matter, it will change immedaitely after the first paint
+        self.block_key = block_key
+        self.block_label = block_label
+        
+        # figure out height of block based on how many params there are
+        i = 30
+        for row, (key, value) in enumerate(self.params):
+            if value is not None:  
+                i+= 20      
+        self.height = i 
+        
+        # figure out width of block based on widest line of text
+        fm = QtGui.QFontMetrics(QtGui.QFont('Helvetica', 10))
+        largest_width = fm.width(self.block_label)/2      
+        for row, (key, value) in enumerate(self.params):
+            if value is not None:  
+                if len(value) > LONG_VALUE:
+                    value = value[:LONG_VALUE-3] + '...'
+                if fm.width(value) > largest_width:
+                    largest_width = fm.width(value)
+                if fm.width(key + ': ') > largest_width:
+                    largest_width = fm.width(key + ': ') # the keys need a little more margin
+        self.width = largest_width*2 + 15 # the *2 is because we only measured half the width, the + 15 is margin
+                    
+        
+        self.setFlag(QtWidgets.QGraphicsItem.ItemIsMovable)
+        
+        
+        '''
         layout = QtWidgets.QVBoxLayout()
         layout.setSpacing(0)
         layout.setContentsMargins(0, 0, 0, 0)
@@ -118,8 +148,48 @@ class Block(QtWidgets.QWidget):
         palette.setColor(self.backgroundRole(), Qt.transparent)
         self.setAutoFillBackground(True)
         self.setPalette(palette)
+        '''
 
+    def paint(self, painter, option, widget):
+        # Set font
+        font = QtGui.QFont('Helvetica', 10)
+        #font.setStretch(70) # makes it more condensed
+        font.setBold(True)
+        
+        # Draw main rectangle
+        painter.setPen(QtGui.QPen(2))
+        painter.setBrush(QtGui.QBrush(QtGui.QColor(0xFA, 0xF8, 0xE0)))
+        ARC = 10
+        painter.drawRoundedRect(self.x, self.y, self.width-1, self.height, ARC, ARC);
+       
+        # Draw block label text
+        painter.setFont(font)
+        painter.drawText(QtCore.QRectF(self.x, self.y - self.height/2 + 10, self.width, self.height), Qt.AlignCenter, self.block_label)  # NOTE the 3rd/4th arg in  QRectF seems to set the bounding box of the text, so if there is ever any clipping, thats why
+   
+        # Draw param text
+        y_offset = 30 # params start 30 down from the top of the box
+        for row, (key, value) in enumerate(self.params):
+            if value is not None:
+                if len(value) > LONG_VALUE:
+                    value = value[:LONG_VALUE-3] + '...'
+                font.setBold(True)
+                painter.setFont(font)
+                painter.drawText(QtCore.QRectF(self.x - self.width/2, self.y + y_offset, self.width, self.height), Qt.AlignRight, key + ': ') 
+                font.setBold(False)
+                painter.setFont(font)
+                painter.drawText(QtCore.QRectF(self.x + self.width/2, self.y + y_offset, self.width, self.height), Qt.AlignLeft, value) 
+                y_offset += 20
 
+   
+    def boundingRect(self): # required to have
+        return QtCore.QRectF(self.x, self.y, self.width, self.height) # same as the rectangle we draw
+
+    def mouseReleaseEvent(self, e):
+        super(Block, self).mouseReleaseEvent(e)
+
+    def mouseDoubleClickEvent(self, e):
+        print("DETECTED DOUBLE CLICK!")
+        super(Block, self).mouseDoubleClickEvent(e)     
 
 
 
