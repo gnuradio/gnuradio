b1836f2b6fb06a848c744bcbe731bfd924320720
devnulling <devnulling@users.noreply.github.com>
2020-01-25 11:22:11 -0800

    Update license header to SPDX format

--- gnuradio-runtime/include/pmt/pmt_serial_tags.h
+++ gnuradio-runtime/include/pmt/pmt_serial_tags.h
@@ -4,23 +4,7 @@
 //
 // This file is part of GNU Radio
 //
-// GNU Radio is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 3, or (at your option)
-// any later version.
-//
-// GNU Radio is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License along
-// with this program; if not, write to the Free Software Foundation, Inc.,
-// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-//
-//
-// THIS FILE IS MACHINE GENERATED FROM gen-serial-tags.py. DO NOT EDIT BY HAND.
-// See pmt-serial-tags.scm for additional commentary.
+// SPDX-License-Identifier: GPL-3.0-or-later
 //
 
 #ifndef INCLUDED_PMT_SERIAL_TAGS_H
