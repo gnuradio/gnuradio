0cec8b9c30548d40d09ff8c794d87b5b9de163e4
Clayton Smith <argilo@gmail.com>
2020-02-13 21:39:33 -0500

    gr-blocks: only apply rotator workaround to VOLK 2.0.0

--- gr-blocks/lib/rotator_cc_impl.cc
+++ gr-blocks/lib/rotator_cc_impl.cc
@@ -59,7 +59,7 @@ int rotator_cc_impl::work(int noutput_items,
     const gr_complex* in = (const gr_complex*)input_items[0];
     gr_complex* out = (gr_complex*)output_items[0];
 
-#if 1
+#ifdef APPLY_BROKEN_ROTATOR_WORKAROUND
     for (int i = 0; i < noutput_items; i++)
         out[i] = d_r.rotate(in[i]);
 #else
