7bfd21dd321950696033115c0f8965b0fa8b63e3
Christophe Seguinot <christophe.seguinot@univ-lille.fr>
2020-02-20 21:34:18 +0100

    qtgui: add eye_sink to GNURadio 3.8
    This is the same code as the one written for 3.9. It works also under
    3.8.
    
    Regards, Christophe

---
+++ gr-qtgui/include/gnuradio/qtgui/eyecontrolpanel.h
@@ -0,0 +1,88 @@
+/* -*- c++ -*- */
+/*
+ * Copyright 2015 Free Software Foundation, Inc.
+ *
+ * This file is part of GNU Radio
+ *
+ * GNU Radio is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * GNU Radio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Radio; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef EYE_CONTROL_PANEL_H
+#define EYE_CONTROL_PANEL_H
+
+#include <gnuradio/qtgui/eyedisplayform.h>
+#include <gnuradio/qtgui/eyedisplaysform.h>
+#include <QtGui/QtGui>
+#include <QCheckBox>
+#include <QComboBox>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QPushButton>
+#include <QSlider>
+#include <vector>
+
+class EyeControlPanel : public QVBoxLayout
+{
+    Q_OBJECT
+
+public:
+    EyeControlPanel(EyeDisplayForm* form);
+    ~EyeControlPanel();
+
+public slots:
+    void toggleAutoScale(bool en);
+    void toggleGrid(bool en);
+    void toggleTriggerMode(gr::qtgui::trigger_mode mode);
+    void toggleTriggerSlope(gr::qtgui::trigger_slope slope);
+    void toggleStopButton();
+
+signals:
+    void signalToggleStopButton();
+
+private:
+    EyeDisplayForm* d_parent;
+    QGroupBox* d_axes_box;
+    QGroupBox* d_trigger_box;
+    QGroupBox* d_extras_box;
+
+    QVBoxLayout* d_axes_layout;
+    QHBoxLayout* d_yoff_layout;
+    QHBoxLayout* d_yrange_layout;
+    QVBoxLayout* d_trigger_layout;
+    QHBoxLayout* d_trigger_level_layout;
+    QHBoxLayout* d_trigger_delay_layout;
+    QVBoxLayout* d_extras_layout;
+
+    QLabel* d_yoff_label;
+    QLabel* d_yrange_label;
+    QLabel* d_trigger_level_label;
+    QLabel* d_trigger_delay_label;
+
+    QCheckBox* d_autoscale_check;
+    QCheckBox* d_grid_check;
+    QCheckBox* d_axislabels_check;
+
+    QPushButton *d_yoff_plus, *d_yoff_minus;
+    QPushButton *d_yrange_plus, *d_yrange_minus;
+    QComboBox* d_trigger_mode_combo;
+    QComboBox* d_trigger_slope_combo;
+    QPushButton *d_trigger_level_plus, *d_trigger_level_minus;
+    QPushButton *d_trigger_delay_plus, *d_trigger_delay_minus;
+    QPushButton* d_autoscale_button;
+    QPushButton* d_stop_button;
+};
+
+#endif /* EYE_CONTROL_PANEL_H */
--- gr-qtgui/python/qtgui/qa_qtgui.py
+++ gr-qtgui/python/qtgui/qa_qtgui.py
@@ -4,20 +4,8 @@
 #
 # This file is part of GNU Radio
 #
-# GNU Radio is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3, or (at your option)
-# any later version.
+# SPDX-License-Identifier: GPL-3.0-or-later
 #
-# GNU Radio is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with GNU Radio; see the file COPYING.  If not, write to
-# the Free Software Foundation, Inc., 51 Franklin Street,
-# Boston, MA 02110-1301, USA.
 #
 
 
@@ -80,6 +68,12 @@ class test_qtgui(gr_unittest.TestCase):
 
     def test12(self):
         self.qtsnk = qtgui.histogram_sink_f(1024, 100, -1, 1, "Test", 1)
+
+    def test13(self):
+        self.qtsnk = qtgui.eye_sink_c(1024, 1, "Test", 1)
+
+    def test14(self):
+        self.qtsnk = qtgui.eye_sink_c(1024, 1, "Test", 1)
 
 if __name__ == '__main__':
     gr_unittest.run(test_qtgui, "test_qtgui.xml")
