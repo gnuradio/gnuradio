dcc8279e55a457cfecace9fc13a75f41d509fa88
alekhgupta1441 <alekhgupta1441@gmail.com>
2020-03-13 01:49:58 +0530

    gr-utils:'#'replaced by '%' for commenting in codes.

--- gr-utils/octave/single_pole_iir.m
+++ gr-utils/octave/single_pole_iir.m
@@ -1,11 +1,11 @@
-#
-# Copyright 2002 Free Software Foundation, Inc.
-#
-# This file is part of GNU Radio
-#
-# SPDX-License-Identifier: GPL-3.0-or-later
-#
-#
+%
+% Copyright 2002 Free Software Foundation, Inc.
+%
+% This file is part of GNU Radio
+%
+% SPDX-License-Identifier: GPL-3.0-or-later
+%
+%
 
 function [b, a] = single_pole_iir (alpha)
   b = [alpha];
--- gr-utils/octave/write_short_binary.m
+++ gr-utils/octave/write_short_binary.m
@@ -1,18 +1,18 @@
-#
-# Copyright 2001 Free Software Foundation, Inc.
-#
-# This file is part of GNU Radio
-#
-# SPDX-License-Identifier: GPL-3.0-or-later
-#
-#
+%
+% Copyright 2001 Free Software Foundation, Inc.
+%
+% This file is part of GNU Radio
+%
+% SPDX-License-Identifier: GPL-3.0-or-later
+%
+%
 
 function v = write_short_binary (data, filename)
 
-  ## usage: write_short_binary (data, filename)
-  ##
-  ##  open filename and write data to it as 16 bit shorts
-  ##
+  %% usage: write_short_binary (data, filename)
+  %%
+  %%  open filename and write data to it as 16 bit shorts
+  %%
 
   if ((m = nargchk (2,2,nargin)))
     usage (m);
