d09576b9107b086f70edb4a96fbbc8bc11f82fbc
Thomas Habets <thomas@habets.se>
2020-03-20 12:19:33 +0000

    Change assert to static_assert, where possible
    These are all ones I could find, so assert->static_assert should now
    be done.

--- gr-dtv/lib/atsc/atsc_trellis_encoder_impl.cc
+++ gr-dtv/lib/atsc/atsc_trellis_encoder_impl.cc
@@ -65,8 +65,8 @@ void atsc_trellis_encoder_impl::encode(atsc_data_segment out[NCODERS],
     unsigned char out_copy[OUTPUT_SIZE];
     unsigned char in_copy[INPUT_SIZE];
 
-    assert(sizeof(in_copy) == sizeof(in[0].data) * NCODERS);
-    assert(sizeof(out_copy) == sizeof(out[0].data) * NCODERS);
+    static_assert(sizeof(in_copy) == sizeof(in[0].data) * NCODERS, "wrong type size");
+    static_assert(sizeof(out_copy) == sizeof(out[0].data) * NCODERS, "wrong type size");
 
     // copy input into contiguous temporary buffer
     for (int i = 0; i < NCODERS; i++) {
@@ -207,7 +207,8 @@ void atsc_trellis_encoder_impl::encode_helper(unsigned char output[OUTPUT_SIZE],
     }         /* Chunks */
 
     /* Check up on ourselves */
-    assert(0 == (INPUT_SIZE * DIBITS_PER_BYTE) % NCODERS);
+    static_assert(0 == (INPUT_SIZE * DIBITS_PER_BYTE) % NCODERS,
+                  "size not divisible by NCODERS");
     assert(OUTPUT_SIZE == out - output);
     assert(NCODERS - ENCODER_SEG_BUMP == encoder);
 }
