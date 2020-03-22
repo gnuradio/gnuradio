5637375ebf5928458a5d970fb09df3a9113b29fd
Thomas Habets <thomas@habets.se>
2020-03-22 18:05:56 +0000

    blocks/udp_sink: Remove manual memory management

--- gr-blocks/lib/udp_sink_impl.h
+++ gr-blocks/lib/udp_sink_impl.h
@@ -13,6 +13,7 @@
 
 #include <gnuradio/blocks/udp_sink.h>
 #include <boost/asio.hpp>
+#include <memory>
 
 namespace gr {
 namespace blocks {
@@ -27,7 +28,7 @@ private:
     bool d_connected;          // are we connected?
     gr::thread::mutex d_mutex; // protects d_socket and d_connected
 
-    boost::asio::ip::udp::socket* d_socket; // handle to socket
+    std::unique_ptr<boost::asio::ip::udp::socket> d_socket; // handle to socket
     boost::asio::ip::udp::endpoint d_endpoint;
     boost::asio::io_service d_io_service;
 
