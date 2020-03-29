ab2f82fdc435d4278545eacb1aa9c5c7303ad9c1
rear1019 <rear1019@posteo.de>
2020-03-29 11:04:24 +0200

    file_sink: Flush file on stop()
    Make sure that data passed to the file sink is written to the disk when
    the flowgraph is stopped. Before this change this only happened
    implicitly due to a fclose() in the base class destructor. This in turn
    means that data was written delayed after stopping the flowgraph [1].
    
    This fixes #2590.
    
    [1] https://github.com/gnuradio/gnuradio/issues/2590

--- gr-blocks/lib/file_sink_impl.h
+++ gr-blocks/lib/file_sink_impl.h
@@ -28,6 +28,8 @@ public:
     int work(int noutput_items,
              gr_vector_const_void_star& input_items,
              gr_vector_void_star& output_items);
+
+    bool stop() override;
 };
 
 } /* namespace blocks */
