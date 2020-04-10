7bc903849652494097c60f326ebd9f15394e5f7f
Marcus Müller <mmueller@gnuradio.org>
2020-04-11 01:01:53 +0200

    analog/dpll: remove unused printf debugging, include

--- gr-analog/lib/dpll_bb_impl.cc
+++ gr-analog/lib/dpll_bb_impl.cc
@@ -14,7 +14,6 @@
 
 #include "dpll_bb_impl.h"
 #include <gnuradio/io_signature.h>
-#include <cstdio>
 
 namespace gr {
 namespace analog {
@@ -34,13 +33,6 @@ dpll_bb_impl::dpll_bb_impl(float period, float gain)
     d_pulse_frequency = 1.0 / period;
     d_gain = gain;
     d_decision_threshold = 1.0 - 0.5 * d_pulse_frequency;
-#if 0
-      fprintf(stderr,"frequency = %f period = %f gain = %f threshold = %f\n",
-	      d_pulse_frequency,
-	      period,
-	      d_gain,
-	      d_decision_threshold);
-#endif
 }
 
 dpll_bb_impl::~dpll_bb_impl() {}
