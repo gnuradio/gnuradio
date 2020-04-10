84ea741d6c48f81ba32894a763db08c45878bfa3
alekhgupta1441 <alekhgupta1441@gmail.com>
2020-04-10 22:39:50 +0530

    Code Cleanup cli files

--- gr-utils/modtool/cli/base.py
+++ gr-utils/modtool/cli/base.py
@@ -31,8 +31,7 @@ class ModToolException(ClickException):
     """ Exception class for enhanced CLI interface """
     def show(self, file = None):
         """ displays the colored message """
-        mesg_=self.format_message()
-        click.secho(f'ModToolException: {mesg_}', fg='red')
+        click.secho(f'ModToolException: {self.format_message()}', fg='red')
 
 
 class CommandCLI(click.Group):
