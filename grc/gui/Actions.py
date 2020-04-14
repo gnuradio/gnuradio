037d2934d8691de932f4ebf00ff647e67db1bd9c
Davide Gerhard <rainbow@irh.it>
2020-04-14 10:01:33 +0200

    gnuradio-companion: add Meta modifier key for macOS
    implements support for the Meta key on macOS.
    Others platforms continue to use Ctrl.

--- grc/gui/Actions.py
+++ grc/gui/Actions.py
@@ -24,6 +24,7 @@ import logging
 
 from gi.repository import Gtk, Gdk, Gio, GLib, GObject
 
+from . import Utils
 
 log = logging.getLogger(__name__)
 
@@ -136,7 +137,10 @@ class Action(Gio.SimpleAction):
         self.label = label
         self.tooltip = tooltip
         self.icon_name = icon_name
-        self.keypresses = keypresses
+        if keypresses:
+            self.keypresses = [kp.replace("<Ctrl>", Utils.get_modifier_key(True)) for kp in keypresses]
+        else:
+            self.keypresses = None
         self.prefix = prefix
         self.preference_name = preference_name
         self.default = default
