26a35ea30166243d1f54fac919158121c1b87f29
Brett Gottula <brett@astranis.com>
2020-04-21 15:39:52 -0700

    filter: Fix firdes RRC filter gain for alpha == 1
    * Fix gain of firdes.root_raised_cosine() when alpha is set to 1.0. Was
      2x the expected gain for alpha==1.0 previously.
    * Adds corresponding unit test.

--- gr-filter/lib/firdes.cc
+++ gr-filter/lib/firdes.cc
@@ -582,6 +582,7 @@ vector<float> firdes::root_raised_cosine(
         } else {
             if (alpha == 1) {
                 taps[i] = -1;
+                scale += taps[i];
                 continue;
             }
             x3 = (1 - alpha) * x1;
