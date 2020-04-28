b9169c0c773eb3e035bda38a99847d88b566176e
Josh Morman <mormjb@gmail.com>
2020-04-28 12:25:46 -0400

    digital: add python bindings for equalizers+modulate_vector

---
+++ gr-digital/python/digital/bindings/docstrings/linear_equalizer_pydoc_template.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2020 Free Software Foundation, Inc.
+ *
+ * This file is part of GNU Radio
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ */
+#include "pydoc_macros.h"
+#define D(...) DOC(gr, digital, __VA_ARGS__)
+/*
+  This file contains placeholders for docstrings for the Python bindings.
+  Do not edit! These were automatically extracted during the binding process
+  and will be overwritten during the build process
+ */
+
+
+static const char* __doc_gr_digital_linear_equalizer = R"doc()doc";
+
+
+static const char* __doc_gr_digital_linear_equalizer_linear_equalizer_0 = R"doc()doc";
+
+
+static const char* __doc_gr_digital_linear_equalizer_linear_equalizer_1 = R"doc()doc";
+
+
+static const char* __doc_gr_digital_linear_equalizer_make = R"doc()doc";
+
+
+static const char* __doc_gr_digital_linear_equalizer_set_taps = R"doc()doc";
+
+
+static const char* __doc_gr_digital_linear_equalizer_taps = R"doc()doc";
+
+
+static const char* __doc_gr_digital_linear_equalizer_equalize = R"doc()doc";
