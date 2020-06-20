75286c00a3ff8175a05a9a6d2a1482b05ff8d95b
Marcus Müller <mmueller@gnuradio.org>
2020-06-21 01:50:42 +0200

    pybind: include and enable realtime_impl wrapping
    This solves the issue of gr.enable_realtime_scheduling() failing because
    rt_status_t wasn't wrapped without.

--- gnuradio-runtime/python/gnuradio/gr/bindings/python_bindings.cc
+++ gnuradio-runtime/python/gnuradio/gr/bindings/python_bindings.cc
@@ -56,7 +56,7 @@ void bind_prefs(py::module&);
 // void bind_pycallback_object(py::module&);
 void bind_random(py::module&);
 void bind_realtime(py::module&);
-// void bind_realtime_impl(py::module&);
+void bind_realtime_impl(py::module&);
 // void bind_rpcbufferedget(py::module&);
 // void bind_rpccallbackregister_base(py::module&);
 // void bind_rpcmanager(py::module&);
@@ -157,7 +157,7 @@ PYBIND11_MODULE(gr_python, m)
     // // bind_pycallback_object(m);
     bind_random(m);
     bind_realtime(m);
-    // // bind_realtime_impl(m);
+    bind_realtime_impl(m);
     // // bind_rpcbufferedget(m);
     // // bind_rpccallbackregister_base(m);
     // // bind_rpcmanager(m);
