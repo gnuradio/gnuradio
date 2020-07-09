585d3578089404a1ed36b46cc009e75a36f39526
Niki <niki@aveer.io>
2020-07-09 17:27:52 -0400

    uhd: Add GPIO handlers for messages
    This allows toggling GPIOs via messages.
    
    Signed-off-by: Martin Braun <martin@gnuradio.org>

--- gr-uhd/lib/usrp_block_impl.h
+++ gr-uhd/lib/usrp_block_impl.h
@@ -127,6 +127,7 @@ protected:
     void _cmd_handler_bw(const pmt::pmt_t& bw, int chan, const pmt::pmt_t& msg);
     void _cmd_handler_lofreq(const pmt::pmt_t& lofreq, int chan, const pmt::pmt_t& msg);
     void _cmd_handler_dspfreq(const pmt::pmt_t& dspfreq, int chan, const pmt::pmt_t& msg);
+    void _cmd_handler_gpio(const pmt::pmt_t& gpio_attr, int chan, const pmt::pmt_t& msg);
 
     /**********************************************************************
      * Helpers
