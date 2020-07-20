a0faef10ee3691719ea17e5377a0d28f186a5cb4
Sebastian Koslowski <sebastian.koslowski@gmail.com>
2020-07-20 19:31:45 +0200

    grc: clean-up flags in epy module

--- grc/core/blocks/embedded_python.py
+++ grc/core/blocks/embedded_python.py
@@ -10,6 +10,7 @@ from textwrap import dedent
 
 from . import Block, register_build_in
 from ._templates import MakoTemplates
+from ._flags import Flags
 
 from .. import utils
 from ..base import Element
@@ -218,15 +219,14 @@ class EPyModule(Block):
         to set parameters of other blocks in your flowgraph.
     """)}
 
-    epy_flags = Block.flags
-    epy_flags.set(epy_flags.SHOW_ID)
+    flags = Flags(Flags.SHOW_ID)
 
     parameters_data = build_params(
         params_raw=[
             dict(label='Code', id='source_code', dtype='_multiline_python_external',
                  default='# this module will be imported in the into your flowgraph',
                  hide='part')
-        ], have_inputs=False, have_outputs=False, flags=epy_flags, block_id=key
+        ], have_inputs=False, have_outputs=False, flags=flags, block_id=key
     )
 
     templates = MakoTemplates(
