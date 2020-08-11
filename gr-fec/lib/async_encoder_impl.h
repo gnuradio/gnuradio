188c3d3ba780a72227e3076a0a18ffcf10a7be82
Thomas Habets <thomas@habets.se>
2020-08-11 19:34:56 +0100

    fec/async_encoder: Remove manual memory management

--- gr-fec/lib/async_encoder_impl.h
+++ gr-fec/lib/async_encoder_impl.h
@@ -14,6 +14,7 @@
 #include <gnuradio/blocks/pack_k_bits.h>
 #include <gnuradio/blocks/unpack_k_bits.h>
 #include <gnuradio/fec/async_encoder.h>
+#include <volk/volk_alloc.hh>
 
 namespace gr {
 namespace fec {
@@ -26,16 +27,16 @@ private:
     pmt::pmt_t d_in_port;
     pmt::pmt_t d_out_port;
 
-    blocks::kernel::unpack_k_bits* d_unpack;
-    blocks::kernel::pack_k_bits* d_pack;
+    blocks::kernel::unpack_k_bits d_unpack;
+    blocks::kernel::pack_k_bits d_pack;
 
     bool d_packed;
     bool d_rev_unpack;
     bool d_rev_pack;
     int d_mtu;
 
-    uint8_t* d_bits_in;
-    uint8_t* d_bits_out;
+    volk::vector<uint8_t> d_bits_in;
+    volk::vector<uint8_t> d_bits_out;
 
     void encode_packed(pmt::pmt_t msg);
     void encode_unpacked(pmt::pmt_t msg);
