93d153a3e19adadab50b49289df680d12b90d633
Clayton Smith <argilo@gmail.com>
2020-10-06 22:31:13 -0400

    gru: remove unused files

--- gnuradio-runtime/python/gnuradio/gru/__init__.py
+++ gnuradio-runtime/python/gnuradio/gru/__init__.py
@@ -5,9 +5,5 @@ from .daemon import *
 from .freqz import *
 from .gnuplot_freqz import *
 from .hexint import *
-from .listmisc import *
 from .mathmisc import *
 from .msgq_runner import *
-from .os_read_exactly import *
-from .seq_with_cursor import *
-from .socket_stuff import *
