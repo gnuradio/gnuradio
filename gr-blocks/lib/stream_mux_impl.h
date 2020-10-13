31c9168cf4dfc2f627bc6356121e099134fc4257
David Pi <david.pinho@gmail.com>
2020-10-13 16:18:13 +0100

    gr-blocks: Stream Mux C++ improvements
    - replace C-style cast with reinterpret_cast<>
    - make some variables const
    - prefer in-class initializers for static data
    - avoid copies in for loop by using a reference

--- gr-blocks/lib/stream_mux_impl.h
+++ gr-blocks/lib/stream_mux_impl.h
@@ -20,9 +20,9 @@ class BLOCKS_API stream_mux_impl : public stream_mux
 {
 private:
     const size_t d_itemsize;
-    unsigned int d_stream;         // index of currently selected stream
-    int d_residual;                // number if items left to put into current stream
-    const gr_vector_int d_lengths; // number if items to pack per stream
+    unsigned int d_stream{ 0 };    // index of currently selected stream
+    int d_residual{ 0 };           // number of items left to put into current stream
+    const gr_vector_int d_lengths; // number of items to pack per stream
 
     void forecast(int noutput_items, gr_vector_int& ninput_items_required);
 
