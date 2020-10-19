0eadd73d5cf8c18ee92caa6dc8b40499b6cbc434
Clayton Smith <argilo@gmail.com>
2020-10-19 07:02:25 -0400

    vocoder: allow input & output rates to differ
    The input & output rates of the FreeDV modulator aren't necessarily the 
    same. In particular, the 2400A and 2400B modes have an input rate of 
    8000 sps and an output rate of 48000 sps. To allow for arbitrary input & 
    output rates, I've changed freedv_tx_ss from sync_block to block.

--- gr-vocoder/lib/freedv_tx_ss_impl.h
+++ gr-vocoder/lib/freedv_tx_ss_impl.h
@@ -50,9 +50,12 @@ public:
     void set_tx_bpf(int val);
 
     // Where all the action really happens
-    int work(int noutput_items,
-             gr_vector_const_void_star& input_items,
-             gr_vector_void_star& output_items);
+    void forecast(int noutput_items, gr_vector_int& ninput_items_required);
+
+    int general_work(int noutput_items,
+                     gr_vector_int& ninput_items,
+                     gr_vector_const_void_star& input_items,
+                     gr_vector_void_star& output_items);
 };
 
 } /* namespace vocoder */
