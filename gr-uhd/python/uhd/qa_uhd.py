05f09299f3ca1afe5c7965b5407bd23353783067
Martin Braun <martin.braun@ettus.com>
2021-01-06 16:45:17 +0100

    uhd: Fix unit test
    The qa_uhd.py test has been broken for a long time. Now fixed.
    
    Signed-off-by: Martin Braun <martin@gnuradio.org>

--- gr-uhd/python/uhd/qa_uhd.py
+++ gr-uhd/python/uhd/qa_uhd.py
@@ -6,11 +6,11 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
-#
-
+"""
+gr-uhd sanity checking
+"""
 
 from gnuradio import gr, gr_unittest, uhd
-
 
 class test_uhd(gr_unittest.TestCase):
 
@@ -37,11 +37,9 @@ class test_uhd(gr_unittest.TestCase):
         """
         Try to manipulate the stream args channels for proper swig'ing checks.
         """
-        # FIXME: stream_args_t.channels.append does not work due to copy operation of STL vectors
-        # Needs to either change API, remove QA test, or somehow remap append()
+        # FYI: stream_args_t.channels.append does not work due to copy operation of STL vectors
         sa = uhd.stream_args_t()
-        sa.channels.append(1)
-        sa.channels.append(0)
+        sa.channels = [1, 0]
         print(sa.channels)
         self.assertEqual(len(sa.channels), 2)
         self.assertEqual(sa.channels[0], 1)
