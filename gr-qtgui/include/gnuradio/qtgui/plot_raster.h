fe39bddf215938a55f143a84114387a91f9293a0
luz paz <luzpaz@users.noreply.github.com>
2021-01-29 15:47:34 -0500

    Fix typos
    Found via `codespell v2.1.dev0`
    `codespell -q 3 -L ans,fo,hist,inout,ist,ith,nd,sinc,uint -S ./volk`
    
    Signed-off-by: luz paz <luzpaz@users.noreply.github.com>
    Signed-off-by: Josh Morman <jmorman@perspectalabs.com>

--- gr-qtgui/include/gnuradio/qtgui/plot_raster.h
+++ gr-qtgui/include/gnuradio/qtgui/plot_raster.h
@@ -27,7 +27,7 @@ class QwtColorMap;
  * \ingroup qtgui_blk
  *
  * \details
- * A time raster displays three-dimenional data, where the 3rd dimension
+ * A time raster displays three-dimensional data, where the 3rd dimension
  * (the intensity) is displayed using colors. The colors are calculated
  * from the values using a color map.
  *
