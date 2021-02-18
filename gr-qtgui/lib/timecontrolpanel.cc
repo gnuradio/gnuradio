2825bf218642ffcb0717d48cffe03e4770528383
Jason Uher <jason.uher@jhuapl.edu>
2021-02-18 23:03:43 +0000

    qtgui: toggle label&icon on plot play/stop button
    Makes two changes:
      - First, toggle the label of the 'stop' button depending on the state in
        the same way the context menu does
      - Second, adds a visual 'play' or 'stop' icon based on the action
    
    Signed-off-by: Jason Uher <jason.uher@jhuapl.edu>
    (cherry picked from commit 7d9c4159ff4481dfcbcdb07ca94426a94b0dafba)
    
    re-clang
    
    Signed-off-by: Jason Uher <jason.uher@jhuapl.edu>
    (cherry picked from commit cbc9344475873034096acd308d91cda994dfbfa9)
    
    Signed-off-by: Jeff Long <willcode4@gmail.com>

--- gr-qtgui/lib/timecontrolpanel.cc
+++ gr-qtgui/lib/timecontrolpanel.cc
@@ -88,7 +88,8 @@ TimeControlPanel::TimeControlPanel(TimeDisplayForm* form) : QVBoxLayout(), d_par
     d_extras_box = new QGroupBox("Extras");
     d_extras_layout = new QVBoxLayout;
     d_autoscale_button = new QPushButton("Autoscale");
-    d_stop_button = new QPushButton("Stop");
+    d_stop_button = new QPushButton(
+        QApplication::style()->standardIcon(QStyle::SP_MediaStop), "Stop");
     d_stop_button->setCheckable(true);
 
     // Set up the boxes into the layout
@@ -156,7 +157,12 @@ TimeControlPanel::TimeControlPanel(TimeDisplayForm* form) : QVBoxLayout(), d_par
 
     connect(
         d_autoscale_button, SIGNAL(pressed(void)), d_parent, SLOT(autoScaleShot(void)));
+
+    // Handle the start/stop button
+    // Call the base class' stop function when they press the button
     connect(d_stop_button, SIGNAL(pressed(void)), d_parent, SLOT(setStop(void)));
+    // Updated the button state regardless of who changed it
+    connect(d_stop_button, SIGNAL(toggled(bool)), this, SLOT(updateStopLabel(bool)));
     connect(
         this, SIGNAL(signalToggleStopButton(void)), d_stop_button, SLOT(toggle(void)));
 }
@@ -188,3 +194,14 @@ void TimeControlPanel::toggleTriggerSlope(gr::qtgui::trigger_slope slope)
 }
 
 void TimeControlPanel::toggleStopButton() { emit signalToggleStopButton(); }
+
+void TimeControlPanel::updateStopLabel(bool on)
+{
+    if (on) {
+        d_stop_button->setText("Start");
+        d_stop_button->setIcon(QApplication::style()->standardIcon(QStyle::SP_MediaPlay));
+    } else {
+        d_stop_button->setText("Stop");
+        d_stop_button->setIcon(QApplication::style()->standardIcon(QStyle::SP_MediaStop));
+    }
+}
