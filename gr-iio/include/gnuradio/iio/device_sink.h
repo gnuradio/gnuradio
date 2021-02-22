5e093b528c549d7cd11aec71895b1a1e435c08c0
Adam Horden <adam.horden@horden.engineering>
2021-02-22 22:22:40 +0000

    feature: gr-iio
    Co-authored-by: Edward Kigwana <ekigwana@scires.com>
    Co-authored-by: Travis Collins <travis.collins@analog.com>
    Signed-off-by: Adam Horden <adam.horden@horden.engineering>
    Signed-off-by: Bernard Tyers - Sane UX Design <bernard+work@ei8fdb.org>

---
+++ gr-iio/include/gnuradio/iio/device_sink.h
@@ -0,0 +1,79 @@
+/* -*- c++ -*- */
+/*
+ * Copyright 2014 Analog Devices Inc.
+ * Author: Paul Cercueil <paul.cercueil@analog.com>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ */
+
+
+#ifndef INCLUDED_IIO_DEVICE_SINK_H
+#define INCLUDED_IIO_DEVICE_SINK_H
+
+#include <gnuradio/iio/api.h>
+#include <gnuradio/sync_block.h>
+
+#define DEFAULT_BUFFER_SIZE 0x8000
+
+extern "C" {
+struct iio_context;
+};
+
+namespace gr {
+namespace iio {
+
+/*!
+ * \brief Generic sink for IIO drivers with buffered input channels
+ * \ingroup iio
+ *
+ * \details
+ * This block allows for streaming data to any IIO driver which has input
+ * scan elements or buffered channels.
+ */
+class IIO_API device_sink : virtual public gr::sync_block
+{
+public:
+    typedef std::shared_ptr<device_sink> sptr;
+
+    /*!
+     * \brief Return a shared_ptr to a new instance of iio::device.
+     *
+     * \param uri  String of the context uri
+     * \param device  String of device name
+     * \param channels  Vector of strings of channels names
+     * \param device_phy String of phy device name where attribute updates are
+     *        applied
+     * \param params  Vector of strings of attributes to set in form:
+     *        "<attribute name>=<value to set>,<attribute name>=<value to set>"
+     * \param buffer_size Integer number of samples to be put into each IIO
+     *        buffered passed to hardware.
+     * \param interpolation Integer number of zeros to insert into transmit
+     *        transmit buffers between samples
+     * \param cyclic Boolean when True sends first buffer_size number of samples
+     *        to hardware which is repeated in the hardware itself. Future
+     *        samples are ignored.
+     */
+    static sptr make(const std::string& uri,
+                     const std::string& device,
+                     const std::vector<std::string>& channels,
+                     const std::string& device_phy,
+                     const std::vector<std::string>& params,
+                     unsigned int buffer_size = DEFAULT_BUFFER_SIZE,
+                     unsigned int interpolation = 0,
+                     bool cyclic = false);
+
+    static sptr make_from(struct iio_context* ctx,
+                          const std::string& device,
+                          const std::vector<std::string>& channels,
+                          const std::string& device_phy,
+                          const std::vector<std::string>& params,
+                          unsigned int buffer_size = DEFAULT_BUFFER_SIZE,
+                          unsigned int interpolation = 0,
+                          bool cyclic = false);
+};
+
+} // namespace iio
+} // namespace gr
+
+#endif /* INCLUDED_IIO_DEVICE_SINK_H */
---
+++ gr-iio/lib/attr_source_impl.h
@@ -0,0 +1,74 @@
+/* -*- c++ -*- */
+/*
+ * Copyright 2018 Analog Devices Inc.
+ * Author: Travis Collins <travis.collins@analog.com>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ */
+
+#ifndef INCLUDED_IIO_ATTR_SOURCE_IMPL_H
+#define INCLUDED_IIO_ATTR_SOURCE_IMPL_H
+
+#include <gnuradio/iio/attr_source.h>
+#include <vector>
+
+#include "device_source_impl.h"
+
+namespace gr {
+namespace iio {
+
+class attr_source_impl : public attr_source
+{
+private:
+    std::string device;
+    std::string channel;
+    std::string uri;
+    std::string attribute;
+    int update_interval_ms;
+    int samples_per_update;
+    int attr_type;
+    int data_type;
+    bool output;
+    int ret;
+    uint32_t address;
+    bool required_enable;
+
+protected:
+    struct iio_context* ctx;
+    struct iio_device* dev;
+    struct iio_channel* chan;
+
+public:
+    attr_source_impl(const std::string& uri,
+                     const std::string& device,
+                     const std::string& channel,
+                     const std::string& attribute,
+                     int update_interval_ms,
+                     int samples_per_update,
+                     int data_type,
+                     int attr_type,
+                     bool output,
+                     uint32_t address,
+                     bool required_enable);
+    ~attr_source_impl();
+
+    // Where all the action really happens
+    int work(int noutput_items,
+             gr_vector_const_void_star& input_items,
+             gr_vector_void_star& output_items);
+
+    size_t type_sizeof(int data_type, int attr_type);
+    void get_attribute_data(const std::string& attribute, double* value);
+    void get_attribute_data(const std::string& attribute, float* value);
+    void get_attribute_data(const std::string& attribute, long long* value);
+    void get_attribute_data(const std::string& attribute, int* value);
+    void get_attribute_data(const std::string& attribute, uint8_t* value);
+    void get_register_data(uint32_t address, int* value);
+    void check(int ret);
+};
+
+} // namespace iio
+} // namespace gr
+
+#endif /* INCLUDED_IIO_ATTR_SOURCE_IMPL_H */
