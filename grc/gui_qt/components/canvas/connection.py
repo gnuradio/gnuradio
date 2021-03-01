95ff66b0e4e9dbcc062648831dd86c2b79b3f5e1
Håkon Vågsether <hakon.vagsether@gmail.com>
2021-03-01 14:30:10 +0100

    grcqt: Use QGraphicsLineItem for connections
    Signed-off-by: Håkon Vågsether <hakon.vagsether@gmail.com>

--- grc/gui_qt/components/canvas/connection.py
+++ grc/gui_qt/components/canvas/connection.py
@@ -4,17 +4,17 @@ from PyQt5.QtCore import Qt
 
 from ....core.Connection import Connection as CoreConnection
 
-class Connection(CoreConnection, QtWidgets.QGraphicsItem):
+class Connection(CoreConnection, QtWidgets.QGraphicsLineItem):
 
-    def __init__(self, parent, source , sink):
+    def __init__(self, parent, source, sink):
         CoreConnection.__init__(self, parent, source, sink)
         QtWidgets.QGraphicsItem.__init__(self)
         parent.addItem(self)
-        
+
         self.source = source
         self.sink = sink
 
-        self._line = []
+        self._line = QtCore.QLineF(source.scenePos(), sink.scenePos())
         self._line_width_factor = 1.0
         self._color1 = self._color2 = None
 
@@ -25,17 +25,7 @@ class Connection(CoreConnection, QtWidgets.QGraphicsItem):
         self._current_cr = None  # for what_is_selected() of curved line
         self._line_path = None
 
-    def paint(self, painter, option, widget):
-        pen = QtGui.QPen(1)
-        pen.setWidth(2)
-        painter.setPen(pen)
-
-        x0, y0 = tuple(self.source.parent.states['coordinate'])
-        x1, y1 = tuple(self.sink.parent.states['coordinate'])
-        painter.drawLine(x0, y0, x1, y1)
-
-
-    def boundingRect(self): # required to have
-        x0, y0 = tuple(self.source.parent.states['coordinate'])
-        x1, y1 = tuple(self.sink.parent.states['coordinate'])
-        return QtCore.QRectF(x0,y0,x1,y1)
+    def updateLine(self):
+        self._line.setP1(self.source.scenePos())
+        self._line.setP2(self.sink.scenePos())
+        self.setLine(self._line)
