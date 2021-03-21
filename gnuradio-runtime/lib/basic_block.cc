08015f91c871edbacb17357855a4a23ceddf6dd4
Marcus Müller <mmueller@gnuradio.org>
2021-03-21 18:18:25 +0100

    runtime: remove ready message receiver queue remnant from basic_block
    5197311dd22744b784ff9ae2cd965a26ef04f4a4 removed the blocking wait, which utilized this. Time for this to go.
    
    Signed-off-by: Marcus Müller <mmueller@gnuradio.org>
    (cherry picked from commit 9ff812715657f964e7a7a7256c4600353d43b741)
    Signed-off-by: Jeff Long <willcode4@gmail.com>

--- gnuradio-runtime/lib/basic_block.cc
+++ gnuradio-runtime/lib/basic_block.cc
@@ -75,8 +75,6 @@ void basic_block::message_port_register_in(pmt::pmt_t port_id)
         throw std::runtime_error("message_port_register_in: bad port id");
     }
     msg_queue[port_id] = msg_queue_t();
-    msg_queue_ready[port_id] =
-        std::shared_ptr<boost::condition_variable>(new boost::condition_variable());
 }
 
 pmt::pmt_t basic_block::message_ports_in()
@@ -176,8 +174,7 @@ void basic_block::insert_tail(pmt::pmt_t which_port, pmt::pmt_t msg)
 {
     gr::thread::scoped_lock guard(mutex);
 
-    if ((msg_queue.find(which_port) == msg_queue.end()) ||
-        (msg_queue_ready.find(which_port) == msg_queue_ready.end())) {
+    if (msg_queue.find(which_port) == msg_queue.end()) {
         GR_LOG_ERROR(d_logger,
                      std::string("attempted insertion on invalid queue ") +
                          pmt::symbol_to_string(which_port));
@@ -185,7 +182,6 @@ void basic_block::insert_tail(pmt::pmt_t which_port, pmt::pmt_t msg)
     }
 
     msg_queue[which_port].push_back(msg);
-    msg_queue_ready[which_port]->notify_one();
 
     // wake up thread if BLKD_IN or BLKD_OUT
     global_block_registry.notify_blk(d_symbol_name);
--- gnuradio-runtime/python/gnuradio/gr/bindings/basic_block_python.cc
+++ gnuradio-runtime/python/gnuradio/gr/bindings/basic_block_python.cc
@@ -14,7 +14,7 @@
 /* BINDTOOL_GEN_AUTOMATIC(0)                                                       */
 /* BINDTOOL_USE_PYGCCXML(0)                                                        */
 /* BINDTOOL_HEADER_FILE(basic_block.h)                                        */
-/* BINDTOOL_HEADER_FILE_HASH(26ded167bb79b740fd37685f86fd553e)                     */
+/* BINDTOOL_HEADER_FILE_HASH(9239cc3381582f5f44010485cd48fa72)                     */
 /***********************************************************************************/
 
 #include <pybind11/complex.h>
