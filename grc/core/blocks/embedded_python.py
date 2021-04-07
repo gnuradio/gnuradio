58529072d150391ae71d772356481965c7a3ac61
Trevor R.H. Clarke <retrev@csh.rit.edu>
2021-04-07 16:34:28 -0400

    Fixed embedded python block and documentation to ensure in/out sizes using parameters are calculated correctly.
    Signed-off-by: Trevor R.H. Clarke <retrev@csh.rit.edu>

--- grc/core/blocks/embedded_python.py
+++ grc/core/blocks/embedded_python.py
@@ -25,6 +25,8 @@ Embedded Python Blocks:
 Each time this file is saved, GRC will instantiate the first class it finds
 to get ports and parameters of your block. The arguments to __init__  will
 be the parameters. All of them are required to have default values!
+Make sure you set the parameter member values before calling the parent class's __init__
+if any parameter is used to set input or output sizes.
 """
 
 import numpy as np
@@ -36,15 +38,16 @@ class blk(gr.sync_block):  # other base classes are basic_block, decim_block, in
 
     def __init__(self, example_param=1.0):  # only default arguments here
         """arguments to this function show up as parameters in GRC"""
+        # if an attribute with the same name as a parameter is found,
+        # a callback is registered (properties work, too).
+        self.example_param = example_param
+
         gr.sync_block.__init__(
             self,
             name='Embedded Python Block',   # will show up in GRC
             in_sig=[np.complex64],
             out_sig=[np.complex64]
         )
-        # if an attribute with the same name as a parameter is found,
-        # a callback is registered (properties work, too).
-        self.example_param = example_param
 
     def work(self, input_items, output_items):
         """example: multiply with constant"""
