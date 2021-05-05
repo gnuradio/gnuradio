d3b74b1a3cff8e737ad95fb89ead4a394f1c48aa
Josh Morman <jmorman@perspectalabs.com>
2021-05-05 11:27:47 -0400

    iio: single parameter iio api for pluto
    rather than complex constructors and a catch all set_params, construct
    with the minimum amount of parameters and then have them individually
    settable
    
    Signed-off-by: Josh Morman <jmorman@perspectalabs.com>
    Signed-off-by: Bernard Tyers - Sane UX Design <bernard+work@ei8fdb.org>

--- gr-iio/python/iio/bindings/pluto_source_python.cc
+++ gr-iio/python/iio/bindings/pluto_source_python.cc
@@ -14,7 +14,7 @@
 /* BINDTOOL_GEN_AUTOMATIC(0)                                                       */
 /* BINDTOOL_USE_PYGCCXML(0)                                                        */
 /* BINDTOOL_HEADER_FILE(pluto_source.h)                                        */
-/* BINDTOOL_HEADER_FILE_HASH(68ea89af32db39f870bfa404f3fbc6c2)                     */
+/* BINDTOOL_HEADER_FILE_HASH(0)                     */
 /***********************************************************************************/
 
 #include <pybind11/complex.h>
@@ -40,36 +40,25 @@ void bind_pluto_source(py::module& m)
 
         .def(py::init(&pluto_source::make),
              py::arg("uri"),
-             py::arg("longfrequency"),
-             py::arg("samplerate"),
-             py::arg("bandwidth"),
              py::arg("buffer_size"),
-             py::arg("quadrature"),
-             py::arg("rfdc"),
-             py::arg("bbdc"),
-             py::arg("gain"),
-             py::arg("gain_value"),
-             py::arg("filter_source") = "",
-             py::arg("filter_filename") = "",
-             py::arg("Fpass") = 0.0,
-             py::arg("Fstop") = 0.0,
              D(pluto_source, make))
 
-        .def("set_params",
-             &pluto_source::set_params,
-             py::arg("longfrequency"),
-             py::arg("samplerate"),
-             py::arg("bandwidth"),
-             py::arg("quadrature"),
-             py::arg("rfdc"),
-             py::arg("bbdc"),
-             py::arg("gain"),
-             py::arg("gain_value"),
-             py::arg("filter_source") = "",
-             py::arg("filter_filename") = "",
-             py::arg("Fpass") = 0.0,
-             py::arg("Fstop") = 0.0,
-             D(pluto_source, set_params))
+
+        .def("set_frequency", &pluto_source::set_frequency, py::arg("longfrequency"))
+        .def("set_samplerate", &pluto_source::set_samplerate, py::arg("samplerate"))
+        .def("set_gain_mode", &pluto_source::set_gain_mode, py::arg("mode"))
+        .def("set_gain", &pluto_source::set_gain, py::arg("gain_value"))
+        .def("set_quadrature", &pluto_source::set_quadrature, py::arg("quadrature"))
+        .def("set_rfdc", &pluto_source::set_rfdc, py::arg("rfdc"))
+        .def("set_bbdc", &pluto_source::set_bbdc, py::arg("bbdc"))
+        .def("set_filter_source",
+             &pluto_source::set_filter_source,
+             py::arg("filter_source"))
+        .def("set_filter_filename",
+             &pluto_source::set_filter_filename,
+             py::arg("filter_filename"))
+        .def("set_fpass", &pluto_source::set_fpass, py::arg("fpass"))
+        .def("set_fstop", &pluto_source::set_fstop, py::arg("fstop"))
 
         ;
 }
