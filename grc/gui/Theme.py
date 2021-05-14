793d9354a6f009d568ffc35c44c4379b52a8d300
Marcus Müller <mmueller@gnuradio.org>
2021-05-14 19:37:20 +0200

    remove unused import
    Signed-off-by: Marcus Müller <mmueller@gnuradio.org>

--- grc/gui/Theme.py
+++ grc/gui/Theme.py
@@ -7,7 +7,6 @@ SPDX-License-Identifier: GPL-2.0-or-later
 
 from ..core.base import Element
 from .defaulttheme import DEFAULT_THEME
-from .Config import Config
 
 from enum import Flag, auto
 from typing import Union, Iterable, Tuple
