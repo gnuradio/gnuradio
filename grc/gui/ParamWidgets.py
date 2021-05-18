3cd188d28eb1ef0090481e56656de75d604524a2
Chuang Zhu <genelocated@yandex.com>
2021-05-18 12:08:42 +0800

    grc: fix dark theme name not affecting colors
    Signed-off-by: Chuang Zhu <genelocated@yandex.com>
    (cherry picked from commit eac195620858b4312d1fcd4a783f17a07bc85ee3)
    Signed-off-by: Jeff Long <willcode4@gmail.com>

--- grc/gui/ParamWidgets.py
+++ grc/gui/ParamWidgets.py
@@ -29,9 +29,9 @@ def have_dark_theme():
     config.read(os.path.expanduser(Constants.GTK_SETTINGS_INI_PATH))
     prefer_dark = config.get(
         'Settings', Constants.GTK_INI_PREFER_DARK_KEY, fallback=None)
-    if prefer_dark in ('1', 'yes', 'true', 'on'):
-        theme_name = config.get(
-            'Settings', Constants.GTK_INI_THEME_NAME_KEY, fallback=None)
+    theme_name = config.get(
+        'Settings', Constants.GTK_INI_THEME_NAME_KEY, fallback=None)
+    if prefer_dark in ('1', 'yes', 'true', 'on') or is_dark_theme(theme_name):
         return True
     try:
         theme = subprocess.check_output(
