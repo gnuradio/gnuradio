688ffb277171ecefc408b2dc5491006184fd368c
0xloem <0xloem@gmail.com>
2021-05-27 10:57:08 -0400

    Fix vector margin for non-frequency data
    Signed-off-by: 0xloem <0xloem@gmail.com>
    Signed-off-by: Bernard Tyers - Sane UX Design <bernard+work@ei8fdb.org>

--- gr-qtgui/lib/VectorDisplayPlot.cc
+++ gr-qtgui/lib/VectorDisplayPlot.cc
@@ -369,9 +369,10 @@ void VectorDisplayPlot::clearMinData()
 
 void VectorDisplayPlot::_autoScale(double bottom, double top)
 {
-    // Auto scale the y-axis with a margin of 10 dB on either side.
-    d_ymin = bottom - 10;
-    d_ymax = top + 10;
+    // Auto scale the y-axis with a margin of 1% on either side
+    double margin = (top - bottom) / 100;
+    d_ymin = bottom - margin;
+    d_ymax = top + margin;
     setYaxis(d_ymin, d_ymax);
 }
 
