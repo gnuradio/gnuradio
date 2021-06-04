d7e67b2274ca884d210213696152cce860b4112d
Marcus Müller <mmueller@gnuradio.org>
2021-06-04 22:13:27 +0200

    uhd: pybind rfnoc_graph,_rx/tx_streamer
    Signed-off-by: Marcus Müller <mmueller@gnuradio.org>
    (cherry picked from commit f29be5398081e78ed0edeaf712cc608e770f2523)
    Signed-off-by: Jeff Long <willcode4@gmail.com>

---
+++ gr-uhd/python/uhd/bindings/docstrings/rfnoc_graph_pydoc_template.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2021 Marcus Müller
+ *
+ * This file is part of GNU Radio
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ */
+#include "pydoc_macros.h"
+#define D(...) DOC(gr, uhd, __VA_ARGS__)
+/*
+  This file contains placeholders for docstrings for the Python bindings.
+  Do not edit! These were automatically extracted during the binding process
+  and will be overwritten during the build process
+ */
+
+
+static const char* __doc_gr_uhd_rfnoc_graph = R"doc()doc";
+
+static const char* __doc_gr_uhd_rfnoc_graph_make = R"doc()doc";
+
+static const char* __doc_gr_uhd_rfnoc_graph_connect = R"doc()doc";
+
+static const char* __doc_gr_uhd_rfnoc_graph_create_rx_streamer = R"doc()doc";
+
+static const char* __doc_gr_uhd_rfnoc_graph_create_tx_streamer = R"doc()doc";
+
+static const char* __doc_gr_uhd_rfnoc_graph_commit = R"doc()doc";
+
+static const char* __doc_gr_uhd_rfnoc_graph_get_block_id = R"doc()doc";
+
+static const char* __doc_gr_uhd_rfnoc_graph_set_time_source = R"doc()doc";
+
+static const char* __doc_gr_uhd_rfnoc_graph_set_clock_source = R"doc()doc";
+
+static const char* __doc_gr_uhd_rfnoc_graph_get_block_ref = R"doc()doc";
