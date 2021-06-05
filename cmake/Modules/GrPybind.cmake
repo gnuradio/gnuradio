2d51507e10f767ddc941220860b7ebfd29e6c15e
Marcus Müller <mmueller@gnuradio.org>
2021-06-05 16:13:16 +0200

    pybind: add precompilation of pybind headers to pybind targets
    Signed-off-by: Marcus Müller <mmueller@gnuradio.org>

--- cmake/Modules/GrPybind.cmake
+++ cmake/Modules/GrPybind.cmake
@@ -154,6 +154,22 @@ target_include_directories(${name}_python PRIVATE
     ${CMAKE_CURRENT_SOURCE_DIR}/${updir}/lib
     ${CMAKE_CURRENT_SOURCE_DIR}/${updir}/include
 )
+
+# Precompile the pybind11 header
+# This should speed up building of the python bindings at least in larger modules
+# This functionality is only available in CMake >= 3.16
+if(${CMAKE_VERSION} VERSION_GREATER_EQUAL "3.16.0")
+  target_precompile_headers(
+    ${name}_python
+    PRIVATE
+    ${pybind11_INCLUDE_DIR}/pybind11/pybind11.h
+    ${pybind11_INCLUDE_DIR}/pybind11/embed.h
+    ${pybind11_INCLUDE_DIR}/pybind11/complex.h
+    ${pybind11_INCLUDE_DIR}/pybind11/operators.h
+    ${pybind11_INCLUDE_DIR}/pybind11/stl.h
+)
+endif()
+
 target_link_libraries(${name}_python PRIVATE ${Boost_LIBRARIES} pybind11::pybind11 Python::Module Python::NumPy gnuradio-${MODULE_NAME})
 if(CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR
    CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
