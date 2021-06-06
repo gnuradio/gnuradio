b96488bfa2f521534b6de13cc23cf20e37ad5dad
Daniel Estévez <daniel@destevez.net>
2021-06-06 11:25:57 +0200

    digital: Add NRZI option to differential en/decoder
    This adds an option to the differential encoder an decoder blocks
    to perform NRZI encoding and decoding. NRZI only makes sense with
    a modulus of 2, so the blocks constructors will throw and exception
    if passed nrzi = true and a modulus different from 2.
    
    The GRC blocks handle this by hiding the modulus field if the user
    selects NRZI encoding.
    
    A new unit test for the NRZI version of the blocks is added. Besides
    checking that encode plus decode gives the original, this test also
    compares the C++ implementation results against a Numpy implementation.
    
    Additionally, a faster implementation of differential encoding/
    decoding for modulus 2 is included here.
    
    Signed-off-by: Daniel Estévez <daniel@destevez.net>
    Signed-off-by: Bernard Tyers - Sane UX Design <bernard+work@ei8fdb.org>

--- gr-digital/lib/diff_encoder_bb_impl.h
+++ gr-digital/lib/diff_encoder_bb_impl.h
@@ -1,6 +1,7 @@
 /* -*- c++ -*- */
 /*
  * Copyright 2006,2012 Free Software Foundation, Inc.
+ * Copyright 2021 Daniel Estevez <daniel@destevez.net>
  *
  * This file is part of GNU Radio
  *
@@ -19,7 +20,8 @@ namespace digital {
 class diff_encoder_bb_impl : public diff_encoder_bb
 {
 public:
-    diff_encoder_bb_impl(unsigned int modulus);
+    diff_encoder_bb_impl(unsigned int modulus,
+                         enum diff_coding_type coding = DIFF_DIFFERENTIAL);
     ~diff_encoder_bb_impl() override;
 
     int work(int noutput_items,
@@ -29,6 +31,7 @@ public:
 private:
     unsigned int d_last_out;
     const unsigned int d_modulus;
+    const enum diff_coding_type d_coding;
 };
 
 } /* namespace digital */
