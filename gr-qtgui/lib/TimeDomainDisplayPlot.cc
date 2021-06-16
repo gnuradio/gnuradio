726752d16f4b940ac832f6f36323c28931db7a62
Marcus Müller <mmueller@gnuradio.org>
2021-06-17 00:37:10 +0200

    qtgui: only one definition of (internal) TimePrecisionClass
    Signed-off-by: Marcus Müller <mmueller@gnuradio.org>

--- gr-qtgui/lib/TimeDomainDisplayPlot.cc
+++ gr-qtgui/lib/TimeDomainDisplayPlot.cc
@@ -11,6 +11,7 @@
 #ifndef TIME_DOMAIN_DISPLAY_PLOT_C
 #define TIME_DOMAIN_DISPLAY_PLOT_C
 
+#include "TimePrecisionClass.h"
 #include <gnuradio/qtgui/TimeDomainDisplayPlot.h>
 
 #include <qwt_legend.h>
@@ -18,25 +19,6 @@
 #include <volk/volk.h>
 #include <QColor>
 #include <cmath>
-
-class TimePrecisionClass
-{
-public:
-    TimePrecisionClass(const int timePrecision) { d_timePrecision = timePrecision; }
-
-    virtual ~TimePrecisionClass() {}
-
-    virtual unsigned int getTimePrecision() const { return d_timePrecision; }
-
-    virtual void setTimePrecision(const unsigned int newPrecision)
-    {
-        d_timePrecision = newPrecision;
-    }
-
-protected:
-    unsigned int d_timePrecision;
-};
-
 
 class TimeDomainDisplayZoomer : public QwtPlotZoomer, public TimePrecisionClass
 {
