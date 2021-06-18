a22902c835e3ab3a9c99c3ef07000f9101d25164
Håkon Vågsether <hakon.vagsether@gmail.com>
2021-06-19 00:04:12 +0200

    grcqt: Use Bezier curves for connections
    TODO: get rid of connection artifacts
    
    Signed-off-by: Håkon Vågsether <hakon.vagsether@gmail.com>

--- grc/gui_qt/components/canvas/connection.py
+++ grc/gui_qt/components/canvas/connection.py
@@ -4,7 +4,7 @@ from PyQt5.QtCore import Qt
 
 from ....core.Connection import Connection as CoreConnection
 
-class Connection(CoreConnection, QtWidgets.QGraphicsLineItem):
+class Connection(CoreConnection, QtWidgets.QGraphicsPathItem):
 
     def __init__(self, parent, source, sink):
         CoreConnection.__init__(self, parent, source, sink)
@@ -14,7 +14,14 @@ class Connection(CoreConnection, QtWidgets.QGraphicsLineItem):
         self.source = source
         self.sink = sink
 
-        self._line = QtCore.QLineF(source.scenePos(), sink.scenePos())
+        #self._line = QtCore.QLineF(source.scenePos(), sink.scenePos())
+        self._line = QtGui.QPainterPath()
+        self._line.moveTo(source.scenePos())
+        c1 = source.scenePos() + QtCore.QPointF(10, 0)
+        c2 = sink.scenePos() - QtCore.QPointF(10, 0)
+        self._line.cubicTo(c1, c2, sink.scenePos())
+        self.setPath(self._line)
+
         self._line_width_factor = 1.0
         self._color1 = self._color2 = None
 
@@ -26,6 +33,9 @@ class Connection(CoreConnection, QtWidgets.QGraphicsLineItem):
         self._line_path = None
 
     def updateLine(self):
-        self._line.setP1(self.source.scenePos())
-        self._line.setP2(self.sink.scenePos())
-        self.setLine(self._line)
+        self._line.moveTo(self.source.scenePos())
+        c1 = self.source.scenePos() + QtCore.QPointF(200, 0)
+        c2 = self.sink.scenePos() - QtCore.QPointF(200, 0)
+        self._line.cubicTo(c1, c2, self.sink.scenePos())
+        self.setPath(self._line)
+
