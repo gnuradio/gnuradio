336079f5988ac72d25e7058f93f72a30400438f6
cmrincon <cmrincon611@hotmail.com>
2021-07-12 14:16:01 +0200

    digital: Added a crc16 basic block. (#4649)
    Signed-off-by: cmrincon <cmrincon611@hotmail.com>
    (cherry picked from commit 7c1d67d6b9e64d93d2b66615be887f23802f16a5)
    Signed-off-by: Jeff Long <willcode4@gmail.com>

---
+++ gr-digital/include/gnuradio/digital/crc16_async_bb.h
@@ -0,0 +1,60 @@
+/* -*- c++ -*- */
+/*
+ * Copyright 2021 Cesar Martinez.
+ *
+ * This file is part of GNU Radio
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+
+ *
+ *
+ */
+
+
+#ifndef INCLUDED_DIGITAL_CRC16_ASYNC_BB_H
+#define INCLUDED_DIGITAL_CRC16_ASYNC_BB_H
+
+#include <gnuradio/block.h>
+#include <gnuradio/digital/api.h>
+
+namespace gr {
+namespace digital {
+
+/*!
+ * \brief Byte-stream CRC block for async messages
+ * \ingroup packet_operators_blk
+ *
+ * \details
+ *
+ * Processes packets (as async PDU messages) for CRC16. The \p
+ * check parameter determines if the block acts to check and strip
+ * the CRC or to calculate and append the CRC16.
+ *
+ * The input PDU is expected to be a message of packet bytes.
+ *
+ * When using check mode, if the CRC passes, the output is a
+ * payload of the message with the CRC stripped, so the output
+ * will be 2 bytes smaller than the input.
+ *
+ * When using calculate mode (check == false), then the CRC is
+ * calculated on the PDU and appended to it. The output is then 2
+ * bytes longer than the input.
+ *
+ * This block implements the CRC16 using the Boost crc_optimal
+ * class for 16-bit CRCs with the standard generator 0x1021.
+ */
+class DIGITAL_API crc16_async_bb : virtual public block
+{
+public:
+    typedef std::shared_ptr<crc16_async_bb> sptr;
+
+    /*!
+     * \param check Set to true if you want to check CRC, false to create CRC.
+     */
+    static sptr make(bool check = false);
+};
+
+} // namespace digital
+} // namespace gr
+
+#endif /* INCLUDED_DIGITAL_CRC16_ASYNC_BB_H */
---
+++ gr-digital/python/digital/bindings/docstrings/crc16_async_bb_pydoc_template.h
@@ -0,0 +1,25 @@
+/* -*- c++ -*- */
+/*
+ * Copyright 2021 Cesar Martinez.
+ *
+ * This file is part of GNU Radio
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ */
+#include "pydoc_macros.h"
+#define D(...) DOC(gr, digital, __VA_ARGS__)
+/*
+  This file contains placeholders for docstrings for the Python bindings.
+  Do not edit! These were automatically extracted during the binding process
+  and will be overwritten during the build process
+ */
+
+
+static const char* __doc_gr_digital_crc16_async_bb = R"doc()doc";
+
+
+static const char* __doc_gr_digital_crc16_async_bb_crc32_async_bb = R"doc()doc";
+
+
+static const char* __doc_gr_digital_crc16_async_bb_make = R"doc()doc";
