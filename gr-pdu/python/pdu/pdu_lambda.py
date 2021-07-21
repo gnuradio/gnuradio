8adfad0aed204f65efd8c29fe0056bf6c65a3164
Jacob Gilbert <jacob.gilbert@protonmail.com>
2021-07-20 20:34:36 -0600

    gr-pdu: fix pdu_lambda and examples
    The lambda block and examples had some errors from when it was an OOT
    block which are fixed in this; docs updated too
    
    Signed-off-by: Jacob Gilbert <jacob.gilbert@protonmail.com>

--- gr-pdu/python/pdu/pdu_lambda.py
+++ gr-pdu/python/pdu/pdu_lambda.py
@@ -28,20 +28,14 @@ class pdu_lambda(gr.basic_block):
         `import pmt, math, time`
     These should also be imported in the flowgraph implementing this block if utilized.
 
-    Errors are generally silently dropped as all function application is done within a
-    try/catch statement. In the event an exception is thrown the data will be emitted
-    as it was received.
+    Exceptions during the function call will be caught and printed to the screen. In the
+    event an exception is thrown the data will be emitted as it was received.
 
     Examples of use:
     - Phase demodulation of a complex PDU: Uniform Vector Mode with function:
         `lambda x: np.unwrap(np.angle(x))`
     - Scale metadata field `val1`: Metadata mode, separate QT range block with name `k`:
         `lambda x: k*x`
-
-    This block cannot add or remove metadata fields, which should be done with the
-    application specific blocks in-tree or the more generic `message_lambda` block. This
-    block also designed to accept and return explicitly PDU type objects, if the PMT
-    object type needs to change, use the `message_lambda` block.
     """
 
     def __init__(self, fn, metadict, key=pmt.PMT_NIL):
@@ -56,7 +50,7 @@ class pdu_lambda(gr.basic_block):
         self.set_msg_handler(pmt.intern("pdu"), self.handle_msg)
 
     def handle_msg(self, pdu):
-        if self.metadict_mode:
+        if self.metadict_mode == "META":
             meta = pmt.car(pdu)
             try:
                 val = pmt.to_python(pmt.dict_ref(meta, self.key, pmt.PMT_NIL))
@@ -69,7 +63,7 @@ class pdu_lambda(gr.basic_block):
             self.message_port_pub(pmt.intern("pdu"),
                 pmt.cons(meta, pmt.cdr(pdu)));
 
-        else: # PDU Vector mode
+        elif self.metadict_mode == "UVEC":
             vec = pmt.cdr(pdu)
             try:
                 vec = pmt.to_pmt(self.fn(pmt.to_python(vec)))
@@ -79,6 +73,21 @@ class pdu_lambda(gr.basic_block):
 
             self.message_port_pub(pmt.intern("pdu"),
                 pmt.cons(pmt.car(pdu), vec));
+        
+        elif self.metadict_mode == "RAW":
+            # TODO: This is more of a "message lambda" block, in the future it should be
+            # a separate block outside the PDU namespace
+            try:
+                pdu = self.fn(pdu)
+            except Exception as e:
+                print(e)
+                pass
+            self.message_port_pub(pmt.intern("pdu"), pdu)
+        
+        else:
+            raise ValueError("pdu_lambda block instantiated in unknown mode " + repr(self.metadict_mode))
+            pass
+            
 
     def set_fn(self,fn):
         self.fn = fn
