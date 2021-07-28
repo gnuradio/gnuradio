8bc9a096da129373d31502cbf02c6b858e78c16c
Philip Balister <philip@balister.org>
2021-07-28 11:53:46 -0400

    Update QA tests to import helper routines from blocks.
    The file metadata QA installs a file in blocks, but imported it
    directly. Updated init.py to import via blocks. matrix interleaver
    imported some routines from a py file that was available via blocks.
    
    These cretaed issues when you install the qa tests in images to run
    them outside the build area.
    
    Signed-off-by: Philip Balister <philip@balister.org>

--- gr-blocks/python/blocks/__init__.py
+++ gr-blocks/python/blocks/__init__.py
@@ -26,6 +26,7 @@ from .msg_meta_to_pair import meta_to_pair
 from .msg_pair_to_var import msg_pair_to_var
 from .var_to_msg import var_to_msg_pair
 from .matrix_interleaver import *
+from .parse_file_metadata import parse_header, parse_extra_dict
 
 # alias old add_vXX and multiply_vXX
 add_vcc = add_cc
