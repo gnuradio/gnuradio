314a87008559a04b911d9ed8fc77cb0c35d6f383
Ron Economos <w6rz@comcast.net>
2021-08-08 13:28:48 -0700

    gr-video-sdl: Fix reversal of U and V channels on sink blocks.
    Signed-off-by: Ron Economos <w6rz@comcast.net>

--- gr-video-sdl/lib/sink_s_impl.cc
+++ gr-video-sdl/lib/sink_s_impl.cc
@@ -103,7 +103,7 @@ sink_s_impl::sink_s_impl(double framerate,
 
     /* Initialize and create the YUV Overlay used for video out */
     if (!(d_image =
-              SDL_CreateYUVOverlay(d_width, d_height, SDL_YV12_OVERLAY, d_screen))) {
+              SDL_CreateYUVOverlay(d_width, d_height, SDL_IYUV_OVERLAY, d_screen))) {
         std::ostringstream msg;
         msg << "Couldn't create a YUV overlay: " << SDL_GetError();
         GR_LOG_ERROR(d_logger, msg.str());
