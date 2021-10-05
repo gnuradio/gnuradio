973a2da868e9a2fb71308e8e7dadc523ba73b49c
Josh Morman <jmorman@peratonlabs.com>
2021-10-05 17:12:41 -0400

    iio: de-duplicate the pluto wrappers
    The fmcomms2 source/sink were wrapped with hier blocks that were used as
    the basis for the pluto blocks
    
    This templatizes the fmcomms2 blocks so they can be directly used, and
    since the pluto block is so close to the fmcomms block, just use grc as
    the wrapper around it
    
    fmcomms2/pluto can also now be used as sc16 or fc32
    
    Signed-off-by: Josh Morman <jmorman@peratonlabs.com>

--- gr-iio/python/iio/bindings/docstrings/pluto_utils_pydoc_template.h
+++ gr-iio/python/iio/bindings/docstrings/pluto_utils_pydoc_template.h
@@ -14,10 +14,4 @@
   and will be overwritten during the build process
  */
 
-static const char* __doc_gr_iio_pluto_source = R"doc()doc";
-
-static const char* __doc_gr_iio_pluto_source_make = R"doc()doc";
-
-static const char* __doc_gr_iio_pluto_source_set_params = R"doc()doc";
-
-static const char* __doc_gr_iio_pluto_source_set_len_tag_key = R"doc()doc";
+static const char* __doc_gr_iio_pluto_utils_get_pluto_uri = R"doc()doc";
