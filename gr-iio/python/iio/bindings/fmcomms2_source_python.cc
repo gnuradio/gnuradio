e57f1152ed960041b02aea76100174384fddaf79
Josh Morman <jmorman@peratonlabs.com>
2021-10-06 06:16:55 -0400

    iio: remove pluto, bind fmcomms2 templated
    Signed-off-by: Josh Morman <jmorman@peratonlabs.com>

--- gr-iio/python/iio/bindings/fmcomms2_source_python.cc
+++ gr-iio/python/iio/bindings/fmcomms2_source_python.cc
@@ -27,31 +27,38 @@ namespace py = pybind11;
 // pydoc.h is automatically generated in the build directory
 #include <fmcomms2_source_pydoc.h>
 
-void bind_fmcomms2_source(py::module& m)
+template <typename T>
+void bind_fmcomms2_source_template(py::module& m, const char *classname)
 {
 
-    using fmcomms2_source = gr::iio::fmcomms2_source;
+    using fmcomms2_source = gr::iio::fmcomms2_source<T>;
 
 
     py::class_<fmcomms2_source,
                gr::sync_block,
                gr::block,
                gr::basic_block,
-               std::shared_ptr<fmcomms2_source>>(m, "fmcomms2_source", D(fmcomms2_source))
+               std::shared_ptr<fmcomms2_source>>(m, classname, D(fmcomms2_source))
 
         .def(py::init(&fmcomms2_source::make),
              py::arg("uri"),
              py::arg("ch_en"),
              py::arg("buffer_size"),
              D(fmcomms2_source, make))
-        .def("set_frequency", &pluto_source::set_frequency, py::arg("longfrequency"))
-        .def("set_samplerate", &pluto_source::set_samplerate, py::arg("samplerate"))
-        .def("set_gain_mode", &pluto_source::set_gain_mode, py::arg("mode"))
-        .def("set_gain", &pluto_source::set_gain, py::arg("gain_value"))
-        .def("set_quadrature", &pluto_source::set_quadrature, py::arg("quadrature"))
-        .def("set_rfdc", &pluto_source::set_rfdc, py::arg("rfdc"))
-        .def("set_bbdc", &pluto_source::set_bbdc, py::arg("bbdc"))
-        .def("set_filter_params", &pluto_source::set_filter_params)
-        .def("set_len_tag_key", &pluto_source::set_len_tag_key, py::arg("len_tag_key"));
+        .def("set_frequency", &fmcomms2_source::set_frequency, py::arg("longfrequency"))
+        .def("set_samplerate", &fmcomms2_source::set_samplerate, py::arg("samplerate"))
+        .def("set_gain_mode", &fmcomms2_source::set_gain_mode, py::arg("chan"), py::arg("mode"))
+        .def("set_gain", &fmcomms2_source::set_gain, py::arg("chan"), py::arg("gain_value"))
+        .def("set_quadrature", &fmcomms2_source::set_quadrature, py::arg("quadrature"))
+        .def("set_rfdc", &fmcomms2_source::set_rfdc, py::arg("rfdc"))
+        .def("set_bbdc", &fmcomms2_source::set_bbdc, py::arg("bbdc"))
+        .def("set_filter_params", &fmcomms2_source::set_filter_params)
+        .def("set_len_tag_key", &fmcomms2_source::set_len_tag_key, py::arg("len_tag_key"));
         ;
+}
+
+void bind_fmcomms2_source(py::module& m)
+{
+    bind_fmcomms2_source_template<std::int16_t>(m, "fmcomms2_source_s");
+    bind_fmcomms2_source_template<gr_complex>(m, "fmcomms2_source_c");
 }
--- gr-iio/python/iio/bindings/python_bindings.cc
+++ gr-iio/python/iio/bindings/python_bindings.cc
@@ -25,11 +25,9 @@ void bind_attr_updater(py::module& m);
 void bind_fmcomms2_sink(py::module& m);
 // void bind_fmcomms5_sink(py::module& m);
 // void bind_fmcomms5_sink_f32c(py::module& m);
-// void bind_fmcomms2_source(py::module& m);
+void bind_fmcomms2_source(py::module& m);
 // void bind_fmcomms5_source(py::module& m);
 // void bind_fmcomms5_source_f32c(py::module& m);
-void bind_pluto_sink(py::module& m);
-void bind_pluto_source(py::module& m);
 #endif
 
 // We need this hack because import_array() returns NULL
@@ -59,15 +57,11 @@ PYBIND11_MODULE(iio_python, m)
     bind_dds_control(m);
     bind_attr_updater(m);
 #ifdef GR_IIO_LIBAD9361
-    // bind_fmcomms2_sink(m);
-    // bind_fmcomms2_sink_f32c(m);
+    bind_fmcomms2_sink(m);
     // bind_fmcomms5_sink(m);
     // bind_fmcomms5_sink_f32c(m);
-    bind_pluto_sink(m);
-    // bind_fmcomms2_source(m);
-    // bind_fmcomms2_source_f32c(m);
+    bind_fmcomms2_source(m);
     // bind_fmcomms5_source(m);
     // bind_fmcomms5_source_f32c(m);
-    bind_pluto_source(m);
 #endif
 }
