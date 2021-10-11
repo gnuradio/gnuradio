50cd47d699747fb7905ac253f6c4bac1b42f2ced
lenhart <malte.lenhart@mailbox.org>
2021-10-11 13:36:03 +0200

    Update qa_message_strobe.py
    Update license in qa_message_stobe.py
    
    Signed-off-by: Lenhart <malte.lenhart@mailbox.org>

--- gr-blocks/python/blocks/qa_message_strobe.py
+++ gr-blocks/python/blocks/qa_message_strobe.py
@@ -1,22 +1,11 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 #
-# Copyright 2021 Malte Lenhart.
+# Copyright 2021 Malte Lenhart
 #
-# This is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3, or (at your option)
-# any later version.
+# This file is part of GNU Radio
 #
-# This software is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# SPDX-License-Identifier: GPL-3.0-or-later
 #
-# You should have received a copy of the GNU General Public License
-# along with this software; see the file COPYING.  If not, write to
-# the Free Software Foundation, Inc., 51 Franklin Street,
-# Boston, MA 02110-1301, USA.
 #
 
 from gnuradio import gr, gr_unittest
