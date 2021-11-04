18ddffcf96b31753852167f9eae3fb31c5d36050
Josh Morman <jmorman@gnuradio.org>
2021-11-04 15:56:09 -0400

    channels: python re-formatting
    Signed-off-by: Josh Morman <jmorman@gnuradio.org>

--- gr-channels/python/channels/phase_bal.py
+++ gr-channels/python/channels/phase_bal.py
@@ -11,13 +11,14 @@ from gnuradio import filter
 from gnuradio import gr
 from gnuradio.filter import firdes
 
+
 class phase_bal(gr.hier_block2):
 
     def __init__(self, alpha=0):
         gr.hier_block2.__init__(
             self, "IQ Phase Balancer",
-            gr.io_signature(1, 1, gr.sizeof_gr_complex*1),
-            gr.io_signature(1, 1, gr.sizeof_gr_complex*1),
+            gr.io_signature(1, 1, gr.sizeof_gr_complex * 1),
+            gr.io_signature(1, 1, gr.sizeof_gr_complex * 1),
         )
 
         ##################################################
@@ -28,7 +29,8 @@ class phase_bal(gr.hier_block2):
         ##################################################
         # Blocks
         ##################################################
-        self.filter_single_pole_iir_filter_xx_0 = filter.single_pole_iir_filter_ff(alpha, 1)
+        self.filter_single_pole_iir_filter_xx_0 = filter.single_pole_iir_filter_ff(
+            alpha, 1)
         self.blocks_sub_xx_1 = blocks.sub_ff(1)
         self.blocks_sub_xx_0 = blocks.sub_ff(1)
         self.blocks_multiply_xx_2 = blocks.multiply_vff(1)
@@ -43,28 +45,39 @@ class phase_bal(gr.hier_block2):
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.blocks_complex_to_float_0, 0), (self.blocks_multiply_xx_0, 0))
-        self.connect((self.blocks_complex_to_float_0, 1), (self.blocks_multiply_xx_0, 1))
-        self.connect((self.blocks_multiply_xx_0, 0), (self.blocks_divide_xx_0, 0))
-        self.connect((self.blocks_sub_xx_0, 0), (self.blocks_float_to_complex_0, 1))
+        self.connect((self.blocks_complex_to_float_0, 0),
+                     (self.blocks_multiply_xx_0, 0))
+        self.connect((self.blocks_complex_to_float_0, 1),
+                     (self.blocks_multiply_xx_0, 1))
+        self.connect((self.blocks_multiply_xx_0, 0),
+                     (self.blocks_divide_xx_0, 0))
+        self.connect((self.blocks_sub_xx_0, 0),
+                     (self.blocks_float_to_complex_0, 1))
         self.connect((self.blocks_multiply_xx_1, 0), (self.blocks_sub_xx_0, 1))
-        self.connect((self.filter_single_pole_iir_filter_xx_0, 0), (self.blocks_multiply_xx_1, 1))
-        self.connect((self.blocks_complex_to_float_0, 0), (self.blocks_multiply_xx_1, 0))
+        self.connect((self.filter_single_pole_iir_filter_xx_0, 0),
+                     (self.blocks_multiply_xx_1, 1))
+        self.connect((self.blocks_complex_to_float_0, 0),
+                     (self.blocks_multiply_xx_1, 0))
         self.connect((self.blocks_multiply_xx_2, 0), (self.blocks_sub_xx_1, 1))
-        self.connect((self.blocks_complex_to_float_0, 1), (self.blocks_sub_xx_0, 0))
-        self.connect((self.blocks_sub_xx_1, 0), (self.blocks_float_to_complex_0, 0))
-        self.connect((self.blocks_complex_to_mag_squared_0, 0), (self.blocks_divide_xx_0, 1))
-        self.connect((self.blocks_complex_to_float_0, 0), (self.blocks_sub_xx_1, 0))
-        self.connect((self.blocks_divide_xx_0, 0), (self.blocks_multiply_const_vxx_0, 0))
-        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.filter_single_pole_iir_filter_xx_0, 0))
+        self.connect((self.blocks_complex_to_float_0, 1),
+                     (self.blocks_sub_xx_0, 0))
+        self.connect((self.blocks_sub_xx_1, 0),
+                     (self.blocks_float_to_complex_0, 0))
+        self.connect((self.blocks_complex_to_mag_squared_0, 0),
+                     (self.blocks_divide_xx_0, 1))
+        self.connect((self.blocks_complex_to_float_0, 0),
+                     (self.blocks_sub_xx_1, 0))
+        self.connect((self.blocks_divide_xx_0, 0),
+                     (self.blocks_multiply_const_vxx_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0),
+                     (self.filter_single_pole_iir_filter_xx_0, 0))
         self.connect((self, 0), (self.blocks_complex_to_float_0, 0))
         self.connect((self, 0), (self.blocks_complex_to_mag_squared_0, 0))
         self.connect((self.blocks_float_to_complex_0, 0), (self, 0))
-        self.connect((self.filter_single_pole_iir_filter_xx_0, 0), (self.blocks_multiply_xx_2, 0))
-        self.connect((self.blocks_complex_to_float_0, 1), (self.blocks_multiply_xx_2, 1))
-
-
-# QT sink close method reimplementation
+        self.connect((self.filter_single_pole_iir_filter_xx_0, 0),
+                     (self.blocks_multiply_xx_2, 0))
+        self.connect((self.blocks_complex_to_float_0, 1),
+                     (self.blocks_multiply_xx_2, 1))
 
     def get_alpha(self):
         return self.alpha
@@ -72,5 +85,3 @@ class phase_bal(gr.hier_block2):
     def set_alpha(self, alpha):
         self.alpha = alpha
         self.filter_single_pole_iir_filter_xx_0.set_taps(self.alpha)
-
-
