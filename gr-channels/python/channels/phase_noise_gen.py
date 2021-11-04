d056ce4e09b793ccb965cdaa7cbb83bd0c8c5ee6
Josh Morman <jmorman@gnuradio.org>
2021-11-04 07:14:29 -0400

    channels: pep8 formatting
    Signed-off-by: Josh Morman <jmorman@gnuradio.org>

--- gr-channels/python/channels/phase_noise_gen.py
+++ gr-channels/python/channels/phase_noise_gen.py
@@ -12,13 +12,14 @@ from gnuradio import filter
 from gnuradio import gr
 from gnuradio.filter import firdes
 
+
 class phase_noise_gen(gr.hier_block2):
 
     def __init__(self, noise_mag=0, alpha=0.1):
         gr.hier_block2.__init__(
             self, "Phase Noise Generator",
-            gr.io_signature(1, 1, gr.sizeof_gr_complex*1),
-            gr.io_signature(1, 1, gr.sizeof_gr_complex*1),
+            gr.io_signature(1, 1, gr.sizeof_gr_complex * 1),
+            gr.io_signature(1, 1, gr.sizeof_gr_complex * 1),
         )
 
         ##################################################
@@ -30,27 +31,32 @@ class phase_noise_gen(gr.hier_block2):
         ##################################################
         # Blocks
         ##################################################
-        self.filter_single_pole_iir_filter_xx_0 = filter.single_pole_iir_filter_ff(alpha, 1)
+        self.filter_single_pole_iir_filter_xx_0 = filter.single_pole_iir_filter_ff(
+            alpha, 1)
         self.blocks_transcendental_0_0 = blocks.transcendental("sin", "float")
         self.blocks_transcendental_0 = blocks.transcendental("cos", "float")
         self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
         self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
-        self.analog_noise_source_x_0 = analog.noise_source_f(analog.GR_GAUSSIAN, noise_mag, 42)
+        self.analog_noise_source_x_0 = analog.noise_source_f(
+            analog.GR_GAUSSIAN, noise_mag, 42)
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.blocks_float_to_complex_0, 0), (self.blocks_multiply_xx_0, 1))
-        self.connect((self.analog_noise_source_x_0, 0), (self.filter_single_pole_iir_filter_xx_0, 0))
+        self.connect((self.blocks_float_to_complex_0, 0),
+                     (self.blocks_multiply_xx_0, 1))
+        self.connect((self.analog_noise_source_x_0, 0),
+                     (self.filter_single_pole_iir_filter_xx_0, 0))
         self.connect((self.blocks_multiply_xx_0, 0), (self, 0))
         self.connect((self, 0), (self.blocks_multiply_xx_0, 0))
-        self.connect((self.filter_single_pole_iir_filter_xx_0, 0), (self.blocks_transcendental_0, 0))
-        self.connect((self.filter_single_pole_iir_filter_xx_0, 0), (self.blocks_transcendental_0_0, 0))
-        self.connect((self.blocks_transcendental_0, 0), (self.blocks_float_to_complex_0, 0))
-        self.connect((self.blocks_transcendental_0_0, 0), (self.blocks_float_to_complex_0, 1))
-
-
-# QT sink close method reimplementation
+        self.connect((self.filter_single_pole_iir_filter_xx_0, 0),
+                     (self.blocks_transcendental_0, 0))
+        self.connect((self.filter_single_pole_iir_filter_xx_0, 0),
+                     (self.blocks_transcendental_0_0, 0))
+        self.connect((self.blocks_transcendental_0, 0),
+                     (self.blocks_float_to_complex_0, 0))
+        self.connect((self.blocks_transcendental_0_0, 0),
+                     (self.blocks_float_to_complex_0, 1))
 
     def get_noise_mag(self):
         return self.noise_mag
@@ -65,5 +71,3 @@ class phase_noise_gen(gr.hier_block2):
     def set_alpha(self, alpha):
         self.alpha = alpha
         self.filter_single_pole_iir_filter_xx_0.set_taps(self.alpha)
-
-
