45884e1c638dc9f3a52d4ab7729ef928ebdab95b
Josh Morman <jmorman@gnuradio.org>
2021-11-24 12:02:17 -0500

    audio: pep8 formatting
    Signed-off-by: Josh Morman <jmorman@gnuradio.org>

--- gr-audio/examples/python/spectrum_inversion.py
+++ gr-audio/examples/python/spectrum_inversion.py
@@ -20,6 +20,7 @@ from gnuradio import blocks
 from gnuradio.eng_arg import eng_float, intx
 from argparse import ArgumentParser
 
+
 class my_top_block(gr.top_block):
 
     def __init__(self):
@@ -27,15 +28,15 @@ class my_top_block(gr.top_block):
 
         parser = ArgumentParser()
         parser.add_argument("-I", "--audio-input", default="",
-                          help="pcm input device name.  E.g., hw:0,0 or /dev/dsp")
+                            help="pcm input device name.  E.g., hw:0,0 or /dev/dsp")
         parser.add_argument("-O", "--audio-output", default="",
-                          help="pcm output device name.  E.g., hw:0,0 or /dev/dsp")
+                            help="pcm output device name.  E.g., hw:0,0 or /dev/dsp")
         parser.add_argument("-r", "--sample-rate", type=eng_float, default=8000,
-                          help="set sample rate to RATE (%(default)r)")
+                            help="set sample rate to RATE (%(default)r)")
         args = parser.parse_args()
         sample_rate = int(args.sample_rate)
-        src = audio.source (sample_rate, args.audio_input)
-        dst = audio.sink (sample_rate, args.audio_output)
+        src = audio.source(sample_rate, args.audio_input)
+        dst = audio.sink(sample_rate, args.audio_output)
 
         vec1 = [1, -1]
         vsource = blocks.vector_source_f(vec1, True)
