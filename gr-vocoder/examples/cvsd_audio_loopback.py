99c987395704fc99ccea4ccdc30ee4dc14ab1ef3
Josh Morman <jmorman@gnuradio.org>
2021-11-24 12:44:30 -0500

    vocoder: pep8 formatting
    Signed-off-by: Josh Morman <jmorman@gnuradio.org>

--- gr-vocoder/examples/cvsd_audio_loopback.py
+++ gr-vocoder/examples/cvsd_audio_loopback.py
@@ -14,6 +14,7 @@ from gnuradio import blocks
 from gnuradio import filter
 from gnuradio import vocoder
 
+
 def build_graph():
     sample_rate = 8000
     scale_factor = 32000
@@ -37,22 +38,25 @@ def build_graph():
     tb.connect(src, src_scale, interp, f2s, enc)
     tb.connect(enc, dec, s2f, decim, sink_scale, sink)
 
-    if 0: # debug
+    if 0:  # debug
         tb.connect(src, blocks.file_sink(gr.sizeof_float, "source.dat"))
-        tb.connect(src_scale, blocks.file_sink(gr.sizeof_float, "src_scale.dat"))
+        tb.connect(src_scale, blocks.file_sink(
+            gr.sizeof_float, "src_scale.dat"))
         tb.connect(interp, blocks.file_sink(gr.sizeof_float, "interp.dat"))
         tb.connect(f2s, blocks.file_sink(gr.sizeof_short, "f2s.dat"))
-        tb.connect(enc, blocks.file_sink(gr.sizeof_char,  "enc.dat"))
+        tb.connect(enc, blocks.file_sink(gr.sizeof_char, "enc.dat"))
         tb.connect(dec, blocks.file_sink(gr.sizeof_short, "dec.dat"))
         tb.connect(s2f, blocks.file_sink(gr.sizeof_float, "s2f.dat"))
         tb.connect(decim, blocks.file_sink(gr.sizeof_float, "decim.dat"))
-        tb.connect(sink_scale, blocks.file_sink(gr.sizeof_float, "sink_scale.dat"))
+        tb.connect(sink_scale, blocks.file_sink(
+            gr.sizeof_float, "sink_scale.dat"))
 
     return tb
+
 
 if __name__ == '__main__':
     tb = build_graph()
     tb.start()
-    input ('Press Enter to exit: ')
+    input('Press Enter to exit: ')
     tb.stop()
     tb.wait()
