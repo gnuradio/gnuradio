790d7d300b4e176808d6ac3f94ec040ce5d2a837
Ryan Volz <ryan.volz@gmail.com>
2021-12-07 17:52:48 -0500

    runtime: Use <spdlog/fmt/fmt.h> header handling SPDLOG_FMT_EXTERNAL
    Regardless of how spdlog was built (with/without SPDLOG_FMT_EXTERNAL),
    this ensures that the proper fmt headers are included. This makes it so
    that fmt is not a required dependency when spdlog is built with
    SPDLOG_FMT_EXTERNAL=OFF.
    
    Signed-off-by: Ryan Volz <ryan.volz@gmail.com>

--- gnuradio-runtime/python/gnuradio/gr/bindings/logger_python.cc
+++ gnuradio-runtime/python/gnuradio/gr/bindings/logger_python.cc
@@ -15,7 +15,7 @@
 /* BINDTOOL_GEN_AUTOMATIC(0)                                                       */
 /* BINDTOOL_USE_PYGCCXML(0)                                                        */
 /* BINDTOOL_HEADER_FILE(logger.h)                                        */
-/* BINDTOOL_HEADER_FILE_HASH(1b018cc2df46366b8b83a4fe08db2806)                     */
+/* BINDTOOL_HEADER_FILE_HASH(3e2c7677a98ddd539794627cd5a43d93)                     */
 /***********************************************************************************/
 
 #include <pybind11/complex.h>
