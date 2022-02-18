ad0563be00bff9a82639e5f668549da8d18dfc47
Volker Schroer <3470424+dl1ksv@users.noreply.github.com>
2022-02-18 12:08:25 +0100

    gnuradio: make test fix a lot of depreciation warnings
    18/243 Testing: qa_hier_block2
    gnuradio-runtime/python/gnuradio/gr/top_block.py:48: DeprecationWarning: setDaemon() is deprecated, set the daemon attribute instead
    
    58/243 Testing: qa_message
    gr-blocks/python/blocks/qa_message.py:44: DeprecationWarning: Please use assertEqual instead.
      self.assertEquals(0, msg.type())
    
    205/243 Testing: qa_ofdm_chanest_vcvc
    ...../usr/lib64/python3.10/random.py:370: DeprecationWarning: non-integer arguments to randrange() have been deprecated since Python 3.10 and will be removed in a subsequent version
      return self.randrange(a, b+1)
    
    114/243 Testing: qa_polar_decoder_sc
    gnuradio/fec/polar/common.py:30: DeprecationWarning: `np.int` is a deprecated alias for the builtin `int`
    
    Deprecated in NumPy 1.20; for more details and guidance: https://numpy.org/devdocs/release/1.20.0-notes.html#deprecations
    
    Signed-off-by: Volker Schroer <3470424+dl1ksv@users.noreply.github.com>

--- gr-blocks/python/blocks/qa_message.py
+++ gr-blocks/python/blocks/qa_message.py
@@ -41,20 +41,20 @@ class test_message(gr_unittest.TestCase):
 
     def test_100(self):
         msg = gr.message(0, 1.5, 2.3)
-        self.assertEquals(0, msg.type())
+        self.assertEqual(0, msg.type())
         self.assertAlmostEqual(1.5, msg.arg1())
         self.assertAlmostEqual(2.3, msg.arg2())
-        self.assertEquals(0, msg.length())
+        self.assertEqual(0, msg.length())
 
     def test_101(self):
         s = b'This is a test'
         msg = gr.message_from_string(s.decode('utf8'))
-        self.assertEquals(s, msg.to_string())
+        self.assertEqual(s, msg.to_string())
 
     def test_102_unicodechars(self):
         s = u"(╯°□°)╯︵ ┻━┻"
         msg = gr.message_from_string(s)
-        self.assertEquals(s.encode('utf8'), msg.to_string())
+        self.assertEqual(s.encode('utf8'), msg.to_string())
 
     # msg_queue was removed from API in 3.8
     # def test_200(self):
@@ -62,23 +62,23 @@ class test_message(gr_unittest.TestCase):
 
     # def body_200(self):
     #     self.msgq.insert_tail(gr.message(0))
-    #     self.assertEquals(1, self.msgq.count())
+    #     self.assertEqual(1, self.msgq.count())
     #     self.msgq.insert_tail(gr.message(1))
-    #     self.assertEquals(2, self.msgq.count())
+    #     self.assertEqual(2, self.msgq.count())
     #     msg0 = self.msgq.delete_head()
-    #     self.assertEquals(0, msg0.type())
+    #     self.assertEqual(0, msg0.type())
     #     msg1 = self.msgq.delete_head()
-    #     self.assertEquals(1, msg1.type())
-    #     self.assertEquals(0, self.msgq.count())
+    #     self.assertEqual(1, msg1.type())
+    #     self.assertEqual(0, self.msgq.count())
 
     # def test_201(self):
     #     self.leak_check(self.body_201)
 
     # def body_201(self):
     #     self.msgq.insert_tail(gr.message(0))
-    #     self.assertEquals(1, self.msgq.count())
+    #     self.assertEqual(1, self.msgq.count())
     #     self.msgq.insert_tail(gr.message(1))
-    #     self.assertEquals(2, self.msgq.count())
+    #     self.assertEqual(2, self.msgq.count())
 
     # def test_202(self):
     #     self.leak_check(self.body_202)
@@ -94,7 +94,7 @@ class test_message(gr_unittest.TestCase):
         tb = gr.top_block()
         tb.connect(src, dst)
         tb.run()
-        self.assertEquals(input_data, dst.data())
+        self.assertEqual(input_data, dst.data())
 
     def test_debug_401(self):
         msg = pmt.intern("TESTING")
