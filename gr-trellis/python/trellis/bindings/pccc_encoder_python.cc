3dd7c1f742b1afd6a8e0ec36e415944aa323f737
Bjoern Kerler <info@revskills.de>
2022-03-27 15:56:17 +0200

    trellis: pccc_encoder python bindings and qa test (#5683)
    * Add missing python binding for trellis pccc_encode
    
    Signed-off-by: Bjoern Kerler <info@revskills.de>

--- gr-trellis/python/trellis/bindings/pccc_encoder_python.cc
+++ gr-trellis/python/trellis/bindings/pccc_encoder_python.cc
@@ -27,4 +27,34 @@ namespace py = pybind11;
 // pydoc.h is automatically generated in the build directory
 #include <pccc_encoder_pydoc.h>
 
-void bind_pccc_encoder(py::module& m) {}
+template <class IN_T, class OUT_T>
+void bind_pccc_encoder_template(py::module& m, const char* classname)
+{
+    using pccc_encoder = gr::trellis::pccc_encoder<IN_T, OUT_T>;
+
+    py::class_<pccc_encoder, gr::block, gr::basic_block, std::shared_ptr<pccc_encoder>>(
+        m, classname)
+        .def(py::init(&gr::trellis::pccc_encoder<IN_T, OUT_T>::make),
+             py::arg("FSM1"),
+             py::arg("ST1"),
+             py::arg("FSM2"),
+             py::arg("ST2"),
+             py::arg("INTERLEAVER"),
+             py::arg("blocklength"))
+        .def("FSM1", &pccc_encoder::FSM1)
+        .def("ST1", &pccc_encoder::ST1)
+        .def("FSM2", &pccc_encoder::FSM2)
+        .def("ST2", &pccc_encoder::ST2)
+        .def("INTERLEAVER", &pccc_encoder::INTERLEAVER)
+        .def("blocklength", &pccc_encoder::blocklength);
+}
+
+void bind_pccc_encoder(py::module& m)
+{
+    bind_pccc_encoder_template<std::uint8_t, std::uint8_t>(m, "pccc_encoder_bb");
+    bind_pccc_encoder_template<std::uint8_t, std::int16_t>(m, "pccc_encoder_bs");
+    bind_pccc_encoder_template<std::uint8_t, std::int32_t>(m, "pccc_encoder_bi");
+    bind_pccc_encoder_template<std::int16_t, std::int16_t>(m, "pccc_encoder_ss");
+    bind_pccc_encoder_template<std::int16_t, std::int32_t>(m, "pccc_encoder_si");
+    bind_pccc_encoder_template<std::int32_t, std::int32_t>(m, "pccc_encoder_ii");
+}
