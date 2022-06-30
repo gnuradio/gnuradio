4ead206af317afb0bddcd67ab4edfa0f2c023331
Josh Morman <jmorman@gnuradio.org>
2022-06-30 09:35:19 -0400

    zeromq: cppzmq>4.8 deprecation fixes
    Signed-off-by: Josh Morman <jmorman@gnuradio.org>
    (cherry picked from commit 0a7b83e5220899698d89f1fca5a385f0701b1730)
    Signed-off-by: Jeff Long <willcode4@gmail.com>

--- gr-zeromq/lib/push_sink_impl.cc
+++ gr-zeromq/lib/push_sink_impl.cc
@@ -42,7 +42,7 @@ int push_sink_impl::work(int noutput_items,
 {
     // Poll with a timeout (FIXME: scheduler can't wait for us)
     zmq::pollitem_t itemsout[] = { { static_cast<void*>(d_socket), 0, ZMQ_POLLOUT, 0 } };
-    zmq::poll(&itemsout[0], 1, d_timeout);
+    zmq::poll(&itemsout[0], 1, std::chrono::milliseconds{ d_timeout });
 
     // If we can send something, do it
     if (itemsout[0].revents & ZMQ_POLLOUT)
