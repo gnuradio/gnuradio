d193c8a9b9059abe72823fd88b43430306cfeb51
Håkon Vågsether <hakon.vagsether@gmail.com>
2022-07-14 15:42:46 +0200

    grcqt: Get rid of old code in block.py
    Signed-off-by: Håkon Vågsether <hakon.vagsether@gmail.com>

--- grc/gui_qt/components/canvas/block.py
+++ grc/gui_qt/components/canvas/block.py
@@ -107,103 +107,6 @@ class PropsDialog(QtWidgets.QDialog):
         self._block.validate()
         self._block.create_shapes_and_labels()
 
-
-'''
-class BlockTitle(QtWidgets.QLabel):
-    def __init__(self, block_key):
-        super(BlockTitle, self).__init__()
-
-        title = block_key.replace('_', ' ').title()
-        self.setText(title)
-        title_font = QtGui.QFont("Sans Serif", 9, QtGui.QFont.Bold)
-        self.setAlignment(Qt.AlignCenter)
-        self.setFont(title_font)
-        self.setContentsMargins(5, 5, 5, 5)
-        self.title_only = False
-        self.color = QtGui.QColor(0xD0, 0xD0, 0xFF)
-
-    def paintEvent(self, event):
-        # handle paintEvent just enough to provide a (partial)
-        # rounded rectangle around the title
-
-        painter = QtGui.QPainter()
-        painter.begin(self)
-        painter.setPen(QtGui.QPen(2))
-        painter.setBrush(QtGui.QBrush(self.color))
-
-        # set rounding to include bottom of title
-        # otherwise, push the bottom of the rounded rectangle
-        # outside of the widget window
-        height = self.height()-1 if self.title_only else self.height()+ARC
-        painter.drawRoundedRect(0, 0, self.width()-1, height, ARC, ARC);
-
-        painter.end()
-
-        # use the default handler for everything else
-        super(BlockTitle, self).paintEvent(event)
-
-    def boundingRect(self): # required to have
-        return QRectF(self.x, self.y, self.current_width, 150) # same as the rectangle we draw
-
-    def mouseReleaseEvent(self, e):
-        super(Block, self).mouseReleaseEvent(e)
-
-    def mouseDoubleClickEvent(self, e):
-        print("DETECTED DOUBLE CLICK!")
-        super(Block, self).mouseDoubleClickEvent(e)
-
-class BlockParams(QtWidgets.QWidget):
-    def __init__(self, params):
-        super(BlockParams, self).__init__()
-        self.params_only = False
-
-        label_font = QtGui.QFont("Sans Serif",  8, QtGui.QFont.Bold)
-        value_font = QtGui.QFont("Sans Serif",  8, QtGui.QFont.Normal)
-
-        layout = QtWidgets.QGridLayout()
-        layout.setSpacing(2)
-        layout.setVerticalSpacing(0)
-        layout.setContentsMargins(ARC, 3, ARC, ARC)
-        for row, (key, value) in enumerate(params):
-            if value is not None:
-                param_label = QtWidgets.QLabel(key+': ')
-                param_label.setAlignment(Qt.AlignRight)
-                param_label.setFont(label_font)
-
-                if len(value) > LONG_VALUE:
-                    value = value[:LONG_VALUE-3] + '...'
-                param_value = QtWidgets.QLabel(value)
-                param_value.setFont(value_font)
-
-                layout.addWidget(param_label, row, 0)
-                layout.addWidget(param_value, row, 1)
-
-        self.setLayout(layout)
-
-        self.setContentsMargins(0, 0, 0, 5)
-        self.color = QtGui.QColor(0xFA, 0xF8, 0xE0)
-
-    def paintEvent(self, event):
-        # handle paintEvent just enough to provide a (partial)
-        # rounded rectangle around the title
-
-        painter = QtGui.QPainter()
-        painter.begin(self)
-        painter.setPen(QtGui.QPen(2))
-        painter.setBrush(QtGui.QBrush(self.color))
-
-        # set rounding to include top of title
-        # otherwise, push the top of the rounded rectangle
-        # outside of the widget window
-        top = 1 if self.params_only else -ARC
-        painter.drawRoundedRect(0, top, self.width()-1, self.height()-1, ARC, ARC);
-
-        painter.end()
-
-        # use the default handler for everything else
-        super(BlockParams, self).paintEvent(event)
-'''
-
 class Block(QtWidgets.QGraphicsItem, CoreBlock):
 
     @classmethod
@@ -395,7 +298,7 @@ class Block(QtWidgets.QGraphicsItem, CoreBlock):
 
         ARC = 10
         rect = QtCore.QRectF(0, 0, self.width, self.height)
-        painter.drawRoundedRect(rect, ARC, ARC);
+        painter.drawRoundedRect(rect, ARC, ARC)
         painter.setPen(QtGui.QPen(1))
 
         # Draw block label text
