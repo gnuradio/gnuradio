37bc582db2856c504304eb85c5120ff51b8563b7
Josh Morman <jmorman@gnuradio.org>
2022-07-26 10:44:07 -0400

    utils/blockbuilder: grc type lookup
    Signed-off-by: Josh Morman <jmorman@gnuradio.org>

--- utils/blockbuilder/scripts/filters.py
+++ utils/blockbuilder/scripts/filters.py
@@ -4,8 +4,8 @@
 type_lookup = {
     'cf64': ['std::complex<double>', 'complex', 'complex'],
     'cf32': ['std::complex<float>', 'complex', 'complex'],
-    'rf64': ['double', 'float', 'real'],
-    'rf32': ['float', 'float', 'real'],
+    'rf64': ['double', 'float', 'float'],
+    'rf32': ['float', 'float', 'float'],
     'ri64': ['int64_t', 'int', 'int'],
     'ri32': ['int32_t', 'int', 'int'],
     'ri16': ['int16_t', 'int', 'short'],
@@ -22,8 +22,18 @@ type_lookup = {
 def is_list(value):
     return isinstance(value, list)
 
+def is_numeric(value):
+    return type(value) is int or type(value) is float or type(value) is complex
+
 # Parses on '/' and returns the second part
 def get_linked_value(value):
+    newvalue = value
+    if '/' in value:
+        list1 = value.split('/')
+        newvalue = list1[1]
+    return newvalue
+
+def get_linked_value_with_args(value):
     newvalue = value
     if '/' in value:
         list1 = value.split('/')
@@ -40,9 +50,35 @@ def cpp_type(input, vec=False):
         x = type_lookup[input][0]
     else:
         x = get_linked_value(input)
-    
+        if input.startswith('parameters/'):
+            x = 'args.' + x
+
     if (vec):
         return f'std::vector<{x}>'
+    return x
+
+def grc_type(input, vec=False, ref=False):
+    if is_list(input):
+        input = input[0]
+    try:
+        if is_numeric(eval(input)):
+            return input
+    except:
+        pass
+    
+    if input.startswith('enums/'):
+        return 'enum'
+    
+    if input in type_lookup:
+        x = type_lookup[input][2]
+    else:
+        x = get_linked_value(input)
+        if (ref):
+            x = f'${{{x}}}'
+        
+    if (vec):
+        return f'{x}_vector'
+
     return x
 
 def py_type(input):
@@ -55,6 +91,8 @@ def custom_filters():
     return {
         'is_list': is_list,
         'cpp_type': cpp_type,
+        'grc_type': grc_type,
         'py_type': py_type,
         'get_linked_value': get_linked_value,
+        'get_linked_value_with_args': get_linked_value_with_args,
     }
