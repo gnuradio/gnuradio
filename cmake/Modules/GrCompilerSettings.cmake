af78fad36d41b7c0d653ad21ec5ad8d58585d230
Josh Morman <jmorman@gnuradio.org>
2022-08-18 14:58:06 -0400

    cmake: reformatted
    cmake-format -c .cmake-format.py \
          -i cmake/cmake_uninstall.cmake.in cmake/Modules/* \
          cmake/Toolchains/arm_cortex_a8_native.cmake \
          cmake/Toolchains/oe-sdk_cross.cmake \
          $(git ls-files | grep -x mako CMakeLists.txt )
    
    Signed-off-by: Josh Morman <jmorman@gnuradio.org>

--- cmake/Modules/GrCompilerSettings.cmake
+++ cmake/Modules/GrCompilerSettings.cmake
@@ -14,50 +14,63 @@ set(__INCLUDED_GR_COMPILER_SETTING_CMAKE TRUE)
 # Compiler version setup
 ########################################################################
 # Append -O2 optimization flag for Debug builds (Not on MSVC since conflicts with RTC1 flag)
-IF (NOT MSVC)
-    SET(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O2")
-    SET(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -O2")
-ENDIF()
+if(NOT MSVC)
+    set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O2")
+    set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -O2")
+endif()
 
 # Check compiler version against our minimum
-IF(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-    IF(DEFINED CMAKE_CXX_COMPILER_VERSION)
-        IF(${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS ${GCC_MIN_VERSION})
-            MESSAGE(WARNING "\nThe compiler selected to build GNU Radio (GCC version ${CMAKE_CXX_COMPILER_VERSION} : ${CMAKE_CXX_COMPILER}) is older than that officially supported (${GCC_MIN_VERSION} minimum). This build may or not work. We highly recommend using a more recent GCC version.")
-        ENDIF()
-    ELSE()
-        MESSAGE(WARNING "\nCannot determine the version of the compiler selected to build GNU Radio (GCC : ${CMAKE_CXX_COMPILER}). This build may or not work. We highly recommend using GCC version ${GCC_MIN_VERSION} or more recent.")
-    ENDIF()
-ELSEIF(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-    EXECUTE_PROCESS(COMMAND
-        ${CMAKE_CXX_COMPILER} -v
-        RESULT_VARIABLE _res ERROR_VARIABLE _err
+if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    if(DEFINED CMAKE_CXX_COMPILER_VERSION)
+        if(${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS ${GCC_MIN_VERSION})
+            message(
+                WARNING
+                    "\nThe compiler selected to build GNU Radio (GCC version ${CMAKE_CXX_COMPILER_VERSION} : ${CMAKE_CXX_COMPILER}) is older than that officially supported (${GCC_MIN_VERSION} minimum). This build may or not work. We highly recommend using a more recent GCC version."
+            )
+        endif()
+    else()
+        message(
+            WARNING
+                "\nCannot determine the version of the compiler selected to build GNU Radio (GCC : ${CMAKE_CXX_COMPILER}). This build may or not work. We highly recommend using GCC version ${GCC_MIN_VERSION} or more recent."
+        )
+    endif()
+elseif(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+    execute_process(
+        COMMAND ${CMAKE_CXX_COMPILER} -v
+        RESULT_VARIABLE _res
+        ERROR_VARIABLE _err
         ERROR_STRIP_TRAILING_WHITESPACE)
-    IF(${_res} STREQUAL "0")
+    if(${_res} STREQUAL "0")
         # output is in error stream
-        STRING(REGEX MATCH "^Apple.*" IS_APPLE ${_err})
-        IF("${IS_APPLE}" STREQUAL "")
-            SET(MIN_VERSION ${CLANG_MIN_VERSION})
-            SET(APPLE_STR "")
+        string(REGEX MATCH "^Apple.*" IS_APPLE ${_err})
+        if("${IS_APPLE}" STREQUAL "")
+            set(MIN_VERSION ${CLANG_MIN_VERSION})
+            set(APPLE_STR "")
             # retrieve the compiler's version from it
-            STRING(REGEX MATCH "clang version [0-9.]+" CLANG_OTHER_VERSION ${_err})
-            STRING(REGEX MATCH "[0-9.]+" CLANG_VERSION ${CLANG_OTHER_VERSION})
-        ELSE()
-            SET(MIN_VERSION ${APPLECLANG_MIN_VERSION})
-            SET(APPLE_STR "Apple ")
+            string(REGEX MATCH "clang version [0-9.]+" CLANG_OTHER_VERSION ${_err})
+            string(REGEX MATCH "[0-9.]+" CLANG_VERSION ${CLANG_OTHER_VERSION})
+        else()
+            set(MIN_VERSION ${APPLECLANG_MIN_VERSION})
+            set(APPLE_STR "Apple ")
             # retrieve the compiler's version from it
-            STRING(REGEX MATCH "(clang-[0-9.]+)" CLANG_APPLE_VERSION ${_err})
-            STRING(REGEX MATCH "[0-9.]+" CLANG_VERSION ${CLANG_APPLE_VERSION})
-        ENDIF()
-        IF(${CLANG_VERSION} VERSION_LESS "${MIN_VERSION}")
-            MESSAGE(WARNING "\nThe compiler selected to build GNU Radio (${APPLE_STR}Clang version ${CLANG_VERSION} : ${CMAKE_CXX_COMPILER}) is older than that officially supported (${MIN_VERSION} minimum). This build may or not work. We highly recommend using Apple Clang version ${APPLECLANG_MIN_VERSION} or more recent, or Clang version ${CLANG_MIN_VERSION} or more recent.")
-        ENDIF()
-    ELSE()
-        MESSAGE(WARNING "\nCannot determine the version of the compiler selected to build GNU Radio (${APPLE_STR}Clang : ${CMAKE_CXX_COMPILER}). This build may or not work. We highly recommend using Apple Clang version ${APPLECLANG_MIN_VERSION} or more recent, or Clang version ${CLANG_MIN_VERSION} or more recent.")
-    ENDIF()
-ELSE()
-    MESSAGE(status "Skipping compiler version check.")
-ENDIF()
+            string(REGEX MATCH "(clang-[0-9.]+)" CLANG_APPLE_VERSION ${_err})
+            string(REGEX MATCH "[0-9.]+" CLANG_VERSION ${CLANG_APPLE_VERSION})
+        endif()
+        if(${CLANG_VERSION} VERSION_LESS "${MIN_VERSION}")
+            message(
+                WARNING
+                    "\nThe compiler selected to build GNU Radio (${APPLE_STR}Clang version ${CLANG_VERSION} : ${CMAKE_CXX_COMPILER}) is older than that officially supported (${MIN_VERSION} minimum). This build may or not work. We highly recommend using Apple Clang version ${APPLECLANG_MIN_VERSION} or more recent, or Clang version ${CLANG_MIN_VERSION} or more recent."
+            )
+        endif()
+    else()
+        message(
+            WARNING
+                "\nCannot determine the version of the compiler selected to build GNU Radio (${APPLE_STR}Clang : ${CMAKE_CXX_COMPILER}). This build may or not work. We highly recommend using Apple Clang version ${APPLECLANG_MIN_VERSION} or more recent, or Clang version ${CLANG_MIN_VERSION} or more recent."
+        )
+    endif()
+else()
+    message(STATUS "Skipping compiler version check.")
+endif()
 
 # Configure C++ standard if not externally specified (will actually be
 # set after CppUnit check below). Use the variable CMAKE_CXX_STANDARD
@@ -76,19 +89,17 @@ set(GR_CXX_VERSION_FEATURE cxx_std_${CMAKE_CXX_STANDARD})
 include(GrMiscUtils) #compiler flag check
 
 include(TestBigEndian)
-TEST_BIG_ENDIAN(GR_IS_BIG_ENDIAN)
+test_big_endian(GR_IS_BIG_ENDIAN)
 
-if(CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR
-   CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+if(CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
     if(NOT WIN32)
         #http://gcc.gnu.org/wiki/Visibility
-        GR_ADD_CXX_COMPILER_FLAG_IF_AVAILABLE(-fvisibility=hidden HAVE_VISIBILITY_HIDDEN)
+        gr_add_cxx_compiler_flag_if_available(-fvisibility=hidden HAVE_VISIBILITY_HIDDEN)
     endif(NOT WIN32)
-    GR_ADD_CXX_COMPILER_FLAG_IF_AVAILABLE(-Wsign-compare HAVE_WARN_SIGN_COMPARE)
-    GR_ADD_CXX_COMPILER_FLAG_IF_AVAILABLE(-Wall HAVE_WARN_ALL)
-    GR_ADD_CXX_COMPILER_FLAG_IF_AVAILABLE(-Wno-uninitialized HAVE_WARN_NO_UNINITIALIZED)
-endif(CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR
-      CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    gr_add_cxx_compiler_flag_if_available(-Wsign-compare HAVE_WARN_SIGN_COMPARE)
+    gr_add_cxx_compiler_flag_if_available(-Wall HAVE_WARN_ALL)
+    gr_add_cxx_compiler_flag_if_available(-Wno-uninitialized HAVE_WARN_NO_UNINITIALIZED)
+endif(CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
 
 if(MSVC)
     include_directories(${PROJECT_SOURCE_DIR}/cmake/msvc) #missing headers
--- cmake/Modules/GrPlatform.cmake
+++ cmake/Modules/GrPlatform.cmake
@@ -17,34 +17,44 @@ if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
     set(LINUX TRUE)
 endif()
 
-if(NOT CMAKE_CROSSCOMPILING AND LINUX AND EXISTS "/etc/debian_version")
+if(NOT CMAKE_CROSSCOMPILING
+   AND LINUX
+   AND EXISTS "/etc/debian_version")
     set(DEBIAN TRUE)
 endif()
 
-if(NOT CMAKE_CROSSCOMPILING AND LINUX AND EXISTS "/etc/redhat-release")
+if(NOT CMAKE_CROSSCOMPILING
+   AND LINUX
+   AND EXISTS "/etc/redhat-release")
     set(REDHAT TRUE)
 endif()
 
-if(NOT CMAKE_CROSSCOMPILING AND LINUX AND EXISTS "/etc/slackware-version")
+if(NOT CMAKE_CROSSCOMPILING
+   AND LINUX
+   AND EXISTS "/etc/slackware-version")
     set(SLACKWARE TRUE)
 endif()
 
 ########################################################################
 # when the library suffix should be 64 (applies to redhat linux family)
 ########################################################################
-if (REDHAT OR SLACKWARE)
+if(REDHAT OR SLACKWARE)
     set(LIB64_CONVENTION TRUE)
 endif()
 
-if(NOT DEFINED LIB_SUFFIX AND LIB64_CONVENTION AND CMAKE_SYSTEM_PROCESSOR MATCHES "64$")
+if(NOT DEFINED LIB_SUFFIX
+   AND LIB64_CONVENTION
+   AND CMAKE_SYSTEM_PROCESSOR MATCHES "64$")
     set(LIB_SUFFIX 64)
 endif()
 
 ########################################################################
 # Detect /lib versus /lib64
 ########################################################################
-if (CMAKE_INSTALL_LIBDIR MATCHES lib64)
+if(CMAKE_INSTALL_LIBDIR MATCHES lib64)
     set(LIB_SUFFIX 64)
 endif()
 
-set(LIB_SUFFIX ${LIB_SUFFIX} CACHE STRING "lib directory suffix")
+set(LIB_SUFFIX
+    ${LIB_SUFFIX}
+    CACHE STRING "lib directory suffix")
