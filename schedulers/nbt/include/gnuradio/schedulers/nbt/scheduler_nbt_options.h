5c84db88fae366b6230fe34cee44a03f54ee9309
Josh Morman <jmorman@gnuradio.org>
2022-09-23 09:32:20 -0400

    scheduler/nbt: make default_buffer a config opt
    Signed-off-by: Josh Morman <jmorman@gnuradio.org>

--- schedulers/nbt/include/gnuradio/schedulers/nbt/scheduler_nbt_options.h
+++ schedulers/nbt/include/gnuradio/schedulers/nbt/scheduler_nbt_options.h
@@ -8,6 +8,7 @@ namespace schedulers {
 struct scheduler_nbt_options {
     std::string name = "nbt";
     size_t default_buffer_size = 32768;
+    std::string default_buffer_type = "cpu_vmcirc";
     bool flush = true;
     size_t flush_count = 8;
     size_t flush_sleep_ms = 10;
