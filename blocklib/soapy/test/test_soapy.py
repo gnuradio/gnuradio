c9e0dc69fd38b177acd678e68171f6ed573bd986
Josh Morman <jmorman@gnuradio.org>
2022-10-05 15:07:12 -0400

    soapy: fix bindings
    Signed-off-by: Josh Morman <jmorman@gnuradio.org>

--- blocklib/soapy/test/test_soapy.py
+++ blocklib/soapy/test/test_soapy.py
@@ -5,12 +5,12 @@ samp_rate = 250000
 src = soapy.source_c('driver=rtlsdr',1)
 src.set_sample_rate(0, samp_rate)
 src.set_gain_mode(0, False)
-src.set_frequency(0, 99500000)
+src.set_frequency(0, 90500000)
 src.set_frequency_correction(0, 0)
-src.set_gain(0, 'TUNER', 20)
+src.set_gain(0, 'TUNER', 40)
 
 hd = streamops.head(100000)
-snk = blocks.null_sink()
+snk = blocks.vector_sink_c()
 
 fg = gr.flowgraph()
 
@@ -19,3 +19,9 @@ fg.connect(hd,0,snk,0)
 
 fg.start()
 fg.wait()
+
+from matplotlib import pyplot as plt
+import numpy as np
+plt.plot(np.real(snk.data()))
+plt.plot(np.imag(snk.data()))
+plt.show()
