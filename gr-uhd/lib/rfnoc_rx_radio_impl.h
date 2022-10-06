e08bea575304c9a06e79921ff2a2bdc3a267d759
Martin Braun <martin.braun@ettus.com>
2022-10-06 16:35:58 +0200

    uhd: Fix consistent override warnings
    The RFNoC block impls had some missing overrides.
    
    Signed-off-by: Martin Braun <martin.braun@ettus.com>
    (cherry picked from commit b22cddf8ab6cb1243a13be9b5180bb84545a9299)
    Signed-off-by: Jeff Long <willcode4@gmail.com>

--- gr-uhd/lib/rfnoc_rx_radio_impl.h
+++ gr-uhd/lib/rfnoc_rx_radio_impl.h
@@ -19,27 +19,32 @@ class rfnoc_rx_radio_impl : public rfnoc_rx_radio
 {
 public:
     rfnoc_rx_radio_impl(::uhd::rfnoc::noc_block_base::sptr block_ref);
-    ~rfnoc_rx_radio_impl();
+    ~rfnoc_rx_radio_impl() override;
 
     /*** API *****************************************************************/
-    double set_rate(const double rate);
-    void set_antenna(const std::string& antenna, const size_t chan);
-    double set_frequency(const double frequency, const size_t chan);
-    void set_tune_args(const ::uhd::device_addr_t& args, const size_t chan);
-    double set_gain(const double gain, const size_t chan);
-    double set_gain(const double gain, const std::string& name, const size_t chan);
-    void set_agc(const bool enable, const size_t chan);
-    void set_gain_profile(const std::string& profile, const size_t chan);
-    double set_bandwidth(const double bandwidth, const size_t chan);
-    void
-    set_lo_source(const std::string& source, const std::string& name, const size_t chan);
-    void
-    set_lo_export_enabled(const bool enabled, const std::string& name, const size_t chan);
-    double set_lo_freq(const double freq, const std::string& name, const size_t chan);
-    void set_dc_offset(const bool enable, const size_t chan);
-    void set_dc_offset(const std::complex<double>& offset, const size_t chan);
-    void set_iq_balance(const bool enable, const size_t chan);
-    void set_iq_balance(const std::complex<double>& correction, const size_t chan);
+    double set_rate(const double rate) override;
+    void set_antenna(const std::string& antenna, const size_t chan) override;
+    double set_frequency(const double frequency, const size_t chan) override;
+    void set_tune_args(const ::uhd::device_addr_t& args, const size_t chan) override;
+    double set_gain(const double gain, const size_t chan) override;
+    double
+    set_gain(const double gain, const std::string& name, const size_t chan) override;
+    void set_agc(const bool enable, const size_t chan) override;
+    void set_gain_profile(const std::string& profile, const size_t chan) override;
+    double set_bandwidth(const double bandwidth, const size_t chan) override;
+    void set_lo_source(const std::string& source,
+                       const std::string& name,
+                       const size_t chan) override;
+    void set_lo_export_enabled(const bool enabled,
+                               const std::string& name,
+                               const size_t chan) override;
+    double
+    set_lo_freq(const double freq, const std::string& name, const size_t chan) override;
+    void set_dc_offset(const bool enable, const size_t chan) override;
+    void set_dc_offset(const std::complex<double>& offset, const size_t chan) override;
+    void set_iq_balance(const bool enable, const size_t chan) override;
+    void set_iq_balance(const std::complex<double>& correction,
+                        const size_t chan) override;
 
 private:
     ::uhd::rfnoc::radio_control::sptr d_radio_ref;
