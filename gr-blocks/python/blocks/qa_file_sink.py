9f8ab0ae0849152c678c79f93a2da711a07353e1
Clayton Smith <argilo@gmail.com>
2022-10-21 12:05:23 -0400

    blocks: Fix file tests to work on Windows
    As noted in the documentation for NamedTemporaryFile, it is not possible
    on all platforms to open the temporary file a second time before closing
    it. Thus, we need to be sure to close the file before opening it in a
    flowgraph.
    
    Signed-off-by: Clayton Smith <argilo@gmail.com>

--- gr-blocks/python/blocks/qa_file_sink.py
+++ gr-blocks/python/blocks/qa_file_sink.py
@@ -19,30 +19,34 @@ class test_file_sink(gr_unittest.TestCase):
     def setUp(self):
         os.environ['GR_CONF_CONTROLPORT_ON'] = 'False'
         self.tb = gr.top_block()
+        temp = tempfile.NamedTemporaryFile(delete=False)
+        temp.close()
+        self._datafilename = temp.name
 
     def tearDown(self):
         self.tb = None
+        os.unlink(self._datafilename)
 
     def test_file_sink(self):
         data = range(1000)
         expected_result = data
 
-        with tempfile.NamedTemporaryFile() as temp:
-            src = blocks.vector_source_f(data)
-            snk = blocks.file_sink(gr.sizeof_float, temp.name)
-            snk.set_unbuffered(True)
-            self.tb.connect(src, snk)
-            self.tb.run()
+        src = blocks.vector_source_f(data)
+        snk = blocks.file_sink(gr.sizeof_float, self._datafilename)
+        snk.set_unbuffered(True)
+        self.tb.connect(src, snk)
+        self.tb.run()
+        snk.close()
 
-            # Check file length (float: 4 * nsamples)
-            file_size = os.stat(temp.name).st_size
-            self.assertEqual(file_size, 4 * len(data))
+        # Check file length (float: 4 * nsamples)
+        file_size = os.stat(self._datafilename).st_size
+        self.assertEqual(file_size, 4 * len(data))
 
-            # Check file contents
-            datafile = open(temp.name, 'rb')
-            result_data = array.array('f')
+        # Check file contents
+        result_data = array.array('f')
+        with open(self._datafilename, 'rb') as datafile:
             result_data.fromfile(datafile, len(data))
-            self.assertFloatTuplesAlmostEqual(expected_result, result_data)
+        self.assertFloatTuplesAlmostEqual(expected_result, result_data)
 
 
 if __name__ == '__main__':
