918ebec6f132f3857f36bfb6f5f51cc2aebeba36
Marcus Müller <mmueller@gnuradio.org>
2022-11-01 13:00:07 +0100

    digital: avoid unnecessary refcount modifications in often-invoked msg handler
    Signed-off-by: Marcus Müller <mmueller@gnuradio.org>

--- gr-digital/lib/crc_check_impl.h
+++ gr-digital/lib/crc_check_impl.h
@@ -44,7 +44,7 @@ public:
                      gr_vector_int& ninput_items,
                      gr_vector_const_void_star& input_items,
                      gr_vector_void_star& output_items);
-    void msg_handler(pmt::pmt_t pmt_msg);
+    void msg_handler(const pmt::pmt_t& pmt_msg);
 };
 
 } // namespace digital
