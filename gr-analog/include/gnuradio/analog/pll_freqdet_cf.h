2049a0c9cd4080883998095ef02e3fb3c126786c
Josh Morman <jmorman@gnuradio.org>
2022-11-08 13:16:18 -0500

    analog: remove wrapped inherited methods from control_loop
    Signed-off-by: Josh Morman <jmorman@gnuradio.org>
    (cherry picked from commit 60e4df3b89b25c1eefb447c73c8db41058adb0cf)
    Signed-off-by: Jeff Long <willcode4@gmail.com>

--- gr-analog/include/gnuradio/analog/pll_freqdet_cf.h
+++ gr-analog/include/gnuradio/analog/pll_freqdet_cf.h
@@ -48,24 +48,6 @@ public:
      * \param min_freq: minimum (normalized) frequency PLL will lock to.
      */
     static sptr make(float loop_bw, float max_freq, float min_freq);
-
-    void set_loop_bandwidth(float bw) override = 0;
-    virtual void set_damping_factor(float df) = 0;
-    virtual void set_alpha(float alpha) = 0;
-    virtual void set_beta(float beta) = 0;
-    virtual void set_frequency(float freq) = 0;
-    virtual void set_phase(float phase) = 0;
-    virtual void set_min_freq(float freq) = 0;
-    virtual void set_max_freq(float freq) = 0;
-
-    virtual float get_loop_bandwidth() const = 0;
-    virtual float get_damping_factor() const = 0;
-    virtual float get_alpha() const = 0;
-    virtual float get_beta() const = 0;
-    virtual float get_frequency() const = 0;
-    virtual float get_phase() const = 0;
-    virtual float get_min_freq() const = 0;
-    virtual float get_max_freq() const = 0;
 };
 
 } /* namespace analog */
--- gr-analog/python/analog/bindings/pll_freqdet_cf_python.cc
+++ gr-analog/python/analog/bindings/pll_freqdet_cf_python.cc
@@ -14,7 +14,7 @@
 /* BINDTOOL_GEN_AUTOMATIC(0)                                                       */
 /* BINDTOOL_USE_PYGCCXML(0)                                                        */
 /* BINDTOOL_HEADER_FILE(pll_freqdet_cf.h)                                        */
-/* BINDTOOL_HEADER_FILE_HASH(aac775a72088e302d8d52ba5367e36b2)                     */
+/* BINDTOOL_HEADER_FILE_HASH(1d03d14ad56b45f6f1460711ba66ea67)                     */
 /***********************************************************************************/
 
 #include <pybind11/complex.h>
@@ -46,87 +46,6 @@ void bind_pll_freqdet_cf(py::module& m)
              py::arg("min_freq"),
              D(pll_freqdet_cf, make))
 
-
-        .def("set_loop_bandwidth",
-             &pll_freqdet_cf::set_loop_bandwidth,
-             py::arg("bw"),
-             D(pll_freqdet_cf, set_loop_bandwidth))
-
-
-        .def("set_damping_factor",
-             &pll_freqdet_cf::set_damping_factor,
-             py::arg("df"),
-             D(pll_freqdet_cf, set_damping_factor))
-
-
-        .def("set_alpha",
-             &pll_freqdet_cf::set_alpha,
-             py::arg("alpha"),
-             D(pll_freqdet_cf, set_alpha))
-
-
-        .def("set_beta",
-             &pll_freqdet_cf::set_beta,
-             py::arg("beta"),
-             D(pll_freqdet_cf, set_beta))
-
-
-        .def("set_frequency",
-             &pll_freqdet_cf::set_frequency,
-             py::arg("freq"),
-             D(pll_freqdet_cf, set_frequency))
-
-
-        .def("set_phase",
-             &pll_freqdet_cf::set_phase,
-             py::arg("phase"),
-             D(pll_freqdet_cf, set_phase))
-
-
-        .def("set_min_freq",
-             &pll_freqdet_cf::set_min_freq,
-             py::arg("freq"),
-             D(pll_freqdet_cf, set_min_freq))
-
-
-        .def("set_max_freq",
-             &pll_freqdet_cf::set_max_freq,
-             py::arg("freq"),
-             D(pll_freqdet_cf, set_max_freq))
-
-
-        .def("get_loop_bandwidth",
-             &pll_freqdet_cf::get_loop_bandwidth,
-             D(pll_freqdet_cf, get_loop_bandwidth))
-
-
-        .def("get_damping_factor",
-             &pll_freqdet_cf::get_damping_factor,
-             D(pll_freqdet_cf, get_damping_factor))
-
-
-        .def("get_alpha", &pll_freqdet_cf::get_alpha, D(pll_freqdet_cf, get_alpha))
-
-
-        .def("get_beta", &pll_freqdet_cf::get_beta, D(pll_freqdet_cf, get_beta))
-
-
-        .def("get_frequency",
-             &pll_freqdet_cf::get_frequency,
-             D(pll_freqdet_cf, get_frequency))
-
-
-        .def("get_phase", &pll_freqdet_cf::get_phase, D(pll_freqdet_cf, get_phase))
-
-
-        .def("get_min_freq",
-             &pll_freqdet_cf::get_min_freq,
-             D(pll_freqdet_cf, get_min_freq))
-
-
-        .def("get_max_freq",
-             &pll_freqdet_cf::get_max_freq,
-             D(pll_freqdet_cf, get_max_freq))
 
         ;
 }
