d156fd8c7eaf77002a06b1e6dc42c2811e98caee
Josh Morman <jmorman@gnuradio.org>
2022-11-09 16:31:22 -0500

    qa: tag test for stream_to_vector
    Signed-off-by: Josh Morman <jmorman@gnuradio.org>

--- blocklib/streamops/stream_to_vector/qa_stream_to_vector.py
+++ blocklib/streamops/stream_to_vector/qa_stream_to_vector.py
@@ -6,6 +6,17 @@
 #
 
 from gnuradio import gr, gr_unittest, blocks, streamops
+import pmtf
+
+def make_tag(key, value, offset, srcid=None):
+    
+    if srcid is None:
+        tag = gr.tag_t(offset, {key: pmtf.pmt(value)})
+    else:
+        tag = gr.tag_t(offset, {key: pmtf.pmt(value), "srcid": pmtf.pmt(srcid)})
+    return tag
+
+
 
 class test_stream_to_vector(gr_unittest.TestCase):
     def setUp(self):
@@ -44,6 +55,36 @@ class test_stream_to_vector(gr_unittest.TestCase):
         dst_data = snk.data()
 
         self.assertFloatTuplesAlmostEqual(data, dst_data)
+
+    def test_003_tags(self):
+        vlen = 11
+        N = 333*vlen
+        data = list(range(N))
+
+        src_tags = []
+        expected_offsets = []
+        for ii in range(333*vlen):
+            if (ii % 15 == 0):
+                src_tags.append(make_tag('key', 'val', ii, 'src'))
+                expected_offsets.append( ii // vlen)
+
+        src = blocks.vector_source_f(data, tags=src_tags)
+        op = streamops.stream_to_vector(vlen)
+        snk = blocks.vector_sink_f(vlen=vlen)
+        tb = gr.flowgraph()
+        tb.connect([src,op,snk])
+        tb.run()
+        dst_data = snk.data()
+
+        self.assertFloatTuplesAlmostEqual(data, dst_data)
+
+        # compare all tags
+        rx_tags = snk.tags()
+        self.assertEqual(len(src_tags), len(rx_tags))
+
+        rx_offsets = [x.offset() for x in rx_tags]
+        self.assertEqual(rx_offsets, expected_offsets)
+
 
 if __name__ == '__main__':
     gr_unittest.run(test_stream_to_vector)
