c530feb8c9c030cdb5e22c4f20524263769d8238
Håkon Vågsether <hakon.vagsether@gmail.com>
2023-01-11 22:58:28 +0100

    grcqt: Add Preferences dialog
    Signed-off-by: Håkon Vågsether <hakon.vagsether@gmail.com>

---
+++ grc/gui_qt/components/preferences.py
@@ -0,0 +1,41 @@
+from __future__ import absolute_import, print_function
+
+# Standard modules
+import logging
+import os
+import sys
+import subprocess
+
+# Third-party  modules
+import six
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.Qt import Qt
+from PyQt5.QtGui import QStandardItemModel
+
+
+# Logging
+log = logging.getLogger(__name__)
+
+#TODO: Move this to a separate file
+class PreferencesDialog(QtWidgets.QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setMinimumSize(600, 400)
+        self.setModal(True)
+
+        self.setWindowTitle("GRC Preferences")
+        self.tabs = QtWidgets.QTabWidget()
+        self.tabs.addTab(QtWidgets.QWidget(), "General")
+        self.tabs.addTab(QtWidgets.QWidget(), "Flowgraphs")
+        self.tabs.addTab(QtWidgets.QWidget(), "Appearance")        
+
+        buttons = QtWidgets.QDialogButtonBox.Ok | QtWidgets.QDialogButtonBox.Cancel
+        self.buttonBox = QtWidgets.QDialogButtonBox(buttons)
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+        self.layout = QtWidgets.QVBoxLayout()
+        self.layout.addWidget(self.tabs)
+        self.layout.addWidget(self.buttonBox)
+
+        self.setLayout(self.layout)
