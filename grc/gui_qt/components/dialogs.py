12a603f1be09ba1e612a502ea41c1a5f15fda995
Håkon Vågsether <hakon.vagsether@gmail.com>
2023-01-14 18:10:05 +0100

    grcqt: Add functioning Errors dialog
    Signed-off-by: Håkon Vågsether <hakon.vagsether@gmail.com>

--- grc/gui_qt/components/dialogs.py
+++ grc/gui_qt/components/dialogs.py
@@ -7,6 +7,7 @@ import logging
 import six
 
 from qtpy import QtCore, QtGui, QtWidgets
+from ..Constants import MIN_DIALOG_HEIGHT
 
 
 class ErrorsDialog(QtWidgets.QDialog):
@@ -15,34 +16,45 @@ class ErrorsDialog(QtWidgets.QDialog):
         self.flowgraph = flowgraph
         self.store = []
         self.setModal(True)
+        self.resize(700, MIN_DIALOG_HEIGHT)
         self.setWindowTitle("Errors and Warnings")
         buttons = QtWidgets.QDialogButtonBox.Ok
         self.buttonBox = QtWidgets.QDialogButtonBox(buttons)
         self.treeview = QtWidgets.QTreeView()
+        self.model = QtGui.QStandardItemModel()
+        self.treeview.setModel(self.model)
+        
         self.layout = QtWidgets.QVBoxLayout()
         self.layout.addWidget(self.treeview)
         self.layout.addWidget(self.buttonBox)
-        self.setLayout(self.layout)
-        
+        self.setLayout(self.layout)        
+        self.update()
     
     def update(self):
-        self.store = []
+        self.model = QtGui.QStandardItemModel()
+        self.model.setHorizontalHeaderLabels(['Source', 'Aspect', 'Message'])
         for element, message in self.flowgraph.iter_error_messages():
             if element.is_block:
-                src, aspect = element.name, ''
+                print("bl")
+                src, aspect = QtGui.QStandardItem(element.name), QtGui.QStandardItem('')
             elif element.is_connection:
-                src = element.source_block.name
-                aspect = "Connection to '{}'".format(element.sink_block.name)
+                print("con")
+                src = QtGui.QStandardItem(element.source_block.name)
+                aspect = QtGui.QStandardItem("Connection to '{}'".format(element.sink_block.name))
             elif element.is_port:
-                src = element.parent_block.name
-                aspect = "{} '{}'".format(
-                    'Sink' if element.is_sink else 'Source', element.name)
+                print("port")
+                src = QtGui.QStandardItem(element.parent_block.name)
+                aspect = QtGui.QStandardItem("{} '{}'".format(
+                    'Sink' if element.is_sink else 'Source', element.name))
             elif element.is_param:
-                src = element.parent_block.name
-                aspect = "Param '{}'".format(element.name)
+                print("par")
+                src = QtGui.QStandardItem(element.parent_block.name)
+                aspect = QtGui.QStandardItem("Param '{}'".format(element.name))
             else:
-                src = aspect = ''
-            self.store.append([src, aspect, message])
+                print("else")
+                src = aspect = QtGui.QStandardItem('')
+            self.model.appendRow([src, aspect, QtGui.QStandardItem(message)])
+        self.treeview.setModel(self.model)
 
 
 '''
