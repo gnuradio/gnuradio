379f5a3e79337ecaa88d1bf4bf82744a5c2786e2
Marcus Müller <mmueller@gnuradio.org>
2023-02-01 13:50:40 +0100

    runtime: getpagesize for windows
    Signed-off-by: Marcus Müller <mmueller@gnuradio.org>

--- gnuradio-runtime/lib/pagesize.cc
+++ gnuradio-runtime/lib/pagesize.cc
@@ -1,6 +1,7 @@
 /* -*- c++ -*- */
 /*
  * Copyright 2003,2013 Free Software Foundation, Inc.
+ * Copyright 2023 Marcus Müller
  *
  * This file is part of GNU Radio
  *
@@ -12,37 +13,74 @@
 #include "config.h"
 #endif
 
-#include <gnuradio/logger.h>
-#include <gnuradio/prefs.h>
-
 #include "pagesize.h"
-#include <unistd.h>
+#include <gnuradio/logger.h>
 
 namespace gr {
 
-#if defined(_WIN32) && defined(HAVE_GETPAGESIZE)
-extern "C" size_t getpagesize(void);
+#if defined(_WIN32)
+
+#include <Sysinfoapi.h>
+
+int native_pagesize(const gr::logger_ptr logger)
+{
+    SYSTEM_INFO win_sysinfo;
+    GetNativeSystemInfo(&win_sysinfo);
+    auto psize = win_sysinfo.dwPageSize;
+    if (psize <= 0) {
+        logger->error("GetNativeSystemInfo: Got invalid dwPageSize {}", psize);
+        return -1;
+    }
+    return psize;
+}
+
+#elif defined(HAVE_GETPAGESIZE) //_WIN32 undefined,  getpagesize available
+
+#include <unistd.h>
+
+int native_pagesize(const gr::logger_ptr logger) { return getpagesize(); }
+
+#elif defined(HAVE_SYSCONF) //_WIN32 undefined, sysconf available
+
+#include <unistd.h>
+
+int native_pagesize(const gr::logger_ptr logger)
+{
+    int pagesize = sysconf(_SC_PAGESIZE);
+    if (pagesize < 1) {
+        logger->error("sysconf: _SC_PAGESIZE = {} < 1: {:s}", pagesize, strerror(errno));
+        return -1;
+    }
+    return pagesize;
+}
+
+#else // neither _WIN32, nor getpagesize, nor sysconf
+
+int native_pagesize(const gr::logger_ptr logger)
+{
+    logger->warning("No supported method of determining the pagesize available.");
+    return -1;
+}
+
 #endif
+
 
 int pagesize()
 {
     static int s_pagesize = -1;
-    gr::logger_ptr logger, debug_logger;
-    gr::configure_default_loggers(logger, debug_logger, "pagesize");
 
     if (s_pagesize == -1) {
-#if defined(HAVE_GETPAGESIZE)
-        s_pagesize = getpagesize();
-#elif defined(HAVE_SYSCONF)
-        s_pagesize = sysconf(_SC_PAGESIZE);
-        if (s_pagesize == -1) {
-            logger->error("_SC_PAGESIZE: {:s}", strerror(errno));
-            s_pagesize = 4096;
+        // only make a logger for the one time we actually intend to log anything.
+        gr::logger_ptr logger, debug_logger;
+        gr::configure_default_loggers(logger, debug_logger, "pagesize");
+        s_pagesize = native_pagesize(logger);
+        if (s_pagesize <= 0) {
+            constexpr int default_pagesize = 4096;
+            logger->error("Unable to determine page size. Using default of {} B instead.",
+                          default_pagesize);
+            s_pagesize = default_pagesize;
         }
-#else
-        logger->error("no info; setting pagesize = 4096");
-        s_pagesize = 4096;
-#endif
+        logger->info("Setting pagesize to {} B", s_pagesize);
     }
     return s_pagesize;
 }
