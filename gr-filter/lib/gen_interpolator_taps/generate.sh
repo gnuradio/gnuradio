78d89e1466864d305fbb8a5b25a7e1e94efc18c4
Marcus Müller <mmueller@gnuradio.org>
2023-02-08 18:41:13 +0100

    filter: make gen_interpolator_taps run on modern gfortran
    also, fix C to actually be standards-compliant
    
    also, a .gitignore for compilation results
    
    Signed-off-by: Marcus Müller <mmueller@gnuradio.org>

--- gr-filter/lib/gen_interpolator_taps/generate.sh
+++ gr-filter/lib/gen_interpolator_taps/generate.sh
@@ -6,25 +6,23 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
+set -e
+CC=gcc
+F=gfortran
+FFLAGS="$FFLAGS -std=legacy -Wall"
+CFLAGS="$CFLAGS -std=c99 -Wall -Wpedantic"
+$CC $CFLAGS -c -o diff_objective_fct.o diff_objective_fct.c
+$F $FFLAGS -c -o praxis.o praxis.f
+$CC $CFLAGS -c -o gen_interp_differentiator_taps.o gen_interp_differentiator_taps.c
 
-gcc      -c -o diff_objective_fct.o             diff_objective_fct.c
-gfortran -c -o praxis.o                         praxis.f
-gcc      -c -o gen_interp_differentiator_taps.o gen_interp_differentiator_taps.c
+$F $FFLAGS -o gen_interp_differentiator_taps gen_interp_differentiator_taps.o praxis.o diff_objective_fct.o -lgsl -lgslcblas
 
-gfortran -o gen_interp_differentiator_taps \
-        gen_interp_differentiator_taps.o praxis.o diff_objective_fct.o \
-        -lgsl -lgslcblas
+./gen_interp_differentiator_taps -n 128 -t 8 -B 0.25 > interp_differentiator_taps.h
 
-./gen_interp_differentiator_taps -n 128 -t 8 -B 0.25 \
-        > interp_differentiator_taps.h
+$CC $CFLAGS -c -o objective_fct.o objective_fct.c
+$CC $CFLAGS -c -o gen_interpolator_taps.o gen_interpolator_taps.c
 
-gcc      -c -o objective_fct.o         objective_fct.c
-gcc      -c -o gen_interpolator_taps.o gen_interpolator_taps.c
+$F $FFLAGS -o gen_interpolator_taps gen_interpolator_taps.o praxis.o objective_fct.o -lgsl -lgslcblas
 
-gfortran -o gen_interpolator_taps \
-        gen_interpolator_taps.o praxis.o objective_fct.o \
-        -lgsl -lgslcblas
-
-./gen_interpolator_taps -n 128 -t 8 -B 0.25 \
-        > interpolator_taps.h
+./gen_interpolator_taps -n 128 -t 8 -B 0.25 > interpolator_taps.h
 
