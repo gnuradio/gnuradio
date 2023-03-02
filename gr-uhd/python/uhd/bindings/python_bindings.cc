7ebaa6ab22c8cf675a764f152fd173716b0c096f
Grant Meyerhoff <grant.meyerhoff@ni.com>
2023-03-02 11:14:52 -0600

    uhd: rfnoc: Add GRC binding and support for replay block
    Signed-off-by: Grant Meyerhoff <grant.meyerhoff@ni.com>

--- gr-uhd/python/uhd/bindings/python_bindings.cc
+++ gr-uhd/python/uhd/bindings/python_bindings.cc
@@ -33,6 +33,7 @@ void bind_rfnoc_siggen(py::module&);
 void bind_rfnoc_window(py::module&);
 void bind_rfnoc_fir_filter(py::module&);
 void bind_rfnoc_graph(py::module&);
+void bind_rfnoc_replay(py::module&);
 void bind_rfnoc_rx_radio(py::module&);
 void bind_rfnoc_rx_streamer(py::module&);
 void bind_rfnoc_tx_radio(py::module&);
@@ -72,6 +73,7 @@ PYBIND11_MODULE(uhd_python, m)
     bind_rfnoc_window(m);
     bind_rfnoc_fir_filter(m);
     bind_rfnoc_graph(m);
+    bind_rfnoc_replay(m);
     bind_rfnoc_rx_radio(m);
     bind_rfnoc_rx_streamer(m);
     bind_rfnoc_tx_radio(m);
