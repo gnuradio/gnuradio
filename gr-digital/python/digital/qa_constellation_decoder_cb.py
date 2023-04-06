ad9e4c02de9db4d07772588629f08508f3207cb7
Cyrille Morin <cyrille.morin@inria.fr>
2023-04-06 18:11:10 +0200

    constellation encoder/decoder: Add const callback
    Adds setter and GRC callback in the constellation_encoder,
    constellation_decoder, and constellation_soft_decoder blocks
    to change the constellation object at runtime.
    
    Signed-off-by: Cyrille Morin <cyrille.morin@inria.fr>

--- gr-digital/python/digital/qa_constellation_decoder_cb.py
+++ gr-digital/python/digital/qa_constellation_decoder_cb.py
@@ -59,5 +59,35 @@ class test_constellation_decoder(gr_unittest.TestCase):
         self.assertFloatTuplesAlmostEqual(expected_result, actual_result)
 
 
+    def test_constellation_decoder_cb_bpsk_to_qpsk(self):
+        cnst = digital.constellation_bpsk()
+        src_data = (0.5 + 0.5j, 0.1 - 1.2j, -0.8 - 0.1j, -0.45 + 0.8j,
+                    0.8 + 1.0j, -0.5 + 0.1j, 0.1 - 1.2j)
+        src = blocks.vector_source_c(src_data)
+        op = digital.constellation_decoder_cb(cnst.base())
+        dst = blocks.vector_sink_b()
+
+        self.tb.connect(src, op)
+        self.tb.connect(op, dst)
+        self.tb.run()               # run the graph and wait for it to finish
+
+        cnst = digital.constellation_qpsk()
+        src_data = (0.5 + 0.5j, 0.1 - 1.2j, -0.8 - 0.1j, -0.45 + 0.8j,
+                    0.8 + 1.0j, -0.5 + 0.1j, 0.1 - 1.2j)
+        expected_result = (1, 1, 0, 0, # BPSK section
+                           1, 0, 1,
+                           3, 1, 0, 2, # QPSK section
+                           3, 2, 1)
+        src.set_data(src_data)
+        op.set_constellation(cnst.base())
+
+        self.tb.run()               # run the graph and wait for it to finish
+
+        actual_result = dst.data()  # fetch the contents of the sink
+        # print("actual result", actual_result)
+        # print("expected result", expected_result)
+        self.assertFloatTuplesAlmostEqual(expected_result, actual_result)
+
+
 if __name__ == '__main__':
     gr_unittest.run(test_constellation_decoder)
