a901c5592174a64cd35fb06a1c7391808702f2b8
Clayton Smith <argilo@gmail.com>
2023-04-12 12:45:10 -0400

    filter: Remove duplicate code
    Signed-off-by: Clayton Smith <argilo@gmail.com>
    (cherry picked from commit 265e886d59d73fd7805c4bb363e0fc33ae63065e)
    Signed-off-by: Jeff Long <willcode4@gmail.com>

--- gr-filter/python/filter/design/filter_design.py
+++ gr-filter/python/filter/design/filter_design.py
@@ -15,6 +15,7 @@ import warnings
 from optparse import OptionParser
 
 from gnuradio import filter, fft
+from .CustomViewBox import CustomViewBox
 
 try:
     import numpy as np
@@ -2334,23 +2335,6 @@ class gr_plot_filter(QtWidgets.QMainWindow):
             self.update_imp_curves()
 
         self.gui.nTapsEdit.setText(str(self.taps.size))
-
-
-class CustomViewBox(pg.ViewBox):
-    def __init__(self, *args, **kwds):
-        pg.ViewBox.__init__(self, *args, **kwds)
-        self.setMouseMode(self.RectMode)
-
-    # Reimplement right-click to zoom out.
-    def mouseClickEvent(self, ev):
-        if ev.button() == QtCore.Qt.RightButton:
-            self.autoRange()
-
-    def mouseDragEvent(self, ev):
-        if ev.button() == QtCore.Qt.RightButton:
-            ev.ignore()
-        else:
-            pg.ViewBox.mouseDragEvent(self, ev)
 
 
 def setup_options():
