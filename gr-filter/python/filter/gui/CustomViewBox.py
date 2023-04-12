14326dbb7fad3e58e265730db7e3a72b3b648997
Clayton Smith <argilo@gmail.com>
2023-04-12 12:46:16 -0400

    filter: Fix exception when dragging plot axes in Filter Design Tool
    Signed-off-by: Clayton Smith <argilo@gmail.com>
    (cherry picked from commit e4a769bc45b16c580ab8ee98927229b1a7a32a65)
    Signed-off-by: Jeff Long <willcode4@gmail.com>

--- gr-filter/python/filter/gui/CustomViewBox.py
+++ gr-filter/python/filter/gui/CustomViewBox.py
@@ -4,6 +4,7 @@ from PyQt5 import QtCore
 
 class CustomViewBox(pg.ViewBox):
     def __init__(self, *args, **kwds):
+        kwds['enableMenu'] = False
         pg.ViewBox.__init__(self, *args, **kwds)
         self.setMouseMode(self.RectMode)
 
@@ -11,9 +12,3 @@ class CustomViewBox(pg.ViewBox):
     def mouseClickEvent(self, ev):
         if ev.button() == QtCore.Qt.RightButton:
             self.autoRange()
-
-    def mouseDragEvent(self, ev):
-        if ev.button() == QtCore.Qt.RightButton:
-            ev.ignore()
-        else:
-            pg.ViewBox.mouseDragEvent(self, ev)
