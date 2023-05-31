c3d3c4b39d4e5be8ace6bdc1ef2d18ebb7b84ff6
Marcus Müller <mmueller@gnuradio.org>
2023-05-31 09:06:46 +0200

    runtime: thread_group use set instead of list for set of threads
    Signed-off-by: Marcus Müller <mmueller@gnuradio.org>
    (cherry picked from commit c2fc93fdc9448299e9d1004a8744347e020ed9bb)
    Signed-off-by: Jeff Long <willcode4@gmail.com>

--- gnuradio-runtime/include/gnuradio/thread/thread_group.h
+++ gnuradio-runtime/include/gnuradio/thread/thread_group.h
@@ -3,6 +3,7 @@
  * Copyright (C) 2001-2003 William E. Kempf
  * Copyright (C) 2007 Anthony Williams
  * Copyright 2008,2009 Free Software Foundation, Inc.
+ * Copyright 2023 Marcus Müller
  *
  *  Distributed under the Boost Software License, Version 1.0. (See accompanying
  *  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -17,7 +18,9 @@
 
 #include <gnuradio/api.h>
 #include <gnuradio/thread/thread.h>
+
 #include <shared_mutex>
+#include <unordered_set>
 #include <functional>
 #include <memory>
 
@@ -27,20 +30,20 @@ namespace thread {
 class GR_RUNTIME_API thread_group
 {
 public:
-    thread_group();
-    ~thread_group();
+    thread_group() = default;
+    ~thread_group() = default;
     thread_group(const thread_group&) = delete;
     thread_group& operator=(const thread_group&) = delete;
 
     boost::thread* create_thread(const std::function<void()>& threadfunc);
-    void add_thread(std::unique_ptr<boost::thread> thrd);
-    void remove_thread(boost::thread* thrd);
+    void add_thread(std::unique_ptr<boost::thread> thrd) noexcept;
+    void remove_thread(boost::thread* thrd) noexcept;
     void join_all();
     void interrupt_all();
-    size_t size() const;
+    size_t size() const noexcept;
 
 private:
-    std::list<std::unique_ptr<boost::thread>> m_threads;
+    std::unordered_set<std::unique_ptr<boost::thread>> m_threads;
     mutable std::shared_mutex m_mutex;
 };
 
