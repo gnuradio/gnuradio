2a7df8ffc55a2a366e6a8f8dbd007616cbd0eb11
Rohit Bisht <rbtunes0@gmail.com>
2023-06-11 00:19:53 +0000

    QT GUI Sink : Added new Matrix Sink
    Signed-off-by: Rohit Bisht <rbtunes0@gmail.com>

---
+++ gr-qtgui/include/gnuradio/qtgui/matrix_sink.h
@@ -0,0 +1,87 @@
+/* -*- c++ -*- */
+/*
+ * Copyright 2023 Free Software Foundation, Inc.
+ *
+ * This file is part of GNU Radio
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#ifndef INCLUDED_QTGUI_MATRIX_SINK_H
+#define INCLUDED_QTGUI_MATRIX_SINK_H
+
+#include <gnuradio/qtgui/api.h>
+#include <gnuradio/sync_block.h>
+#include <qapplication.h>
+#include <QWidget>
+
+namespace gr {
+namespace qtgui {
+
+/*!
+ * \brief A graphical sink that displays a matrix.
+ * \ingroup qtgui
+ * \ingroup qtgui_blk
+ *
+ * \details
+ * This block displays a matrix as a 2D plot. The matrix is
+ * displayed as a contour plot, with the color of each contour
+ * determined by the value of the matrix at that point.
+ *
+ * The matrix is displayed as a 2D plot, with the x and y axes
+ * corresponding to the row and column indices of the matrix,
+ * respectively. The z axis corresponds to the value of the
+ * matrix at that point.
+ *
+ */
+class QTGUI_API matrix_sink : virtual public gr::sync_block
+{
+public:
+    // gr::qtgui::matrix_sink::sptr
+    typedef std::shared_ptr<matrix_sink> sptr;
+
+    /*!
+     * \brief Build a matrix sink block.
+     *
+     * \param name The name of the block.
+     * \param num_cols The number of columns in the matrix.
+     * \param vlen The vector length of the matrix.
+     * \param contour Whether or not to display the matrix as a contour plot.
+     * \param color_map The color map to use for the contour plot.
+     * \param interpolation The interpolation method to use for the contour plot.
+     * \param x_start The starting value of the x axis.
+     * \param x_end The ending value of the x axis.
+     * \param y_start The starting value of the y axis.
+     * \param y_end The ending value of the y axis.
+     * \param z_max The maximum value of the z axis.
+     * \param z_min The minimum value of the z axis.
+     * \param x_axis_label The label for the x axis.
+     * \param y_axis_label The label for the y axis.
+     * \param z_axis_label The label for the z axis.
+     * \param parent The parent QWidget.
+     */
+    static sptr make(const std::string& name,
+                     unsigned int num_cols,
+                     unsigned int vlen,
+                     bool contour,
+                     const std::string& color_map,
+                     const std::string& interpolation,
+                     QWidget* parent = nullptr);
+
+    virtual void exec_() = 0;
+    virtual QWidget* qwidget() = 0;
+    virtual void set_x_start(double x_start) = 0;
+    virtual void set_x_end(double x_end) = 0;
+    virtual void set_y_start(double y_start) = 0;
+    virtual void set_y_end(double y_end) = 0;
+    virtual void set_z_max(double z_max) = 0;
+    virtual void set_z_min(double z_min) = 0;
+    virtual void set_x_axis_label(const std::string& x_axis_label) = 0;
+    virtual void set_y_axis_label(const std::string& y_axis_label) = 0;
+    virtual void set_z_axis_label(const std::string& z_axis_label) = 0;
+};
+
+} // namespace qtgui
+} // namespace gr
+
+#endif /* INCLUDED_QTGUI_MATRIX_SINK_H */
---
+++ gr-qtgui/lib/matrix_display.h
@@ -0,0 +1,118 @@
+/* -*- c++ -*- */
+/*
+ * Copyright 2023 Free Software Foundation, Inc.
+ *
+ * This file is part of GNU Radio
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#ifndef MATRIX_DISPLAY_H
+#define MATRIX_DISPLAY_H
+
+#include <gnuradio/qtgui/form_menus.h>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QScrollArea>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+#include <qwt_color_map.h>
+#include <qwt_interval.h>
+#include <qwt_matrix_raster_data.h>
+#include <qwt_plot.h>
+#include <qwt_plot_spectrogram.h>
+#include <qwt_scale_widget.h>
+#include <QAction>
+#include <QApplication>
+#include <QFileDialog>
+#include <QMenu>
+#include <QMouseEvent>
+#include <QPixmap>
+#include <QVector>
+#include <vector>
+
+/*!
+ * \brief Matrix Display child for managing sink plot.
+ * \ingroup qtgui_blk
+ */
+class matrix_display : public QWidget
+{
+    Q_OBJECT
+
+    QwtPlotSpectrogram* d_spectrogram;
+    QwtPlot* d_plot;
+    QwtLinearColorMap* d_colorMap;
+    QwtMatrixRasterData d_data;
+    QwtScaleWidget* d_x_axis;
+    QwtScaleWidget* d_y_axis;
+    QwtScaleWidget* d_z_axis;
+    QwtLinearColorMap* d_z_colorMap;
+
+    QHBoxLayout* d_hLayout;
+
+
+    unsigned int d_vlen;
+    unsigned int d_num_cols;
+    double d_x_start;
+    double d_x_end;
+    double d_y_start;
+    double d_y_end;
+    double d_z_max;
+    double d_z_min;
+    QList<double> d_contour_levels;
+    QMenu* d_menu = nullptr;
+    QAction* d_save_act = nullptr;
+    bool d_menu_on;
+    bool d_stop_state;
+    QAction* d_stop_act = nullptr;
+    QMenu* d_contour_menu = nullptr;
+    QMenu* d_interpolation_menu = nullptr;
+    QMenu* d_color_map_menu = nullptr;
+    bool d_isclosed = false;
+
+public:
+    explicit matrix_display(const std::string& name,
+                            unsigned int num_cols,
+                            unsigned int vlen,
+                            bool contour,
+                            const std::string& color_map,
+                            const std::string& interpolation,
+                            double x_start,
+                            double x_end,
+                            double y_start,
+                            double y_end,
+                            double z_max,
+                            double z_min,
+                            const std::string& x_axis_label,
+                            const std::string& y_axis_label,
+                            const std::string& z_axis_label,
+                            QWidget* parent = nullptr);
+    ~matrix_display();
+
+    void set_contour(bool contour);
+    void set_color_map(const std::string& color_map);
+    void set_interpolation(const std::string& interpolation);
+    void set_x_start(double x_start);
+    void set_x_end(double x_end);
+    void set_y_start(double y_start);
+    void set_y_end(double y_end);
+    void set_z_max(double z_max);
+    void set_z_min(double z_min);
+    void set_x_axis_label(const std::string& x_axis_label);
+    void set_y_axis_label(const std::string& y_axis_label);
+    void set_z_axis_label(const std::string& z_axis_label);
+    void initialize_mouse_actions();
+    bool isClosed() const;
+
+public slots:
+    void set_data(QVector<double> data);
+    void save_figure();
+    void mousePressEvent(QMouseEvent* e) override;
+    void set_stop(bool on);
+    void set_stop();
+    void contour(bool contour);
+    void interpolation(const std::string& interpolation);
+    void closeEvent(QCloseEvent* e) override;
+};
+#endif // MATRIX_DISPLAY_H
