9ce546c1cbc0f7bb73232321b5040dfd38a036da
Clayton Smith <argilo@gmail.com>
2023-08-15 15:10:17 -0400

    Set the frequency sink's output multiple on start
    This improves responsiveness, because it prevents the default output
    multiple of 32768 samples from being used. (This large output multiple
    is presumably set in the constructor to ensure that the shceduler
    allocates large enough buffers to accommodate runtime FFT size changes.
    
    Signed-off-by: Clayton Smith <argilo@gmail.com>
    (cherry picked from commit 2f5891bfb77138aa96de74421ec49e22ec8fee38)
    Signed-off-by: Jeff Long <willcode4@gmail.com>

--- gr-qtgui/lib/freq_sink_c_impl.h
+++ gr-qtgui/lib/freq_sink_c_impl.h
@@ -166,6 +166,7 @@ public:
     int work(int noutput_items,
              gr_vector_const_void_star& input_items,
              gr_vector_void_star& output_items) override;
+    bool start() override;
 };
 
 } /* namespace qtgui */
