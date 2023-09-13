cb5ce0add410b6cad239f9cd23675dc2010c1820
Daniel Estévez <daniel@destevez.net>
2023-09-13 11:11:27 +0200

    blocks: fix repeat block tag propagation
    The repeat block was refactored as a general block in #6813
    (it was a sync_interpolator before). However, tag propagation was
    missing from the refactor. Manual tag propagation needs to be done
    if the block is a general block, because the automatic tag propagation
    preserves the offset of tags, which is not what we want for the
    repeat block.
    
    Incidentally, before the refactor tag propagation was also somehow
    broken for interpolation changes at runtime. After an interpolation change,
    tags would disappear in the output for some time, or appear in the wrong
    place.
    
    The tag propagation behaviour introduced by this block is the most
    natural one. Tags from each input item are copied to the first
    repetition of such input item in the output.
    
    Additionally, two QA tests for tag propagation have been added. One
    of them does no interpolation changes at runtime, and the other
    does one interpolation change at runtime. The two tests do not pass
    with the previous version of the repeat block, but they pass after
    adding this fix.
    
    Signed-off-by: Daniel Estévez <daniel@destevez.net>

--- gr-blocks/lib/repeat_impl.h
+++ gr-blocks/lib/repeat_impl.h
@@ -1,6 +1,7 @@
 /* -*- c++ -*- */
 /*
  * Copyright 2012 Free Software Foundation, Inc.
+ * Copyright 2023 Daniel Estevez <daniel@destevez.net>
  *
  * This file is part of GNU Radio
  *
@@ -12,6 +13,7 @@
 #define INCLUDED_REPEAT_IMPL_H
 
 #include <gnuradio/blocks/repeat.h>
+#include <cstdint>
 
 namespace gr {
 namespace blocks {
@@ -37,6 +39,19 @@ private:
 
     void msg_set_interpolation(const pmt::pmt_t& msg);
     const pmt::pmt_t c_msg_port;
+
+    //! \brief helper function to propagate tags manually
+    void copy_tags(int consumed, int produced, int num_items)
+    {
+        std::vector<tag_t> tags;
+        const auto start_in = nitems_read(0) + static_cast<uint64_t>(consumed);
+        const auto start_out = nitems_written(0) + static_cast<uint64_t>(produced);
+        get_tags_in_range(tags, 0, start_in, start_in + num_items);
+        for (const auto& tag : tags) {
+            const auto offset = d_interp * (tag.offset - start_in) + start_out;
+            add_item_tag(0, offset, tag.key, tag.value);
+        }
+    }
 };
 
 } /* namespace blocks */
