11118f0e6965650321d33d2dfbd61f6529fb865c
Martin Braun <martin.braun@ettus.com>
2023-09-25 11:20:06 +0200

    uhd: rfnoc: rx radio: Add issue_stream_cmd() API
    This allows using the C++ or Pyton APIs to directly call
    issue_stream_cmd().
    
    Signed-off-by: Martin Braun <martin.braun@ettus.com>
    (cherry picked from commit 1bf2a3aaccc8fc2fb3e5045245c0e087657e6cb0)
    Signed-off-by: Jeff Long <willcode4@gmail.com>

--- gr-uhd/lib/rfnoc_rx_radio_impl.h
+++ gr-uhd/lib/rfnoc_rx_radio_impl.h
@@ -45,6 +45,7 @@ public:
     void set_iq_balance(const bool enable, const size_t chan) override;
     void set_iq_balance(const std::complex<double>& correction,
                         const size_t chan) override;
+    void issue_stream_cmd(const ::uhd::stream_cmd_t& cmd, const size_t chan) override;
     void enable_rx_timestamps(const bool enable, const size_t chan) override;
 
 private:
