3e7cf48a6f9a5801a87a4b11305b6448fc98a2bd
Martin Braun <martin.braun@ettus.com>
2023-09-26 15:42:54 +0200

    uhd: rfnoc: rx radio: Add 'stream_cmd' message handler
    This allows sending stream commands to the RFNoC RX Radio block. Note
    that unlike the usrp_source, the RFNoC RX Radio block does not
    automatically initiate a stream, so this message port adds another
    option (besides calling issue_stream_cmd() directly) to start streaming
    in RFNoC-only flow graphs.
    
    Includes changes to the manual.
    
    Signed-off-by: Martin Braun <martin.braun@ettus.com>
    (cherry picked from commit 70e3981878643b6f443d14519b103adcfc816e1e)
    Signed-off-by: Jeff Long <willcode4@gmail.com>

--- gr-uhd/lib/rfnoc_rx_radio_impl.cc
+++ gr-uhd/lib/rfnoc_rx_radio_impl.cc
@@ -12,6 +12,7 @@
 
 #include "rfnoc_rx_radio_impl.h"
 #include <gnuradio/io_signature.h>
+#include <gnuradio/uhd/cmd_keys.h>
 
 namespace gr {
 namespace uhd {
@@ -33,6 +34,14 @@ rfnoc_rx_radio::sptr rfnoc_rx_radio::make(rfnoc_graph::sptr graph,
 rfnoc_rx_radio_impl::rfnoc_rx_radio_impl(::uhd::rfnoc::noc_block_base::sptr block_ref)
     : rfnoc_block(block_ref), d_radio_ref(get_block_ref<::uhd::rfnoc::radio_control>())
 {
+#define REGISTER_CMD_HANDLER(key, _handler)                                   \
+    register_msg_cmd_handler(                                                 \
+        key, [this](const pmt::pmt_t& var, int chan, const pmt::pmt_t& msg) { \
+            this->_handler(var, chan, msg);                                   \
+        })
+    // Register default command handlers:
+    REGISTER_CMD_HANDLER(pmt::symbol_to_string(cmd_stream_cmd_key()),
+                         _cmd_handler_stream_cmd);
 }
 
 rfnoc_rx_radio_impl::~rfnoc_rx_radio_impl() {}
@@ -146,6 +155,70 @@ void rfnoc_rx_radio_impl::issue_stream_cmd(const ::uhd::stream_cmd_t& cmd,
 void rfnoc_rx_radio_impl::enable_rx_timestamps(const bool enable, const size_t chan)
 {
     return d_radio_ref->enable_rx_timestamps(enable, chan);
+}
+
+
+/******************************************************************************
+ * Command handlers
+ *****************************************************************************/
+void rfnoc_rx_radio_impl::_cmd_handler_stream_cmd(const pmt::pmt_t& cmd_p,
+                                                  int chan_i,
+                                                  const pmt::pmt_t& msg)
+{
+    ::uhd::stream_cmd_t cmd(::uhd::stream_cmd_t::STREAM_MODE_START_CONTINUOUS);
+    const std::string stream_mode_str = pmt::write_string(
+        pmt::dict_ref(cmd_p, pmt::mp("stream_mode"), pmt::mp("start_cont")));
+    if (stream_mode_str == "start_cont") {
+        // All set already
+    } else if (stream_mode_str == "stop_cont") {
+        cmd.stream_mode = ::uhd::stream_cmd_t::STREAM_MODE_STOP_CONTINUOUS;
+    } else if (stream_mode_str == "num_done") {
+        cmd.stream_mode = ::uhd::stream_cmd_t::STREAM_MODE_NUM_SAMPS_AND_DONE;
+    } else if (stream_mode_str == "num_more") {
+        cmd.stream_mode = ::uhd::stream_cmd_t::STREAM_MODE_NUM_SAMPS_AND_MORE;
+    } else {
+        d_logger->warn("Invalid stream command in command message: {}", stream_mode_str);
+        return;
+    }
+
+    cmd.stream_now =
+        pmt::to_bool(pmt::dict_ref(cmd_p, pmt::mp("stream_now"), pmt::mp(false)));
+    cmd.num_samps = pmt::to_long(pmt::dict_ref(cmd_p, pmt::mp("num_samps"), pmt::mp(0)));
+
+    if (pmt::dict_has_key(cmd_p, cmd_time_key())) {
+        pmt::pmt_t timespec_p = pmt::dict_ref(msg, cmd_time_key(), pmt::PMT_NIL);
+        if (timespec_p == pmt::PMT_NIL) {
+            cmd.time_spec = ::uhd::time_spec_t(0.0);
+        } else {
+            ::uhd::time_spec_t timespec(
+                time_t(pmt::to_uint64(pmt::car(timespec_p))), // Full secs
+                pmt::to_double(pmt::cdr(timespec_p))          // Frac secs
+            );
+            cmd.time_spec = timespec;
+        }
+    } else if (!cmd.stream_now) {
+        // If the user requests don't stream now, but doesn't provide a time
+        // stamp, we'll automatically add a timestamp a bit into the future.
+        // However, we can only read back the current time from the radio block
+        // starting with UHD 4.2. For older versions, we therefore fall back to
+        // stream-now, because stream-not-now without a timespec would guarantee
+        // a late command error.
+#if UHD_VERSION >= 4020000
+        static const double reasonable_delay = 0.1; // s
+        cmd.time_spec = d_radio_ref->get_time_now() + reasonable_delay;
+#else
+        d_logger->warn("Stream command set stream_now to false, but did not provide a "
+                       "time spec! Setting stream_now to true.");
+        cmd.stream_now = true;
+#endif
+    }
+
+    d_logger->debug("Received command message: issue stream command ({})",
+                    stream_mode_str);
+
+    const size_t chan = (chan_i == -1) ? ALL_CHANS : static_cast<size_t>(chan_i);
+
+    issue_stream_cmd(cmd, chan);
 }
 
 } /* namespace uhd */
