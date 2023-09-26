a69ba325c3394c3ec0b95913ee07a3c62038950d
Martin Braun <martin.braun@ettus.com>
2023-09-26 15:36:08 +0200

    uhd: rfnoc: Add command message port to RFNoC blocks
    This adds a 'command' message port which can be used by any RFNoC block.
    However, it does not add any actions. Individual blocks can register
    handlers for various types of messages.
    
    Signed-off-by: Martin Braun <martin.braun@ettus.com>

--- gr-uhd/lib/rfnoc_block.cc
+++ gr-uhd/lib/rfnoc_block.cc
@@ -10,10 +10,13 @@
 #include "config.h"
 #endif
 
+#include <gnuradio/pmt_fmt.h>
+#include <gnuradio/uhd/cmd_keys.h>
 #include <gnuradio/uhd/rfnoc_block.h>
 
 namespace gr {
 namespace uhd {
+
 
 /******************************************************************************
  * Factory and Structors
@@ -51,6 +54,76 @@ rfnoc_block::rfnoc_block(::uhd::rfnoc::noc_block_base::sptr block_ref)
           gr::io_signature::make(0, 0, 0)),
       d_block_ref(block_ref)
 {
+    // Set up message ports:
+    message_port_register_in(pmt::mp("command"));
+    set_msg_handler(pmt::mp("command"),
+                    [this](pmt::pmt_t msg) { this->_msg_handler_command(msg); });
+}
+
+
+/******************************************************************************
+ * Command Message Interface
+ *****************************************************************************/
+void rfnoc_block::_msg_handler_command(pmt::pmt_t msg)
+{
+    // pmt_dict is a subclass of pmt_pair. Make sure we use pmt_pair!
+    if (!pmt::is_dict(msg) && pmt::is_pair(msg)) {
+        d_logger->debug(
+            "Command message is pair, converting to dict: '{}': car({}), cdr({})",
+            msg,
+            pmt::car(msg),
+            pmt::cdr(msg));
+        msg = pmt::dict_add(pmt::make_dict(), pmt::car(msg), pmt::cdr(msg));
+    }
+
+    // Make sure, we use dicts!
+    if (!pmt::is_dict(msg)) {
+        d_logger->error("Command message is neither dict nor pair: {}",
+                        pmt::write_string(msg));
+        return;
+    }
+
+    /*** Start the actual message processing *************************/
+    /// 1) Read chan value
+    int chan = int(pmt::to_long(pmt::dict_ref(msg,
+                                              cmd_chan_key(),
+                                              pmt::from_long(-1) // Default to all chans
+                                              )));
+
+    /// 2) Loop through all the values
+    d_debug_logger->debug("Processing command message {}", pmt::write_string(msg));
+    pmt::pmt_t msg_items = pmt::dict_items(msg);
+    for (size_t i = 0; i < pmt::length(msg_items); i++) {
+        try {
+            _dispatch_msg_cmd_handler(pmt::write_string(pmt::car(pmt::nth(i, msg_items))),
+                                      pmt::cdr(pmt::nth(i, msg_items)),
+                                      chan,
+                                      msg);
+        } catch (const pmt::wrong_type& e) {
+            d_logger->alert("Invalid command value for key {}: {}",
+                            pmt::car(pmt::nth(i, msg_items)),
+                            pmt::cdr(pmt::nth(i, msg_items)));
+            break;
+        }
+    }
+}
+
+void rfnoc_block::_dispatch_msg_cmd_handler(const std::string& cmd,
+                                            const pmt::pmt_t& val,
+                                            int chan,
+                                            pmt::pmt_t& msg)
+{
+    if (_msg_cmd_handlers.count(cmd)) {
+        _msg_cmd_handlers[cmd](val, chan, msg);
+    } else if (cmd != "chan" || cmd != "time") {
+        // We don't warn for keys that are usually additional arguments
+        d_logger->warn("No command handler registered for command: `{}'", cmd);
+    }
+}
+
+void rfnoc_block::register_msg_cmd_handler(const std::string& cmd, cmd_handler_t handler)
+{
+    _msg_cmd_handlers[cmd] = handler;
 }
 
 /******************************************************************************
