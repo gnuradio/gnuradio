e86b851652c9aec745c5dd56400af4292a270ea7
Bailey Campbell <baileycampbell1990@gmail.com>
2023-10-03 17:39:49 -0400

    grc: Remove param field colorings by type and replace with labels
    Signed-off-by: Bailey Campbell <baileycampbell1990@gmail.com>

--- grc/gui/canvas/param.py
+++ grc/gui/canvas/param.py
@@ -28,22 +28,22 @@ class Param(CoreParam):
             gtk input class
         """
         dtype = self.dtype
-        if dtype in ('file_open', 'file_save'):
+        if dtype in ("file_open", "file_save"):
             input_widget_cls = ParamWidgets.FileParam
 
-        elif dtype == 'dir_select':
+        elif dtype == "dir_select":
             input_widget_cls = ParamWidgets.DirectoryParam
 
-        elif dtype == 'enum':
+        elif dtype == "enum":
             input_widget_cls = ParamWidgets.EnumParam
 
         elif self.options:
             input_widget_cls = ParamWidgets.EnumEntryParam
 
-        elif dtype == '_multiline':
+        elif dtype == "_multiline":
             input_widget_cls = ParamWidgets.MultiLineEntryParam
 
-        elif dtype == '_multiline_python_external':
+        elif dtype == "_multiline_python_external":
             input_widget_cls = ParamWidgets.PythonEditorParam
 
         else:
@@ -54,37 +54,45 @@ class Param(CoreParam):
     def format_label_markup(self, have_pending_changes=False):
         block = self.parent
         # fixme: using non-public attribute here
-        has_callback = \
-            hasattr(block, 'templates') and \
-            any(self.key in callback for callback in block.templates.get('callbacks', ''))
-
-        return '<span {underline} {foreground} font_desc="Sans 9">{label}</span>'.format(
-            underline='underline="low"' if has_callback else '',
-            foreground='foreground="blue"' if have_pending_changes else
-                       'foreground="red"' if not self.is_valid() else '',
-            label=Utils.encode(self.name)
+        has_callback = hasattr(block, "templates") and any(
+            self.key in callback for callback in block.templates.get("callbacks", "")
         )
+
+        return (
+            '<span {underline} {foreground} font_desc="Sans 9">{label}</span>'.format(
+                underline='underline="low"' if has_callback else "",
+                foreground='foreground="blue"'
+                if have_pending_changes
+                else 'foreground="red"'
+                if not self.is_valid()
+                else "",
+                label=Utils.encode(self.name),
+            )
+        )
+
+    def format_dtype_markup(self):
+        return f'<span font_desc="Sans 9">[{Utils.encode(self.dtype)}]</span>'
 
     def format_tooltip_text(self):
         errors = self.get_error_messages()
-        tooltip_lines = ['Key: ' + self.key, 'Type: ' + self.dtype]
+        tooltip_lines = ["Key: " + self.key, "Type: " + self.dtype]
         if self.is_valid():
             value = self.get_evaluated()
             if hasattr(value, "__len__"):
-                tooltip_lines.append('Length: {}'.format(len(value)))
+                tooltip_lines.append("Length: {}".format(len(value)))
             value = str(value)
             # ensure that value is a UTF-8 string
             # Old PMTs could produce non-UTF-8 strings
-            value = value.encode('utf-8', 'backslashreplace').decode('utf-8')
+            value = value.encode("utf-8", "backslashreplace").decode("utf-8")
             if len(value) > 100:
-                value = '{}...{}'.format(value[:50], value[-50:])
-            tooltip_lines.append('Value: ' + value)
+                value = "{}...{}".format(value[:50], value[-50:])
+            tooltip_lines.append("Value: " + value)
         elif len(errors) == 1:
-            tooltip_lines.append('Error: ' + errors[0])
+            tooltip_lines.append("Error: " + errors[0])
         elif len(errors) > 1:
-            tooltip_lines.append('Error:')
-            tooltip_lines.extend(' * ' + msg for msg in errors)
-        return '\n'.join(tooltip_lines)
+            tooltip_lines.append("Error:")
+            tooltip_lines.extend(" * " + msg for msg in errors)
+        return "\n".join(tooltip_lines)
 
     ##################################################
     # Truncate helper method
@@ -94,12 +102,11 @@ class Param(CoreParam):
         max_len = max(27 - len(self.name), 3)
         if len(string) > max_len:
             if style < 0:  # Front truncate
-                string = '...' + string[3 - max_len:]
+                string = "..." + string[3 - max_len :]
             elif style == 0:  # Center truncate
-                string = string[:max_len // 2 - 3] + \
-                    '...' + string[-max_len // 2:]
+                string = string[: max_len // 2 - 3] + "..." + string[-max_len // 2 :]
             elif style > 0:  # Rear truncate
-                string = string[:max_len - 3] + '...'
+                string = string[: max_len - 3] + "..."
         return string
 
     def pretty_print(self):
@@ -138,8 +145,8 @@ class Param(CoreParam):
                 truncate = 1
             else:
                 # Small vectors use eval
-                dt_str = ', '.join(map(Utils.num_to_str, e))
-        elif t in ('file_open', 'file_save'):
+                dt_str = ", ".join(map(Utils.num_to_str, e))
+        elif t in ("file_open", "file_save"):
             dt_str = self.get_value()
             truncate = -1
         else:
@@ -147,7 +154,7 @@ class Param(CoreParam):
             dt_str = str(e)
             # ensure that value is a UTF-8 string
             # Old PMTs could produce non-UTF-8 strings
-            dt_str = dt_str.encode('utf-8', 'backslashreplace').decode('utf-8')
+            dt_str = dt_str.encode("utf-8", "backslashreplace").decode("utf-8")
 
         # Done
         return self.truncate(dt_str, truncate)
@@ -169,13 +176,11 @@ class Param(CoreParam):
 
         # Include the value defined by the user (after evaluation)
         if not is_evaluated or show_value or not show_expr:
-            display_value += Utils.encode(
-                self.pretty_print().replace('\n', ' '))
+            display_value += Utils.encode(self.pretty_print().replace("\n", " "))
 
         # Include the expression that was evaluated to get the value
         if is_evaluated and show_expr:
-            expr_string = "<i>" + \
-                Utils.encode(self.truncate(self.value)) + "</i>"
+            expr_string = "<i>" + Utils.encode(self.truncate(self.value)) + "</i>"
 
             if display_value:  # We are already displaying the value
                 display_value = expr_string + "=" + display_value
@@ -183,5 +188,8 @@ class Param(CoreParam):
                 display_value = expr_string
 
         return '<span {foreground} font_desc="{font}"><b>{label}:</b> {value}</span>'.format(
-            foreground='foreground="red"' if not self.is_valid() else '', font=Constants.PARAM_FONT,
-            label=Utils.encode(self.name), value=display_value)
+            foreground='foreground="red"' if not self.is_valid() else "",
+            font=Constants.PARAM_FONT,
+            label=Utils.encode(self.name),
+            value=display_value,
+        )
