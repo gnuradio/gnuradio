14d8bcde365f8025cd85c1d0bfad0e575b86e312
Bailey Campbell <baileycampbell1990@gmail.com>
2023-10-03 19:57:47 -0400

    grc: Fix formatting
    Signed-off-by: Bailey Campbell <baileycampbell1990@gmail.com>

--- grc/gui/ParamWidgets.py
+++ grc/gui/ParamWidgets.py
@@ -25,29 +25,22 @@ def have_dark_theme():
         """
         Check if a theme is dark based on its name.
         """
-        return theme_name and (
-            theme_name in Constants.GTK_DARK_THEMES or "dark" in theme_name.lower()
-        )
+        return theme_name and (theme_name in Constants.GTK_DARK_THEMES or "dark" in theme_name.lower())
 
     # GoGoGo
     config = configparser.ConfigParser()
     config.read(os.path.expanduser(Constants.GTK_SETTINGS_INI_PATH))
     prefer_dark = config.get(
-        "Settings", Constants.GTK_INI_PREFER_DARK_KEY, fallback=None
-    )
-    theme_name = config.get("Settings", Constants.GTK_INI_THEME_NAME_KEY, fallback=None)
-    if prefer_dark in ("1", "yes", "true", "on") or is_dark_theme(theme_name):
+        'Settings', Constants.GTK_INI_PREFER_DARK_KEY, fallback=None)
+    theme_name = config.get(
+        'Settings', Constants.GTK_INI_THEME_NAME_KEY, fallback=None)
+    if prefer_dark in ('1', 'yes', 'true', 'on') or is_dark_theme(theme_name):
         return True
     try:
-        theme = (
-            subprocess.check_output(
-                ["gsettings", "get", "org.gnome.desktop.interface", "gtk-theme"],
-                stderr=subprocess.DEVNULL,
-            )
-            .decode("utf-8")
-            .strip()
-            .replace("'", "")
-        )
+        theme = subprocess.check_output(
+            ["gsettings", "get", "org.gnome.desktop.interface", "gtk-theme"],
+            stderr=subprocess.DEVNULL
+        ).decode('utf-8').strip().replace("'", "")
     except:
         return False
     return is_dark_theme(theme)
@@ -60,12 +53,11 @@ def add_style_provider():
     style_provider = Gtk.CssProvider()
     dark_theme = have_dark_theme()
     style_provider.load_from_data(
-        DARK_THEME_STYLES if dark_theme else LIGHT_THEME_STYLES
-    )
+        DARK_THEME_STYLES if dark_theme else LIGHT_THEME_STYLES)
     Gtk.StyleContext.add_provider_for_screen(
         Gdk.Screen.get_default(),
         style_provider,
-        Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION,
+        Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
     )
 
 
@@ -77,9 +69,7 @@ class InputParam(Gtk.HBox):
 
     expand = False
 
-    def __init__(
-        self, param, changed_callback=None, editing_callback=None, transient_for=None
-    ):
+    def __init__(self, param, changed_callback=None, editing_callback=None, transient_for=None):
         Gtk.HBox.__init__(self)
 
         self.param = param
@@ -103,7 +93,7 @@ class InputParam(Gtk.HBox):
         self.tp = None
         self._have_pending_changes = False
 
-        self.connect("show", self._update_gui)
+        self.connect('show', self._update_gui)
 
     def set_tooltip_text(self, text):
         pass
@@ -116,14 +106,13 @@ class InputParam(Gtk.HBox):
         Set the markup, color, tooltip, show/hide.
         """
         self.label.set_markup(
-            self.param.format_label_markup(self._have_pending_changes)
-        )
+            self.param.format_label_markup(self._have_pending_changes))
         if self.dtype_label is not None:
             self.dtype_label.set_markup(self.param.format_dtype_markup())
 
         self.set_tooltip_text(self.param.format_tooltip_text())
 
-        if self.param.hide == "all":
+        if self.param.hide == 'all':
             self.hide()
         else:
             self.show_all()
@@ -154,10 +143,7 @@ class InputParam(Gtk.HBox):
         self._update_gui()
 
     def _handle_key_press(self, widget, event):
-        if (
-            event.keyval == Gdk.KEY_Return
-            and event.get_state() & Gdk.ModifierType.CONTROL_MASK
-        ):
+        if event.keyval == Gdk.KEY_Return and event.get_state() & Gdk.ModifierType.CONTROL_MASK:
             self._apply_change(widget, event)
             return True
         return False
@@ -174,9 +160,9 @@ class EntryParam(InputParam):
         InputParam.__init__(self, *args, **kwargs)
         self._input = Gtk.Entry()
         self._input.set_text(self.param.get_value())
-        self._input.connect("changed", self._mark_changed)
-        self._input.connect("focus-out-event", self._apply_change)
-        self._input.connect("key-press-event", self._handle_key_press)
+        self._input.connect('changed', self._mark_changed)
+        self._input.connect('focus-out-event', self._apply_change)
+        self._input.connect('key-press-event', self._handle_key_press)
         self.pack_start(self._input, True, True, 0)
 
     def get_text(self):
@@ -195,12 +181,12 @@ class MultiLineEntryParam(InputParam):
         InputParam.__init__(self, *args, **kwargs)
         self._buffer = Gtk.TextBuffer()
         self._buffer.set_text(self.param.get_value())
-        self._buffer.connect("changed", self._mark_changed)
+        self._buffer.connect('changed', self._mark_changed)
 
         self._view = Gtk.TextView()
         self._view.set_buffer(self._buffer)
-        self._view.connect("focus-out-event", self._apply_change)
-        self._view.connect("key-press-event", self._handle_key_press)
+        self._view.connect('focus-out-event', self._apply_change)
+        self._view.connect('key-press-event', self._handle_key_press)
 
         self._sw = Gtk.ScrolledWindow()
         self._sw.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
@@ -211,9 +197,8 @@ class MultiLineEntryParam(InputParam):
 
     def get_text(self):
         buf = self._buffer
-        text = buf.get_text(
-            buf.get_start_iter(), buf.get_end_iter(), include_hidden_chars=False
-        )
+        text = buf.get_text(buf.get_start_iter(), buf.get_end_iter(),
+                            include_hidden_chars=False)
         return text.strip()
 
     def set_tooltip_text(self, text):
@@ -223,17 +208,16 @@ class MultiLineEntryParam(InputParam):
 class PythonEditorParam(InputParam):
     def __init__(self, *args, **kwargs):
         InputParam.__init__(self, *args, **kwargs)
-        open_button = self._open_button = Gtk.Button(label="Open in Editor")
-        open_button.connect("clicked", self.open_editor)
+        open_button = self._open_button = Gtk.Button(label='Open in Editor')
+        open_button.connect('clicked', self.open_editor)
         self.pack_start(open_button, True, True, True)
-        chooser_button = self._chooser_button = Gtk.Button(label="Choose Editor")
-        chooser_button.connect("clicked", self.open_chooser)
+        chooser_button = self._chooser_button = Gtk.Button(label='Choose Editor')
+        chooser_button.connect('clicked', self.open_chooser)
         self.pack_start(chooser_button, True, True, True)
 
     def open_editor(self, widget=None):
         self.param.parent_flowgraph.install_external_editor(
-            self.param, parent=self._transient_for
-        )
+            self.param, parent=self._transient_for)
 
     def open_chooser(self, widget=None):
         self.param.parent_flowgraph.remove_external_editor(param=self.param)
@@ -260,8 +244,8 @@ class EnumParam(InputParam):
 
         self.param_values = list(self.param.options)
         self._input.set_active(self.param_values.index(self.param.get_value()))
-        self._input.connect("changed", self._editing_callback)
-        self._input.connect("changed", self._apply_change)
+        self._input.connect('changed', self._editing_callback)
+        self._input.connect('changed', self._apply_change)
         self.pack_start(self._input, False, False, 0)
 
     def get_text(self):
@@ -288,10 +272,10 @@ class EnumEntryParam(InputParam):
             self._input.set_active(-1)
             self._input.get_child().set_text(value)
 
-        self._input.connect("changed", self._apply_change)
-        self._input.get_child().connect("changed", self._mark_changed)
-        self._input.get_child().connect("focus-out-event", self._apply_change)
-        self._input.get_child().connect("key-press-event", self._handle_key_press)
+        self._input.connect('changed', self._apply_change)
+        self._input.get_child().connect('changed', self._mark_changed)
+        self._input.get_child().connect('focus-out-event', self._apply_change)
+        self._input.get_child().connect('key-press-event', self._handle_key_press)
         self.pack_start(self._input, False, False, 0)
 
     @property
@@ -316,8 +300,8 @@ class FileParam(EntryParam):
 
     def __init__(self, *args, **kwargs):
         EntryParam.__init__(self, *args, **kwargs)
-        self._open_button = Gtk.Button(label="...")
-        self._open_button.connect("clicked", self._handle_clicked)
+        self._open_button = Gtk.Button(label='...')
+        self._open_button.connect('clicked', self._handle_clicked)
         self.pack_start(self._open_button, False, False, 0)
 
     def _handle_clicked(self, widget=None):
@@ -326,44 +310,39 @@ class FileParam(EntryParam):
         Replace the text in the entry with the new filename from the file dialog.
         """
         # get the paths
-        file_path = self.param.is_valid() and self.param.get_evaluated() or ""
-        (dirname, basename) = (
-            os.path.isfile(file_path) and os.path.split(file_path) or (file_path, "")
-        )
+        file_path = self.param.is_valid() and self.param.get_evaluated() or ''
+        (dirname, basename) = os.path.isfile(
+            file_path) and os.path.split(file_path) or (file_path, '')
         # check for qss theme default directory
-        if self.param.key == "qt_qss_theme":
+        if self.param.key == 'qt_qss_theme':
             dirname = os.path.dirname(dirname)  # trim filename
             if not os.path.exists(dirname):
                 config = self.param.parent_platform.config
-                dirname = os.path.join(config.install_prefix, "/share/gnuradio/themes")
+                dirname = os.path.join(
+                    config.install_prefix, '/share/gnuradio/themes')
         if not os.path.exists(dirname):
             dirname = os.getcwd()  # fix bad paths
 
         # build the dialog
-        if self.param.dtype == "file_open":
+        if self.param.dtype == 'file_open':
             file_dialog = Gtk.FileChooserDialog(
-                title="Open a Data File...",
-                action=Gtk.FileChooserAction.OPEN,
+                title='Open a Data File...', action=Gtk.FileChooserAction.OPEN,
                 transient_for=self._transient_for,
             )
             file_dialog.add_buttons(
-                "gtk-cancel", Gtk.ResponseType.CANCEL, "gtk-open", Gtk.ResponseType.OK
-            )
-        elif self.param.dtype == "file_save":
+                'gtk-cancel', Gtk.ResponseType.CANCEL, 'gtk-open', Gtk.ResponseType.OK)
+        elif self.param.dtype == 'file_save':
             file_dialog = Gtk.FileChooserDialog(
-                title="Save a Data File...",
-                action=Gtk.FileChooserAction.SAVE,
+                title='Save a Data File...', action=Gtk.FileChooserAction.SAVE,
                 transient_for=self._transient_for,
             )
             file_dialog.add_buttons(
-                "gtk-cancel", Gtk.ResponseType.CANCEL, "gtk-save", Gtk.ResponseType.OK
-            )
+                'gtk-cancel', Gtk.ResponseType.CANCEL, 'gtk-save', Gtk.ResponseType.OK)
             file_dialog.set_do_overwrite_confirmation(True)
             file_dialog.set_current_name(basename)  # show the current filename
         else:
             raise ValueError(
-                "Can't open file chooser dialog for type " + repr(self.param.dtype)
-            )
+                "Can't open file chooser dialog for type " + repr(self.param.dtype))
         file_dialog.set_current_folder(dirname)  # current directory
         file_dialog.set_select_multiple(False)
         file_dialog.set_local_only(True)
@@ -383,29 +362,24 @@ class DirectoryParam(FileParam):
         Open the directory selector, when the button is clicked.
         On success, update the entry.
         """
-        dirname = self.param.get_evaluated() if self.param.is_valid() else ""
+        dirname = self.param.get_evaluated() if self.param.is_valid() else ''
 
         # Check if directory exists, if not fall back to workdir
         if not os.path.isdir(dirname):
             dirname = os.getcwd()
 
-        if (
-            self.param.dtype == "dir_select"
-        ):  # Setup directory selection dialog, and fail for unexpected dtype
+        if self.param.dtype == "dir_select":  # Setup directory selection dialog, and fail for unexpected dtype
             dir_dialog = Gtk.FileChooserDialog(
-                title="Select a Directory...",
-                action=Gtk.FileChooserAction.SELECT_FOLDER,
-                transient_for=self._transient_for,
+                title='Select a Directory...', action=Gtk.FileChooserAction.SELECT_FOLDER,
+                transient_for=self._transient_for
             )
         else:
             raise ValueError(
-                "Can't open directory chooser dialog for type " + repr(self.param.dtype)
-            )
+                "Can't open directory chooser dialog for type " + repr(self.param.dtype))
 
         # Set dialog properties
         dir_dialog.add_buttons(
-            "gtk-cancel", Gtk.ResponseType.CANCEL, "gtk-open", Gtk.ResponseType.OK
-        )
+            'gtk-cancel', Gtk.ResponseType.CANCEL, 'gtk-open', Gtk.ResponseType.OK)
         dir_dialog.set_current_folder(dirname)
         dir_dialog.set_local_only(True)
         dir_dialog.set_select_multiple(False)
