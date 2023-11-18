89ba65a7c640dedab9a9b8ce01308d074ef0987d
Håkon Vågsether <hakon.vagsether@gmail.com>
2023-11-18 11:16:27 +0100

    grcqt: Don't import ProjectManager
    Signed-off-by: Håkon Vågsether <hakon.vagsether@gmail.com>

--- grc/gui_qt/components/__init__.py
+++ grc/gui_qt/components/__init__.py
@@ -5,7 +5,6 @@ from .documentation_tab import DocumentationTab
 from .wiki_tab import WikiTab
 from .console import Console
 from .flowgraph import FlowgraphView
-from .project_manager import ProjectManager
 from .undoable_actions import ChangeStateAction
 from .variable_editor import VariableEditor
 
