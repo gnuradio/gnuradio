c6e8f4d81e4af2b683a92eb7362a127ff64c62a6
Marcus Müller <mmueller@gnuradio.org>
2023-12-27 22:55:55 +0100

    network: move gr-pdu dependent test to own file
    And enable only conditionally
    
    Signed-off-by: Marcus Müller <mmueller@gnuradio.org>

--- gr-network/python/network/qa_socket_pdu.py
+++ gr-network/python/network/qa_socket_pdu.py
@@ -9,7 +9,7 @@
 #
 
 
-from gnuradio import gr, gr_unittest, blocks, pdu
+from gnuradio import gr, gr_unittest, blocks
 from gnuradio import network
 import random
 import pmt
@@ -25,7 +25,7 @@ class qa_socket_pdu (gr_unittest.TestCase):
     def tearDown(self):
         self.tb = None
 
-    def test_001(self):
+    def test_001_creation_destruction(self):
         # Test that blocks can be created and destroyed without hanging
         port = str(random.Random().randint(0, 30000) + 10000)
         self.pdu_send = network.socket_pdu("UDP_CLIENT", "localhost", port)
@@ -33,7 +33,7 @@ class qa_socket_pdu (gr_unittest.TestCase):
         self.pdu_send = None
         self.pdu_recv = None
 
-    def test_002(self):
+    def test_002_send_pdu_through_udp(self):
         # Send a PDU through a pair of UDP sockets
         port = str(random.Random().randint(0, 30000) + 10000)
         srcdata = (0x64, 0x6f, 0x67, 0x65)
@@ -63,52 +63,7 @@ class qa_socket_pdu (gr_unittest.TestCase):
             msg_data.append(pmt.u8vector_ref(received_data, i))
         self.assertEqual(srcdata, tuple(msg_data))
 
-    def test_003(self):
-        # Test that block stops when interacting with streaming interface
-        port = str(random.Random().randint(0, 30000) + 10000)
-        srcdata = (
-            0x73,
-            0x75,
-            0x63,
-            0x68,
-            0x74,
-            0x65,
-            0x73,
-            0x74,
-            0x76,
-            0x65,
-            0x72,
-            0x79,
-            0x70,
-            0x61,
-            0x73,
-            0x73)
-        tag_dict = {"offset": 0}
-        tag_dict["key"] = pmt.intern("len")
-        tag_dict["value"] = pmt.from_long(8)
-        tag1 = gr.python_to_tag(tag_dict)
-        tag_dict["offset"] = 8
-        tag2 = gr.python_to_tag(tag_dict)
-        tags = [tag1, tag2]
-
-        src = blocks.vector_source_b(srcdata, False, 1, tags)
-        ts_to_pdu = pdu.tagged_stream_to_pdu(gr.types.byte_t, "len")
-        pdu_send = network.socket_pdu("UDP_CLIENT", "localhost", "4141")
-        #pdu_recv = network.socket_pdu("UDP_SERVER", "localhost", port)
-        pdu_to_ts = pdu.pdu_to_tagged_stream(gr.types.byte_t, "len")
-        head = blocks.head(gr.sizeof_char, 10)
-        sink = blocks.vector_sink_b(1)
-
-        self.tb.connect(src, ts_to_pdu)
-        self.tb.msg_connect(ts_to_pdu, "pdus", pdu_send, "pdus")
-        # a UDP socket connects pdu_send to pdu_recv
-        # TODO: test that the recv socket can be destroyed from downstream
-        # that signals DONE. Also that we get the PDUs we sent
-        #self.tb.msg_connect(pdu_recv, "pdus", pdu_to_ts, "pdus")
-        #self.tb.connect(pdu_to_ts, head, sink)
-        self.tb.run()
-
-    def test_004(self):
+    def test_004_tcp_sub_mtu(self):
         # Test that the TCP server can stream PDUs <= the MTU size.
         port = str(random.Random().randint(0, 30000) + 10000)
         mtu = 10000
