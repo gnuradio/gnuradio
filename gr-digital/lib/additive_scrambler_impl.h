d9343906cb4df4a1d56b7419e5312a537318648c
Daniel Estévez <daniel@destevez.net>
2024-01-03 09:44:53 +0100

    digital: add soft symbol handling to additive scrambler
    In receivers it is typical to perform additive descrambling with
    soft symbols before passing these soft symbols to an FEC decoder.
    For instance, CCSDS protocols work like this.
    
    Currently, the Additive Scrambler block is only able to work with
    hard symbols, which makes using it to descramble soft symbols rather
    awkward (but not impossible, since it is possible to feed it zeros,
    take its output, convert from unipolar to bipolar and then mulitply
    this by the soft symbols). This commit adds to additive scrambler
    the capability to work directly with soft symbols.
    
    The additive_scrambler_bb class has been refactored as a template
    additive_scrambler, which is instantiated for gr_complex, float,
    int32_t, int16_t and uint8_t. The uint8_t instantiation corresponds
    to the behaviour of the current additive_scrambler_bb class and
    is intended to be backwards compatible with no API changes.
    
    The new instantiations all correspond to soft symbols (in different
    numeric formats). The LFSR output is used to decide whether to invert
    the sign of each soft symbol or not. For these soft symbols
    instantiations it is mandatory that bits_per_byte == 1. The constructor
    will throw an execption otherwise.
    
    A simple QA test for the float instantiation has been added.
    
    The old digital_additive_scrambler_bb GRC block has been marked as
    deprecated, and a new digital_additive_scrambler_xx GRC block with
    an option to select the IO Type (defaulting to byte) has been added
    as a replacement.
    
    Signed-off-by: Daniel Estévez <daniel@destevez.net>

--- gr-digital/lib/additive_scrambler_impl.h
+++ gr-digital/lib/additive_scrambler_impl.h
@@ -1,6 +1,7 @@
 /* -*- c++ -*- */
 /*
  * Copyright 2008,2010,2012 Free Software Foundation, Inc.
+ * Copyright 2024 Daniel Estevez <daniel@destevez.net>
  *
  * This file is part of GNU Radio
  *
@@ -8,18 +9,20 @@
  *
  */
 
-#ifndef INCLUDED_DIGITAL_ADDITIVE_SCRAMBLER_BB_IMPL_H
-#define INCLUDED_DIGITAL_ADDITIVE_SCRAMBLER_BB_IMPL_H
+#ifndef INCLUDED_DIGITAL_ADDITIVE_SCRAMBLER_IMPL_H
+#define INCLUDED_DIGITAL_ADDITIVE_SCRAMBLER_IMPL_H
 
-#include <gnuradio/digital/additive_scrambler_bb.h>
+#include <gnuradio/digital/additive_scrambler.h>
 #include <gnuradio/digital/lfsr.h>
 
 namespace gr {
 namespace digital {
 
-class additive_scrambler_bb_impl : public additive_scrambler_bb
+template <class T>
+class additive_scrambler_impl : public additive_scrambler<T>
 {
 private:
+    static constexpr bool is_hard_symbol = std::is_same<T, uint8_t>();
     digital::lfsr d_lfsr;
     int64_t d_count;  //!< Reset the LFSR after this many bytes (not bits)
     uint64_t d_bytes; //!< Count the processed bytes
@@ -31,13 +34,13 @@ private:
     int64_t _get_next_reset_index(int64_t noutput_items, int64_t last_reset_index = -1);
 
 public:
-    additive_scrambler_bb_impl(uint64_t mask,
-                               uint64_t seed,
-                               uint8_t len,
-                               int64_t count = 0,
-                               uint8_t bits_per_byte = 1,
-                               const std::string& reset_tag_key = "");
-    ~additive_scrambler_bb_impl() override;
+    additive_scrambler_impl(uint64_t mask,
+                            uint64_t seed,
+                            uint8_t len,
+                            int64_t count = 0,
+                            uint8_t bits_per_byte = 1,
+                            const std::string& reset_tag_key = "");
+    ~additive_scrambler_impl() override;
 
     uint64_t mask() const override;
     uint64_t seed() const override;
@@ -53,4 +56,4 @@ public:
 } /* namespace digital */
 } /* namespace gr */
 
-#endif /* INCLUDED_DIGITAL_ADDITIVE_SCRAMBLER_BB_IMPL_H */
+#endif /* INCLUDED_DIGITAL_ADDITIVE_SCRAMBLER_IMPL_H */
---
+++ gr-digital/python/digital/bindings/docstrings/additive_scrambler_pydoc_template.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2020 Free Software Foundation, Inc.
+ *
+ * This file is part of GNU Radio
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ */
+#include "pydoc_macros.h"
+#define D(...) DOC(gr, digital, __VA_ARGS__)
+/*
+  This file contains placeholders for docstrings for the Python bindings.
+  Do not edit! These were automatically extracted during the binding process
+  and will be overwritten during the build process
+ */
+
+
+static const char* __doc_gr_digital_additive_scrambler = R"doc()doc";
+
+
+static const char* __doc_gr_digital_additive_scrambler_additive_scrambler_0 = R"doc()doc";
+
+
+static const char* __doc_gr_digital_additive_scrambler_additive_scrambler_1 = R"doc()doc";
+
+
+static const char* __doc_gr_digital_additive_scrambler_make = R"doc()doc";
+
+
+static const char* __doc_gr_digital_additive_scrambler_mask = R"doc()doc";
+
+
+static const char* __doc_gr_digital_additive_scrambler_seed = R"doc()doc";
+
+
+static const char* __doc_gr_digital_additive_scrambler_len = R"doc()doc";
+
+
+static const char* __doc_gr_digital_additive_scrambler_count = R"doc()doc";
+
+
+static const char* __doc_gr_digital_additive_scrambler_bits_per_byte = R"doc()doc";
