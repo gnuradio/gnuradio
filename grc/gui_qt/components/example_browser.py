270f8c5fdca1379e53373c614e46a38fcd0a4e8c
Håkon Vågsether <hakon.vagsether@gmail.com>
2024-02-10 17:17:28 +0100

    grcqt: Use silent cache for example browser
    Signed-off-by: Håkon Vågsether <hakon.vagsether@gmail.com>

--- grc/gui_qt/components/example_browser.py
+++ grc/gui_qt/components/example_browser.py
@@ -238,7 +238,7 @@ class ExampleBrowser(QWidget, base.Component):
     def find_examples(self, progress_callback, ext="grc"):
         """Iterate through the example flowgraph directories and parse them."""
         examples_dict = {}
-        with Cache(Constants.EXAMPLE_CACHE_FILE) as cache:
+        with Cache(Constants.EXAMPLE_CACHE_FILE, log=False) as cache:
             for entry in self.platform.config.example_paths:
                 if entry == '':
                     log.error("Empty example path!")
