8c8e3f37db1483b20a91195fd55bab859c88733f
Chris Gorman <chrisjohgorman@gmail.com>
2024-07-10 19:13:48 -0400

    gui_prefs_file does not need to use os.path.join
    When os.path.join is used, it appends an extra directory to the config file path.  For
    grc the config file is $HOME/.config/gnuradio/grc.conf/grc.conf and for grc qt the file is
    $HOME/.config/gnuradio/grc.conf/grc_qt.conf.
    
    This patch proposes to change this and remove the grc.conf directory from the path.  This would
    make the grc config file $HOME/.config/gnuradio/grc.conf and the grc qt config file
    $HOME/.config/gnuradio/grc_qt.conf.
    
    All that is needed to accomplish this is to remove the use of os.path.join to generate the
    gui_prefs_file.
    
    Signed-off-by: Chris Gorman <chrisjohgorman@gmail.com>
    (cherry picked from commit ecef337f4a22437e991b8a70828cdcdea8776770)
    Signed-off-by: Jeff Long <willcode4@gmail.com>

--- grc/gui/Config.py
+++ grc/gui/Config.py
@@ -28,8 +28,7 @@ class Config(CoreConfig):
 
     name = 'GNU Radio Companion'
 
-    gui_prefs_file = os.environ.get(
-        'GRC_PREFS_PATH', os.path.join(get_config_file_path(), 'grc.conf'))
+    gui_prefs_file = os.environ.get('GRC_PREFS_PATH', get_config_file_path())
 
     def __init__(self, install_prefix, *args, **kwargs):
         CoreConfig.__init__(self, *args, **kwargs)
