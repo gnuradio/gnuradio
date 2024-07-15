c1f29e94518cbcaca5e01eddc8ec87056865f9d4
Kefu Chai <tchaikov@gmail.com>
2024-07-15 09:27:16 +0800

    blocks,runtime: io_signature: include spdlog/*/ranges.h for using fmt::join()
    fmt::join() was moved into fmt/ranges.h since fmt 11, so let's
    include the corresponding header in spdlog for using it.
    
    Signed-off-by: Kefu Chai <tchaikov@gmail.com>

--- gr-blocks/lib/message_debug_impl.cc
+++ gr-blocks/lib/message_debug_impl.cc
@@ -19,6 +19,11 @@
 #include <pmt/pmt.h>
 #include <spdlog/common.h>
 #include <spdlog/fmt/fmt.h>
+#if __has_include(<spdlog/fmt/ranges.h>)
+#include <spdlog/fmt/ranges.h>
+#elif __has_include(<spdlog/fmt/bundled/ranges.h>)
+#include <spdlog/fmt/bundled/ranges.h>
+#endif
 #include <functional>
 #include <utility>
 #include <vector>
