e2a97e2d8a85fdc598258ef530b13eb22d153cd2
ZakyHermawan <zaky.hermawan9615@gmail.com>
2024-07-24 21:33:37 +0700

    grc: Enable pluggable workflow
    In the previous version of GRC, workflows were hard-coded. There was C++
    vs.  Python, with and without Qt, as hier block or as top block. This
    changeset enables a different paradigm: All workflows are now available
    as plugins. By default, GNU Radio supports all workflows that were
    previously hardcoded (see list above, plus gr-bokeh_gui with Python).
    
    A major part of this changeset is the updated Options block. The YAML
    for the Options block no longer lists all the available workflows.
    Rather, there is a Python-based implementation of the Options block,
    which will dynamically load all workflows it can find.
    
    Co-authored-by: Martin Braun <martin.braun@ettus.com>
    Signed-off-by: ZakyHermawan <zaky.hermawan9615@gmail.com>
    Signed-off-by: Martin Braun <martin.braun@ettus.com>

--- grc/tests/test_generator.py
+++ grc/tests/test_generator.py
@@ -5,22 +5,46 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
 
-import pytest
-
+import logging
+import platform as sys_platform
 from os import path
-import tempfile
 
 from grc.core.platform import Platform
 
 
 def test_generator():
-    # c&p form compiler code.
-    grc_file = path.join(path.dirname(__file__),
-                         'resources', 'test_compiler.grc')
-    out_dir = tempfile.gettempdir()
+    """Verify flow graphs, then generate source codes."""
+    log = logging.getLogger('test_generator')
+
+    grc_files = [
+        path.join(path.dirname(__file__), 'resources', 'test_compiler.grc'),
+        path.join(path.dirname(__file__), 'resources', 'test_python_bokeh_gui_workflow.grc'),
+        path.join(path.dirname(__file__), 'resources', 'test_python_nogui_workflow.grc'),
+        path.join(path.dirname(__file__), 'resources', 'test_python_qt_gui_workflow.grc'),
+        path.join(path.dirname(__file__), 'resources', 'test_cpp_nogui_workflow.grc'),
+        path.join(path.dirname(__file__), 'resources', 'test_cpp_qt_gui_workflow.grc'),
+    ]
+    # TODO: There's an issue with setting environment variables in Windows
+    # in GrTest.cmake. The hier-block flow graphs require the environment
+    # variable GRC_HIER_PATH to be correctly set, but there's a bug on Windows
+    # systems that doesn't do that properly. This is a workaround to skip the
+    # hier-block flow graphs on Windows systems. It should be removed once the
+    # issue is fixed.
+    if sys_platform.system() != 'Windows':
+        grc_files += [
+            path.join(path.dirname(__file__), 'resources', 'test_python_hb_nogui_workflow.grc'),
+            path.join(path.dirname(__file__), 'resources', 'test_python_hb_qt_gui_workflow.grc'),
+            path.join(path.dirname(__file__), 'resources', 'test_cpp_hb_nogui_workflow.grc'),
+            path.join(path.dirname(__file__), 'resources', 'test_cpp_hb_qt_gui_workflow.grc'),
+        ]
+
+    # Note: Even if gr-analog and gr-qtgui are disabled, we can read the GRC
+    # files. No code will be executed in this test.
     block_paths = [
-        path.join(path.dirname(__file__), '../../grc/blocks'),
-        path.join(path.dirname(__file__), '../../gr-blocks/grc')
+        path.normpath(path.join(path.dirname(__file__), '../../grc/blocks')),
+        path.normpath(path.join(path.dirname(__file__), '../../gr-blocks/grc')),
+        path.normpath(path.join(path.dirname(__file__), '../../gr-analog/grc')),
+        path.normpath(path.join(path.dirname(__file__), '../../gr-qtgui/grc')),
     ]
 
     platform = Platform(
@@ -28,14 +52,23 @@ def test_generator():
         prefs=None,
         version='0.0.0',
     )
+    log.info("Building library...")
     platform.build_library(block_paths)
+    log.info("Available workflows:")
+    for workflow in platform.workflow_manager.workflows:
+        log.info(f"{workflow.id}")
 
-    flow_graph = platform.make_flow_graph(grc_file)
-    flow_graph.rewrite()
-    flow_graph.validate()
+    for grc_file in grc_files:
+        log.info(f"Testing {grc_file}...")
+        log.info("Loading flow graph...")
+        flow_graph = platform.make_flow_graph(grc_file)
+        flow_graph.rewrite()
+        flow_graph.validate()
 
-    assert flow_graph.is_valid()
+        assert flow_graph.is_valid(), \
+            f"Flow graph {grc_file} is not valid: " + str(flow_graph.get_error_messages())
 
-    generator = platform.Generator(
-        flow_graph, path.join(path.dirname(__file__), 'resources'))
-    generator.write()
+        log.info("Calling generator...")
+        generator = platform.Generator(
+            flow_graph, path.join(path.dirname(__file__), 'resources'))
+        generator.write()
