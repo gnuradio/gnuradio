5539e1441be2413744bcc813d6e7db9dd1a9f68e
Harishankar14 <harishankarpp7@gmail.com>
2025-02-12 10:03:50 +0530

    Reviewed the reviews, and made the changes (addresses #7697)
    Signed-off-by: Harishankar14 <harishankarpp7@gmail.com>

--- gr-network/python/network/qa_tcp_sink.py
+++ gr-network/python/network/qa_tcp_sink.py
@@ -1,4 +1,8 @@
-import gnuradio
+#!/usr/bin/env python
+# Copyright 2021 Free Software Foundation, Inc.
+# Copyright 2025 Harishankar 
+# This file is part of GNU Radio
+# SPDX-License-Identifier: GPL-3.0-or-later
 from gnuradio import gr, gr_unittest, blocks, network
 import socket
 import threading
@@ -20,10 +24,6 @@ class qa_tcp_sink(gr_unittest.TestCase):
             client_socket.sendall(test_data)
             client_socket.close()
 
-    def tearDown(self):
-        self.tb = None
-        self.received_data = bytearray()  # Reset received data for the next test
-
     def tcp_server(self):
         """TCP Server to receive and store data sent by tcp_sink."""
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
@@ -37,6 +37,10 @@ class qa_tcp_sink(gr_unittest.TestCase):
                     if not data:
                         break
                     self.received_data.extend(data)
+
+    def tearDown(self):
+        self.tb = None
+        self.received_data = bytearray()  # Reset received data for the next test
 
     def test_server_mode(self):
         """Test tcp_sink in server mode (sending known data)."""
@@ -82,13 +86,53 @@ class qa_tcp_sink(gr_unittest.TestCase):
 
         # Run the flowgraph
         self.tb.start()
-        time.sleep(0.5)  # Allow time for data transfer
+        time.sleep(0.1)  # Allow time for data transfer
         self.tb.stop()
         self.tb.wait()
 
         # Ensure the server receives the same data sent by client
         client_thread.join()
         self.assertEqual(self.received_data, bytearray(test_data))
+
+    def tcp_receive(self, serversocket):
+        """Helper function to receive data from tcp_sink."""
+        client_socket, _ = serversocket.accept()
+        with client_socket:
+            while True:
+                data = client_socket.recv(4096)
+                if not data:
+                    break
+                self.received_data.extend(data)
+
+    def test_restart(self):
+        """Test restarting the GNU Radio flowgraph with tcp_sink."""
+        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        serversocket.settimeout(30.0)  # Set a timeout to prevent indefinite blocking
+        serversocket.bind(('localhost', self.port))
+        serversocket.listen()
+
+        # Start a thread to receive data
+        thread = threading.Thread(target=self.tcp_receive, args=(serversocket,))
+        thread.start()
+
+        # Create the GNU Radio flowgraph
+        null_source = blocks.null_source(gr.sizeof_gr_complex)
+        throttle = blocks.throttle(gr.sizeof_gr_complex, 320000, True)
+        tcp_sink = network.tcp_sink(gr.sizeof_gr_complex, 1, '127.0.0.1', self.port, 1)
+        self.tb.connect(null_source, throttle, tcp_sink)
+
+        # Start, stop, and restart the flowgraph
+        self.tb.start()
+        time.sleep(0.1)  # Allow some time for data flow
+        self.tb.stop()
+        time.sleep(0.1)  # Short pause before restarting
+        self.tb.start()
+        time.sleep(0.1)
+        self.tb.stop()
+
+        # Ensure thread completes and clean up
+        thread.join()
+        serversocket.close()
 
 
 if __name__ == '__main__':
