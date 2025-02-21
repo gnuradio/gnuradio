4722ef5b2894be5bc69ddf669232be6eba1b0cca
Marcus Müller <mmueller@gnuradio.org>
2025-02-21 12:03:18 +0100

    filter/mmse_resampler: fix out of bounds access, resampler indexing
    The resampler regularly read input beyond what was available, because d_resamp.interpolate needs ntaps() amount of samples.
    
    I had unify mmse_{resampler,fir_interp}_{cc,ff} into templates.
    
    This became necessary because I didn't want to manually duplicate
    my fixes to mmse_resampler_cc to mmse_resampler_ff.
    
    Less code, less maintenance, fewer bugs.

--- gr-filter/include/gnuradio/filter/mmse_resampler_cc.h
+++ gr-filter/include/gnuradio/filter/mmse_resampler_cc.h
@@ -11,41 +11,13 @@
 #ifndef INCLUDED_MMSE_RESAMPLER_CC_H
 #define INCLUDED_MMSE_RESAMPLER_CC_H
 
-#include <gnuradio/block.h>
 #include <gnuradio/filter/api.h>
+#include <gnuradio/filter/mmse_resampler.h>
+#include <gnuradio/types.h>
 
 namespace gr {
 namespace filter {
-
-/*!
- * \brief resampling MMSE filter with complex input, complex output
- * \ingroup resamplers_blk
- *
- * \details
- * The resampling ratio and mu parameters can be set with a pmt dict
- * message. Keys are pmt symbols with the strings "resamp_ratio" and "mu"
- * and values are pmt floats.
- */
-class FILTER_API mmse_resampler_cc : virtual public block
-{
-public:
-    // gr::filter::mmse_resampler_cc::sptr
-    typedef std::shared_ptr<mmse_resampler_cc> sptr;
-
-    /*!
-     * \brief Build the resampling MMSE filter (complex input, complex output)
-     *
-     * \param phase_shift The phase shift of the output signal to the input
-     * \param resamp_ratio The resampling ratio = input_rate / output_rate.
-     */
-    static sptr make(float phase_shift, float resamp_ratio);
-
-    virtual float mu() const = 0;
-    virtual float resamp_ratio() const = 0;
-    virtual void set_mu(float mu) = 0;
-    virtual void set_resamp_ratio(float resamp_ratio) = 0;
-};
-
+using mmse_resampler_cc = mmse_resampler<gr_complex>;
 } /* namespace filter */
 } /* namespace gr */
 
