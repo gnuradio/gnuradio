8ac681ac75c9e254878d85b028e49df5cf5be6f0
Marcus Müller <mmueller@gnuradio.org>
2025-04-13 13:29:59 +0200

    qtgui/matrixdisplay: avoid extraneous copy, work with Qt<5.14
    Signed-off-by: Marcus Müller <mmueller@gnuradio.org>

--- gr-qtgui/lib/matrix_sink_impl.cc
+++ gr-qtgui/lib/matrix_sink_impl.cc
@@ -1,6 +1,7 @@
 /* -*- c++ -*- */
 /*
  * Copyright 2023 Free Software Foundation, Inc.
+ * Copyright 2025 Marcus Müller
  *
  * This file is part of GNU Radio
  *
@@ -111,9 +112,13 @@ int matrix_sink_impl::work(int noutput_items,
     auto in = static_cast<const input_type*>(input_items[0]);
 
     for (int k = 0; k < noutput_items; k++) {
-        QVector<double> qvec(d_vlen);
-        std::copy(in + k * d_vlen, in + (k + 1) * d_vlen, qvec.begin());
-        emit d_signal->data_ready(qvec);
+#if QT_VERSION < QT_VERSION_CHECK(5, 14, 0)
+        QVector<double> data(d_vlen);
+        std::copy_n(in + k * d_vlen, d_vlen, data.begin());
+#else
+        QVector<double> data(in + k * d_vlen, in + (k + 1) * d_vlen);
+#endif
+        emit d_signal->data_ready(data);
     }
 
     return noutput_items;
