9b4d803deed092f6262d18912451d168d33042fb
Marcus Müller <mmueller@gnuradio.org>
2025-06-19 16:57:00 +0200

    CI: Only run sccache setup if variables set
    Signed-off-by: Marcus Müller <mmueller@gnuradio.org>

--- tools/setup_cmake_cache_flags.bash
+++ tools/setup_cmake_cache_flags.bash
@@ -6,14 +6,17 @@ bail_with_message() {
   exit 0
 }
 
-type -p sccache || bail_with_message 'skipping sccache setup' 'sccache not found'
+type -p sccache > /dev/null || bail_with_message 'skipping sccache setup' 'sccache not found'
+[[ -z "${SCCACHE_S3_BUCKET}" ]] && bail_with_message 'skipping sccache setup' 'S3 bucket not defined (empty/missing SCCACHE_S3_BUCKET env var)'
+
+SCCACHE_BIN="$(type -p sccache)"
 
 add_output() {
   printf '%s=%s\n' "$1" "$2" >> "${GITHUB_OUTPUT}"
 }
 
-add_output C_LAUNCHER   "-DCMAKE_C_COMPILER_LAUNCHER=$(type -p sccache)"
-add_output CXX_LAUNCHER "-DCMAKE_CXX_COMPILER_LAUNCHER=$(type -p sccache)"
+add_output C_LAUNCHER   "-DCMAKE_C_COMPILER_LAUNCHER=${SCCACHE_BIN}"
+add_output CXX_LAUNCHER "-DCMAKE_CXX_COMPILER_LAUNCHER=${SCCACHE_BIN}"
 
 mkdir -p ~/.config/sccache
 
@@ -26,4 +29,6 @@ server_side_encryption = false
 no_credentials = false
 region = "${SCCACHE_S3_REGION}"
 EOF
+
+bail_with_message 'enabling sccache' "Using ${SCCACHE_BIN} ($(${SCCACHE_BIN} --version)) as sccache binary."
 
