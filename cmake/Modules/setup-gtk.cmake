20344029d721666a4eebcabe8d4da7e7cbadb30c
Marian Koop <marian.koop@emerson.com>
2025-07-08 01:33:05 +0200

    cmake: Resolve errors related to differences between C++ package managers
    - GrPython.cmake: Auto-detect Conan toolchain via CMAKE_PREFIX_PATH and
      set Python paths dynamically (REL_PYTHON_INSTALL_PATH/LIB_PATH)
    - setup-gtk.cmake: Use dynamic paths, default GTK_BIN_DIR to install prefix
    - gr-fft/CMakeLists.txt: Handle FFTW3/FFTW3f package name variations
    - gr-qtgui/CMakeLists.txt: Handle qwt/Qwt package name variations
    
    Signed-off-by: Marian Koop <marian.koop@emerson.com>

--- cmake/Modules/setup-gtk.cmake
+++ cmake/Modules/setup-gtk.cmake
@@ -1,14 +1,19 @@
 # Setup variables + functions required to use GTK on Windows due
 # to how difficult GTK makes consuming their binaries (also a lack of RPath)
 
-set(GR_BUNDLE_PYTHON "..\\Python${PYTHON_SHORT_VER}\\python.exe")
+message(STATUS "GTK_BIN_DIR was defined as '${GTK_BIN_DIR}'")
+message(STATUS "REL_PYTHON_INSTALL_PATH was defined as '${REL_PYTHON_INSTALL_PATH}'")
+message(STATUS "REL_PYTHON_INSTALL_LIB_PATH was defined as '${REL_PYTHON_INSTALL_LIB_PATH}'")
 if(NOT GTK_BIN_DIR)
     # set GVS Build default install location for the GTK
     # ideally this is set by the user
-    set(GTK_BIN_DIR "C:/gtk-build/gtk/x64/release/bin")
+    # set(GTK_BIN_DIR "C:/gtk-build/gtk/x64/release/bin")
+    set(GTK_BIN_DIR "${CMAKE_INSTALL_PREFIX}/bin")
+    message(STATUS "Setting GTK_BIN_DIR to '${GTK_BIN_DIR}'.\n Override with -DGTK_BIN_DIR=<path to local installation>")
 endif()
 get_filename_component(GTK_ROOT ${GTK_BIN_DIR} DIRECTORY)
-set(GTK_REL_BIN_DIR "../Python${PYTHON_SHORT_VER}/Lib/site-packages/gi/gtk/bin")
+#set(GTK_REL_BIN_DIR "../Python${PYTHON_SHORT_VER}/Lib/site-packages/gi/gtk/bin")
+set(GTK_REL_BIN_DIR "../${REL_PYTHON_INSTALL_LIB_PATH}/gi/gtk/bin")
 if(WIN32)
     set(DLL_IMPORT_GTK "os.add_dll_directory(os.path.abspath(\"${GTK_REL_BIN_DIR}\"))")
     set(CMAKE_IMPORT_GTK "import os;os.add_dll_directory(\"${GTK_BIN_DIR}\");")
