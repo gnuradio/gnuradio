c5dc6118aa6f003a2e0ae8d1bc9c91f723b7fd26
Marian Koop <marian.koop@emerson.com>
2025-07-08 17:41:16 +0200

    cmake: Implement workarounds to continue support building on ubuntu 22
    Ubuntu 22 uses older cmake version (older then 3.24),
    which treats some function arguments as required and not support
    others.
    - define_property: Define now optional docu arguments
    - find_package:    Filter out new 'GLOBAL' argument
    
    Signed-off-by: Marian Koop <marian.koop@emerson.com>
    Signed-off-by: mohamedal36 <ma1066498@gmail.com>

--- cmake/Modules/GrPython.cmake
+++ cmake/Modules/GrPython.cmake
@@ -13,7 +13,12 @@ set(__INCLUDED_GR_PYTHON_CMAKE TRUE)
 
 include(CMakeOverloads)
 
-define_property(GLOBAL PROPERTY GR_PYTHON_VENDOR_DEPS)
+define_property(
+	GLOBAL
+       	PROPERTY GR_PYTHON_VENDOR_DEPS
+	BRIEF_DOCS "GR Python module dependencies" 
+	FULL_DOCS "GR Python module dependencies"
+)
 
 ########################################################################
 # Setup the python interpreter:
