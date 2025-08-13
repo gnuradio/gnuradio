62ec3bb257362860e6307133562aab64a5ec7780
Marian Koop <marian.koop@emerson.com>
2025-08-14 00:17:56 +0200

    ci: windows: installer: Integrate building UHD as external project
    - Updated UHD source reference to 4.9
    - Added logic to allow passing PYTHON_EXECUTABLE preprocessor var
      to external uhd project and expand cmake argument when building
      cmake deps
    - Enabled building uhd as cmake external project dependency
    - Ensure bundle can include libusb as uhd dependency by utilizing a
      windows specific uhd cmake feature that includes a copy of libusb
      into prefix bin target.
      The Bundle code is intern able find libusb when inspecting uhd.dll
      for its dynamic runtime dependencies.
    
    Signed-off-by: Clemens Felber <clemens.felber@emerson.com>
    Signed-off-by: Marian Koop <marian.koop@emerson.com>
    
    Co-authored-by: Clemens Felber <clemens.felber@emerson.com>
    Signed-off-by: mohamedal36 <ma1066498@gmail.com>

--- release/deps/deps/FetchAndBuildUHD.cmake
+++ release/deps/deps/FetchAndBuildUHD.cmake
@@ -1,17 +1,45 @@
-set(UHD_URL https://github.com/EttusResearch/uhd/archive/refs/tags/v4.6.0.0.tar.gz)
-set(UHD_HASH 5e6eb05abb4a3973bef8837fb45a5147be04698229e22801369f728fcc6db38d)
+set(UHD_VERSION 4.9.0.0) # version is also used in custom install command
+set(UHD_URL https://github.com/EttusResearch/uhd/archive/refs/tags/v${UHD_VERSION}.zip)
+set(UHD_HASH 23DEAD8C3A2C6A57C169853E96D09360494824EFECAD2365CFBB1D2A5D2FA486)
 set(UHD_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/uhd)
+# Define some optional CMAKE_ARGS
+# Solution has the drawback of generating cmake warnings when optional
+# arguments evaluate to empty string.
+# On Windows disable UHD SIM feature
+if(WIN32)
+  set(EXTRA_CMAKE_ARGS_SIM -DENABLE_SIM=OFF)
+  set(EXTRA_CMAKE_ARGS_LIBUSB -DENABLE_EXTEND_WIN_PACKAGING=ON)
+else()
+  set(EXTRA_CMAKE_ARGS_LIBUSB -DENABLE_EXTEND_WIN_PACKAGING=OFF)
+endif(WIN32)
+# UHD is using FindPythonInterp to find and identify the python installation.
+# This method is depreciated and has limited support for detecting python 
+# virtual environments and expects the venv folder name to include an encoded 
+# python major minor version (like venv312).
+# Common workaround for this, is defining the PYTHON_EXECUTABLE preprocessor 
+# variable, if its defined, pass it allow to the external uhd project.
+if(DEFINED PYTHON_EXECUTABLE)
+  set(EXTRA_CMAKE_ARGS_PYTHON -DPYTHON_EXECUTABLE=${PYTHON_EXECUTABLE})
+endif(DEFINED PYTHON_EXECUTABLE)
+# Define value for CMAKE_ARGS as variable to allow variables evaluating to 
+# empty string to be ignored (and not treaded as empty cmake args)
+set(CMAKE_ARGS -DLIBUSB_INCLUDE_DIRS=${LIBUSB_INCLUDE_DIRS} -DCMAKE_BUILD_TYPE=Release -DLIBUSB_LIBRARIES=${LIBUSB_LIBRARIES} -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH} -DUHD_RELEASE_MODE=release -DENABLE_EXAMPLES=OFF -DENABLE_TESTS=OFF  ${EXTRA_CMAKE_ARGS_LIBUSB} -DENABLE_PYTHON_API=ON -DCMAKE_INSTALL_PREFIX=${UHD_INSTALL_DIR} "-DCMAKE_CXX_FLAGS=-DBOOST_ALL_NO_LIB /EHsc" ${EXTRA_CMAKE_ARGS_SIM} ${EXTRA_CMAKE_ARGS_PYTHON})
+# Define custom install command to install python module on Windows
+if(NOT DEFINED PYTHON_SHORT_VER)
+  set(PYTHON_SHORT_VER ${Python_VERSION_MAJOR}${Python_VERSION_MINOR})
+endif(NOT DEFINED PYTHON_SHORT_VER)
+if(WIN32)
+  set(INSTALL_COMMAND INSTALL_COMMAND ninja install && python -m pip install <BINARY_DIR>/python/dist/uhd-${UHD_VERSION}-cp${PYTHON_SHORT_VER}-cp${PYTHON_SHORT_VER}-win_amd64.whl --force --disable-pip-version-check)
+endif(WIN32)
 include(ExternalProject)
 ExternalProject_Add(
     gnu-radio-UHD
     URL ${UHD_URL}
     URL_HASH SHA256=${UHD_HASH}
     PREFIX ${CMAKE_BINARY_DIR}/deps
-    CMAKE_ARGS -DLIBUSB_INCLUDE_DIRS=${LIBUSB_INCLUDE_DIRS} -DCMAKE_BUILD_TYPE=Release -DLIBUSB_LIBRARIES=${LIBUSB_LIBRARIES} -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH} -DUHD_RELEASE_MODE=ON -DENABLE_EXAMPLES=OFF -DENABLE_TESTS=OFF -DCMAKE_INSTALL_PREFIX=${UHD_INSTALL_DIR} "-DCMAKE_CXX_FLAGS=-DBOOST_ALL_NO_LIB /EHsc"
+    CMAKE_ARGS ${CMAKE_ARGS}
+    ${INSTALL_COMMAND}
     SOURCE_SUBDIR host
 )
 
 add_dependencies(gnu-radio-UHD gnu-radio-libusb)
-
-
-# -DBoost_INCLUDE_DIR=${Boost_INCLUDE_DIR} -DBoost_LIBRARIES=${Boost_LIBRARIES}
