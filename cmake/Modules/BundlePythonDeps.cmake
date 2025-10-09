e46c56b59d06adcf6c11288393654a17fa17ab2c
Abhishek Verma <abhishek.verma@ni.com>
2025-10-09 14:04:56 +0200

    python: add missing deps to installer bundle
    -requirements: add matplotlib and QtPy.
    -gr-utils: register matplotlib, colorama and their deps
    -gr-utils: import using  matplotlib.pylab instead of pylab
    -gr-uhd: register matplotlib and its dep
    -bundlePythonDeps: add manual copy of SciPy-libs. Also added
     	Installation for single file modules like six
    -bundlePythonDeps: add install for module's *dist-info folder
     add cmake logic to identify all python modules which are
     installed with bundle and then install the corresponding *dist-info
     folder as well. This helps in listing the available python modules
     in the gnuradio installer bundle.
    -grPythonCmake: correct syntax to clear cmake warnings
    -gr-fec: install build file instead of source file
             cmake install was using the original py file to install
             polar_channel_construction instead of build py file.
             And the template variable @DLL_IMPORT_GTK@ present in
             original file remains as it is after installation.
    -cmake: add cmake file to setup GR_PKG_DIR
            added setupGrPkgPath.cmake file to set a template variable
            GR_PKG_DIR_SETUP with GNU Radio installation directory.
            This is used in python example to add desired dll paths to
            the search directories.
    -grc: register QtPy and add Qt5PrintSupport as grc dependency.
     QtPy needs Qt5Printsupport module which is present in conan
     build dir but is not installed because no gr module has any
     dependency on it. This change adds a helper C file to create
     run-time dependency of Qt5PrintSupport on grc so that cmake
     can link and install it.
    
    Signed-off-by: Abhishek Verma <abhishek.verma@ni.com>

--- cmake/Modules/BundlePythonDeps.cmake
+++ cmake/Modules/BundlePythonDeps.cmake
@@ -8,11 +8,9 @@ else()
   set(py_minor_patch 12.10)
 endif()
 set(PYTHON_VERSION "3.${py_minor_patch}" CACHE STRING "Version of Python to build." FORCE)
-# Use official Python.org full distribution for Windows (includes tkinter and full standard library)
-# Keep ParaView distribution for other platforms
 set(PYTHON_DIR "Python-${PYTHON_VERSION}")
 if(WIN32)
-  # Use Python.org full distribution (includes tkinter, pip, complete standard library)
+  # Use Python.org full distribution (includes tkinter library)
   set(PY_DOWNLOAD_LINK "https://www.python.org/ftp/python/${PYTHON_VERSION}/")
   set(PY_FILENAME "python-${PYTHON_VERSION}-amd64.zip")
   set(PYTHON_PLAT_EXT "-amd64")
@@ -44,18 +42,18 @@ endif()
 message(STATUS "Successfully downloaded ${PY_FILENAME}")
 
 # extract
-if(WIN32)
+if(APPLE)
+  # ParaView distributions create their own subdirectory structure
+  execute_process(COMMAND ${CMAKE_COMMAND} -E tar xfz
+    "${CMAKE_CURRENT_BINARY_DIR}/${PY_FILENAME}"
+    WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}"
+    RESULT_VARIABLE res)
+elseif(WIN32)
   # Python.org full distribution extracts files directly, so create target directory first
   file(MAKE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/${PYTHON_DIR}${PYTHON_PLAT_EXT}")
   execute_process(COMMAND ${CMAKE_COMMAND} -E tar xfz
     "${CMAKE_CURRENT_BINARY_DIR}/${PY_FILENAME}"
     WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/${PYTHON_DIR}${PYTHON_PLAT_EXT}"
-    RESULT_VARIABLE res)
-else()
-  # ParaView distributions create their own subdirectory structure
-  execute_process(COMMAND ${CMAKE_COMMAND} -E tar xfz
-    "${CMAKE_CURRENT_BINARY_DIR}/${PY_FILENAME}"
-    WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}"
     RESULT_VARIABLE res)
 endif()
 if(NOT res EQUAL 0)
@@ -74,39 +72,113 @@ if(GR_PYTHON_DEPS_SET AND ENABLE_PYTHON)
   get_property(GR_PYTHON_DEPS GLOBAL PROPERTY GR_PYTHON_VENDOR_DEPS)
   foreach(module ${GR_PYTHON_DEPS})
     execute_process(COMMAND ${PYTHON_EXECUTABLE} -c
-                    "import importlib
-import inspect
-import os
+                    "import importlib, inspect, os, site, glob
 ${CMAKE_IMPORT_GTK}
 mod = importlib.import_module(\"${module}\")
-print(os.path.dirname(inspect.getfile(mod)))
+module_file = inspect.getfile(mod)
+module_dir = os.path.dirname(module_file)
+is_single_file = os.path.basename(module_file).replace('.py', '') == \"${module}\" or os.path.basename(module_file) == '__init__.py'
+# Find module's dist-info folder
+dist_info_path = ''
+try:
+    site_dir = site.getsitepackages()[1]
+    if os.path.exists(site_dir):
+        module_name = \"${module}\".lower().replace('_', '-').replace('.', '-')
+        for dist_dir in glob.glob(os.path.join(site_dir, '*.dist-info')):
+            dist_name = os.path.basename(dist_dir).lower()
+            if (dist_name.startswith(module_name + '-') or 
+                module_name in dist_name):
+                dist_info_path = dist_dir
+                break
+except: pass
+
+# Output module info and dist-info on single line with separator
+if (is_single_file and not os.path.basename(module_file) == '__init__.py'):
+    # Single file module like six.py
+    print('SINGLE_FILE:' + module_file + '|DIST_INFO:' + dist_info_path)
+else:
+    # Directory module 
+    print('DIRECTORY:' + module_dir + '|DIST_INFO:' + dist_info_path)
 "
       OUTPUT_STRIP_TRAILING_WHITESPACE
-      OUTPUT_VARIABLE MODULE_DIR
+      OUTPUT_VARIABLE MODULE_INFO
     )
-    file(TO_CMAKE_PATH ${MODULE_DIR} MODULE_DIR)
-    message(STATUS "Installing ${module} to ${GR_PYTHON_DIR}/${module}")
-    install(DIRECTORY ${MODULE_DIR} DESTINATION ${GR_PYTHON_DIR} COMPONENT pythonapi)
+    
+    # Parse module and dist-info from single line with | separator
+    string(FIND "${MODULE_INFO}" "|DIST_INFO:" DIST_INFO_POS)
+    if(DIST_INFO_POS GREATER -1)
+      string(SUBSTRING "${MODULE_INFO}" 0 ${DIST_INFO_POS} MODULE_PART)
+      math(EXPR DIST_INFO_START "${DIST_INFO_POS} + 11")  # Length of "|DIST_INFO:"
+      string(SUBSTRING "${MODULE_INFO}" ${DIST_INFO_START} -1 DIST_INFO_DIR)
+    else()
+      set(MODULE_PART "${MODULE_INFO}")
+      set(DIST_INFO_DIR "")
+    endif()
+    
+    # Install module
+    if(MODULE_PART MATCHES "^SINGLE_FILE:")
+      string(REGEX REPLACE "^SINGLE_FILE:" "" MODULE_FILE "${MODULE_PART}")
+      file(TO_CMAKE_PATH ${MODULE_FILE} MODULE_FILE)
+      message(STATUS "Installing single-file module ${module} from ${MODULE_FILE}")
+      install(FILES ${MODULE_FILE} DESTINATION ${GR_PYTHON_DIR} COMPONENT pythonapi)
+    else()
+      string(REGEX REPLACE "^DIRECTORY:" "" MODULE_DIR "${MODULE_PART}")
+      file(TO_CMAKE_PATH ${MODULE_DIR} MODULE_DIR)
+      message(STATUS "Installing directory module ${module} from ${MODULE_DIR}")
+      install(DIRECTORY ${MODULE_DIR} DESTINATION ${GR_PYTHON_DIR} COMPONENT pythonapi)
+    endif()
+    
+    # Install dist-info if found
+    if(DIST_INFO_DIR AND NOT DIST_INFO_DIR STREQUAL "")
+      if(EXISTS "${DIST_INFO_DIR}")
+        file(TO_CMAKE_PATH ${DIST_INFO_DIR} DIST_INFO_DIR)
+        get_filename_component(dist_name "${DIST_INFO_DIR}" NAME)
+        message(STATUS "Installing ${dist_name} for ${module}")
+        install(DIRECTORY ${DIST_INFO_DIR} DESTINATION ${GR_PYTHON_DIR} COMPONENT pythonapi)
+      endif()
+    endif()
+    
     if(${module} STREQUAL "gi" AND WIN32)
       # Gi requires the GTK c++ binaries to function, install them with the python module
       # for a self contained, relocatable Python${PYTHON_SHORT_VER} distro
       message(STATUS "Installing ${GTK_ROOT} to ${GR_PYTHON_DIR}/${module}")
       install(DIRECTORY ${GTK_ROOT}/ DESTINATION ${GR_PYTHON_DIR}/${module}/gtk/ COMPONENT pythonapi)
     endif()
-  endforeach()
-  # Need to install numpy.libs as well as numpy
-  execute_process(COMMAND ${PYTHON_EXECUTABLE} -c
-  "import site
+
+    # Need to install numpy.libs as well as numpy
+    if(${module} STREQUAL "numpy")
+      execute_process(COMMAND ${PYTHON_EXECUTABLE} -c
+      "import site
 import os
 print(os.path.join(site.getsitepackages()[1], \"numpy.libs\"))
 "
-  OUTPUT_STRIP_TRAILING_WHITESPACE
-  OUTPUT_VARIABLE NUMPY_LIB_DIR
-  )
-  if(NUMPY_LIB_DIR)
-    file(TO_CMAKE_PATH ${NUMPY_LIB_DIR} NUMPY_LIB_DIR)
-    install(DIRECTORY ${NUMPY_LIB_DIR} DESTINATION ${GR_PYTHON_DIR} COMPONENT pythonapi)
-  endif()
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+      OUTPUT_VARIABLE NUMPY_LIB_DIR
+      )
+      if(NUMPY_LIB_DIR)
+        file(TO_CMAKE_PATH ${NUMPY_LIB_DIR} NUMPY_LIB_DIR)
+        message(STATUS "Installing numpy.libs from ${NUMPY_LIB_DIR}")
+        install(DIRECTORY ${NUMPY_LIB_DIR} DESTINATION ${GR_PYTHON_DIR} COMPONENT pythonapi)
+      endif()
+    endif()
+
+    # Need to install scipy.libs as well as scipy
+    if(${module} STREQUAL "scipy")
+      execute_process(COMMAND ${PYTHON_EXECUTABLE} -c
+      "import site
+import os
+print(os.path.join(site.getsitepackages()[1], \"scipy.libs\"))
+"
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+      OUTPUT_VARIABLE SCIPY_LIB_DIR
+      )
+      if(SCIPY_LIB_DIR)
+        file(TO_CMAKE_PATH ${SCIPY_LIB_DIR} SCIPY_LIB_DIR)
+        message(STATUS "Installing scipy.libs from ${SCIPY_LIB_DIR}")
+        install(DIRECTORY ${SCIPY_LIB_DIR} DESTINATION ${GR_PYTHON_DIR} COMPONENT pythonapi)
+      endif()
+    endif()
+  endforeach()
 endif()
 
 
