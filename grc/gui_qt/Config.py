25acc8436c9b79038b1eeb33fdbee092d3664d57
Sam Lane <sl01172@surrey.ac.uk>
2025-10-17 14:24:57 +0000

    grcc/grc: add GRC_HIER_PATH_POST & GRC_BLOCK_PATH_POST env variables
    Add extra env variables to allow selective ordering of loading blocks, sometimes (especially when compiling OOTs against an env to which they are already installed) we want to ensure a block is loaded from the source tree as opposed to being overwritten by the intree remenent.
    
    Signed-off-by: Sam Lane <sl01172@surrey.ac.uk>
    
    t

--- grc/gui_qt/Config.py
+++ grc/gui_qt/Config.py
@@ -20,23 +20,3 @@ class Config(CoreConfig):
     @property
     def wiki_block_docs_url_prefix(self):
         return self._gr_prefs.get_string('grc-docs', 'wiki_block_docs_url_prefix', '')
-
-    @property
-    def block_paths(self):
-        paths_sources = self.hier_block_search_dirs + [
-            os.environ.get('GRC_BLOCKS_PATH', ''),
-            self._gr_prefs.get_string('grc', 'local_blocks_path', ''),
-            self._gr_prefs.get_string('grc', 'global_blocks_path', ''),
-            self.qsettings.value('grc/custom_block_paths', ''),
-        ]
-
-        collected_paths = sum((paths.split(os.pathsep)
-                               for paths in paths_sources), [])
-
-        valid_paths = [normpath(expanduser(expandvars(path)))
-                       for path in collected_paths if exists(path)]
-        # Deduplicate paths to avoid warnings about finding blocks twice, but
-        # preserve order of paths
-        valid_paths = list(OrderedDict.fromkeys(valid_paths))
-
-        return valid_paths
