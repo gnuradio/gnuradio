9c0c3bee9cad37d511da45768d37b6eebbe11eb8
Sam Lane <sl01172@surrey.ac.uk>
2025-10-18 10:07:31 +0000

    grc: choose whether to override output directory with state directory
    We need to treat GUI generated hiers differently to grcc, GNU generated need to go in the state directory, wheras grcc need to go wherever theyre told (or the state directory if -u)
    
    Signed-off-by: Sam Lane <sl01172@surrey.ac.uk>

--- grc/gui/Notebook.py
+++ grc/gui/Notebook.py
@@ -15,7 +15,7 @@ from . import Actions
 from .StateCache import StateCache
 from .Constants import MIN_WINDOW_WIDTH, MIN_WINDOW_HEIGHT
 from .DrawingArea import DrawingArea
-
+from ..main import get_state_directory
 
 log = logging.getLogger(__name__)
 
@@ -133,8 +133,8 @@ class Page(Gtk.HBox):
 
     def _handle_scroll_window_key_press(self, widget, event):
         is_ctrl_pg = (
-            event.state & Gdk.ModifierType.CONTROL_MASK and
-            event.keyval in (Gdk.KEY_Page_Up, Gdk.KEY_Page_Down)
+                event.state & Gdk.ModifierType.CONTROL_MASK and
+                event.keyval in (Gdk.KEY_Page_Up, Gdk.KEY_Page_Down)
         )
         if is_ctrl_pg:
             return self.get_parent().event(event)
@@ -147,7 +147,11 @@ class Page(Gtk.HBox):
             generator
         """
         platform = self.flow_graph.parent_platform
-        return platform.Generator(self.flow_graph, os.path.dirname(self.file_path))
+        if self.flow_graph.get_option('generate_options').startswith('hb'):
+            output_dir = get_state_directory()
+        else:
+            output_dir = os.path.dirname(self.file_path)
+        return platform.Generator(self.flow_graph, output_dir)
 
     def _handle_button(self, button):
         """
