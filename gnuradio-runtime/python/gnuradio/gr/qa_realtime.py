0fefa119a6c3dc2fc7926c091ca5efa431ba80b8
Gowri S <gowri21115@gmail.com>
2026-01-25 16:09:49 +0530

    runtime: allow qa_realtime to pass without realtime privileges
    Signed-off-by: Gowri S <gowri21115@gmail.com>

---
+++ gnuradio-runtime/python/gnuradio/gr/qa_realtime.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#
+# Copyright 2023 Free Software Foundation, Inc.
+#
+# This file is part of GNU Radio
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+
+from gnuradio import gr, gr_unittest
+
+
+class test_realtime(gr_unittest.TestCase):
+    def test_000_enable_realtime(self):
+        """Ensure this function is callable."""
+        try:
+            gr.enable_realtime_scheduling()
+        except Exception:
+            pass
+
+
+if __name__ == "__main__":
+    gr_unittest.run(test_realtime)
