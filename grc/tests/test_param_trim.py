38b6e3b7647765143dc64a4988d73c85eb2f0b8c
Marcus Müller <mmueller@gnuradio.org>
2026-02-09 16:33:56 +0100

    grc/param: fix python formatting
    Signed-off-by: Marcus Müller <mmueller@gnuradio.org>
    Signed-off-by: mohamedal36 <ma1066498@gmail.com>

--- grc/tests/test_param_trim.py
+++ grc/tests/test_param_trim.py
@@ -6,25 +6,24 @@ class _DummyParent:
 
 
 def _make_param(dtype: str) -> Param:
-    return Param(parent=_DummyParent(), id='test', dtype=dtype, default='')
+    return Param(parent=_DummyParent(), id="test", dtype=dtype, default="")
 
 
 def test_set_value_trims_whitespace_for_strings():
-    param = _make_param('string')
-    param.set_value('  tcp  ')
-    assert param.get_value() == 'tcp'
+    param = _make_param("string")
+    param.set_value("  tcp  ")
+    assert param.get_value() == "tcp"
 
 
 def test_set_value_trims_mixed_whitespace_variants():
-    param = _make_param('string')
-    for raw in (' tcp', 'tcp ', '  tcp', 'tcp  '):
+    param = _make_param("string")
+    for raw in (" tcp", "tcp ", "  tcp", "tcp  "):
         param.set_value(raw)
-        assert param.get_value() == 'tcp'
+        assert param.get_value() == "tcp"
 
 
 def test_set_value_preserves_multiline_whitespace():
-    param = _make_param('_multiline')
-    text = '  line 1\n  line 2  '
+    param = _make_param("_multiline")
+    text = "  line 1\n  line 2  "
     param.set_value(text)
     assert param.get_value() == text
-
