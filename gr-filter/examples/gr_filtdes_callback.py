664c6eb96950f982f6443985689f0f3b6945adf0
Håkon Vågsether <hakon.vagsether@gmail.com>
2026-05-25 19:19:58 +0200

    filter: Add explicit imports and exec_->exec in filtdes examples
    Signed-off-by: Håkon Vågsether <hakon.vagsether@gmail.com>

--- gr-filter/examples/gr_filtdes_callback.py
+++ gr-filter/examples/gr_filtdes_callback.py
@@ -11,8 +11,7 @@
 from gnuradio.filter import filter_design
 import sys
 try:
-    from qtpy import QtCore, QtGui
-    from qtpy.QtCore import Qt
+    from qtpy.QtWidgets import QApplication
 except ImportError:
     print("Please install PyQt6 to run this script (http://www.riverbankcomputing.co.uk/software/pyqt/download)")
     raise SystemExit(1)
@@ -33,8 +32,8 @@ def print_params(filtobj):
     print("Filter Coefficients", filtobj.get_taps())
 
 
-app = Qt.QApplication(sys.argv)
+app = QApplication(sys.argv)
 # launch function returns gr_filter_design mainwindow object
 main_win = filter_design.launch(sys.argv, print_params)
 main_win.show()
-app.exec_()
+app.exec()
